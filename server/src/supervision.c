e7c0f27b87ac47f30c9d163dbd0865f00ac9561e
Kenneth Ostberg <kenneth.ostberg@ri.se>
2017-05-10 09:47:05 +0200

    Supervision reads traj dir

--- server/src/supervision.c
+++ server/src/supervision.c
@@ -12,11 +12,13 @@
   -- Include files.
   ------------------------------------------------------------*/
 
+
+#include <dirent.h>
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
-#include <errno.h>
 
 #include "util.h"
 #include "supervision.h"
@@ -24,6 +26,9 @@
 /*------------------------------------------------------------
   -- Defines
   ------------------------------------------------------------*/
+
+#define LOCALHOST "127.0.0.1"
+
 #define LDM_SIZE            5
 #define RECV_MESSAGE_BUFFER 1024
 #define SAFETY_MARGIN       0.5
@@ -40,8 +45,63 @@ typedef struct {
 } monitor_t;
 
 /*------------------------------------------------------------
-  -- Function declarations.
+  -- Function declarations & definitions.
   ------------------------------------------------------------*/
+
+/* This code is a straight copy from objectcontrol.c */
+
+static void vFindObjectsInfo ( char object_traj_file    [MAX_OBJECTS][MAX_FILE_PATH],
+                               char object_address_name [MAX_OBJECTS][MAX_FILE_PATH],
+                               int* nbr_objects )
+{
+  DIR*           traj_directory;
+  struct dirent* directory_entry;
+  int            iForceObjectToLocalhost;
+
+  iForceObjectToLocalhost = 0;
+
+  traj_directory = opendir(TRAJECTORY_PATH);
+  if(traj_directory == NULL)
+  {
+    util_error("ERR: Failed to open trajectory directory");
+  }
+
+  (void)iUtilGetIntParaConfFile("ForceObjectToLocalhost",&iForceObjectToLocalhost);
+
+  while ((directory_entry = readdir(traj_directory)) && ((*nbr_objects) < MAX_OBJECTS))
+  {
+    /* Check so it's not . or .. */
+    if (strncmp(directory_entry->d_name,".",1))
+    {
+      bzero(object_address_name[(*nbr_objects)],MAX_FILE_PATH);
+
+      bzero(object_traj_file[(*nbr_objects)],MAX_FILE_PATH);
+      (void)strcat(object_traj_file[(*nbr_objects)],TRAJECTORY_PATH);
+      (void)strcat(object_traj_file[(*nbr_objects)],directory_entry->d_name);
+
+      if(0 == iForceObjectToLocalhost)
+      {
+        (void)strncat(object_address_name[(*nbr_objects)],directory_entry->d_name,strlen(directory_entry->d_name));
+      }
+      else
+      {
+        (void)strcat(object_address_name[(*nbr_objects)],LOCALHOST);
+      }
+
+#ifdef DEBUG
+      printf ( "DBG: SV : otf = %s , oan = %s \n",
+               object_traj_file    [(*nbr_objects)],
+               object_address_name [(*nbr_objects)] );
+        ;
+      fflush(stdout);
+#endif
+
+      ++(*nbr_objects);
+    }
+  }
+  (void)closedir(traj_directory);
+}
+
 
 /*------------------------------------------------------------
 -- Private variables
@@ -53,14 +113,16 @@ typedef struct {
 
 void supervision_task()
 {
-  FILE*     fp              ;
-  char      bFileName [100] ;
-  char      bFileLine [100] ;
-  char*     bFileLine_p     ;
-  size_t    len             ;
-  int       read            ;
+  char      object_traj_file    [MAX_OBJECTS][MAX_FILE_PATH];
+  char      object_address_name [MAX_OBJECTS][MAX_FILE_PATH];
+  int       nbr_objects = 0 ;
 
-  char*     bLine      ;
+  FILE*     fp          [MAX_OBJECTS]       ;
+  char      bFileLine   [MAX_OBJECTS] [100] ;
+  char*     bFileLine_p [MAX_OBJECTS]       ;
+  size_t    len              ;
+  int       read             ;
+
   float     time       ;
   double    x          ;
   double    y          ;
@@ -84,18 +146,18 @@ void supervision_task()
 
   struct timespec sleep_time ;
   struct timespec ref_time   ;
-  
+
   char pcTempBuffer[512];
 
   double dRes = 0.0;
-  
+
 #ifdef DEBUG
   printf ("--------------------------------------------------\n");
   printf ("DBG: SV Supervision started.\n");
   printf ("--------------------------------------------------\n");
   fflush(stdout);
 #endif
-  
+
   /*----------------------------------------------------------------------
   -- Init
   ----------------------------------------------------------------------*/
@@ -109,68 +171,75 @@ void supervision_task()
   -- Open a drive file
   ----------------------------------------------------------------------*/
 
-  bzero ( &bFileName, sizeof(bFileName) );
-  sprintf ( bFileName, "%s", "traj/127.0.0.1");
-  printf("INF: SV fname %s.\n", bFileName);
-  fp = fopen ( bFileName, "r");
-  if (fp == NULL)
+  /* Get objects; name and drive file */
+  vFindObjectsInfo ( object_traj_file,
+                     object_address_name,
+                     &nbr_objects);
+
+  for ( iIndex = 0           ;
+        iIndex < nbr_objects ;
+        ++iIndex )
     {
-      perror ("ERR: SV Failed to open trajectory drive file");
-      exit(1);
+
+      printf ( "INF: SV : Open file %s \n",
+               object_traj_file [ iIndex ] );
+
+      fp [ iIndex ]  = fopen ( object_traj_file [ iIndex ], "rb");
+      if ( fp [ iIndex ] == NULL)
+        {
+          util_error("ERR: Failed to open trajectory file");
+        }
+
+      bFileLine_p [ iIndex ] = bFileLine [ iIndex ];
+
+      bzero ( &bFileLine [ iIndex ], sizeof(bFileLine [ iIndex ]) );
+      read = getline ( &bFileLine_p [ iIndex ], &len, fp [ iIndex ]);
+      printf ( "INF: SV line %s.\n", bFileLine [ iIndex ] );
+
+      bzero ( &bFileLine [ iIndex ], sizeof(bFileLine [ iIndex ]) );
+      read = getline ( &bFileLine_p [ iIndex ] , &len, fp [ iIndex ] );
+      printf ( "INF: SV line %s.\n", bFileLine [ iIndex ] );
+
+      sscanf ( &bFileLine [ iIndex ] [5],
+               "%f;%lf;%lf;%lf;%f;%f;",
+               &time,
+               &x,
+               &y,
+               &z,
+               &hdg,
+               &vel );
+
+      printf ( "INF: SV t %f, x %lf, y %lf, z %lf, hdg %f, vel %f\n",
+               time,
+               x,
+               y,
+               z,
+               hdg,
+               vel );
     }
-  printf("INF: SV fp %d.\n", fp);
 
-  bFileLine_p = bFileLine;
 
-  bzero ( &bFileLine, sizeof(bFileLine) );
-  read = getline ( &bFileLine_p, &len, fp);
-  printf ( "INF: SV line %s.\n", bFileLine );
-  bzero ( &bFileLine, sizeof(bFileLine) );
-  read = getline ( &bFileLine_p, &len, fp);
-  printf ( "INF: SV line %s.\n", bFileLine );
+  printf ( "INF: SV : Opened all trajectory files.\n" );
 
-  bLine = (char *) malloc (200);
+  exit (0);
 
-  sscanf ( &bFileLine [5],
-           "%f;%lf;%lf;%lf;%f;%f;",
-           &time,
-           &x,
-           &y,
-           &z,
-           &hdg,
-           &vel );
 
-  printf ( "INF: SV t %f, x %lf, y %lf, z %lf, hdg %f, vel %f\n",
-           time,
-           x,
-           y,
-           z,
-           hdg,
-           vel );
 
-#ifdef DEBUG
-  printf ( "DBG: SV Supervision read trajectory file %s,  r %d, l %d.\n",
-           bFileName,
-           read,
-           len );
-  fflush(stdout);
-#endif
-  
   /*----------------------------------------------------------------------
   -- Listen loop.
   ----------------------------------------------------------------------*/
-  
+
   (void) iCommInit(IPC_RECV, MQ_SV, 0);
 
   /* Start sending and receiving HEAB, MONR and visualization */
   int iExit = 0;
   int iCommand;
-  
+
   while(!iExit)
   {
     bzero(cpBuffer, RECV_MESSAGE_BUFFER);
     (void) iCommRecv(&iCommand, cpBuffer, RECV_MESSAGE_BUFFER);
-    
+
     #ifdef DEBUG
       printf("INF: SV received a command: %s\n", cpBuffer);
       fflush(stdout);
@@ -185,17 +254,17 @@ void supervision_task()
 
         /* Start parsing messages */
         sscanf(cpBuffer,
-	       "%" SCNu16 ";0;%" SCNu64 ";%" SCNd32 ";%" SCNd32 ";%" SCNd32 ";%" SCNu16 ";%" SCNu16 ";%" SCNu8 ";",
-	       &iIndex,
-	       &timestamp,
-	       &latitude,
-	       &longitude,
-	       &altitude,
-	       &speed,
-	       &heading,
-	       &drivedirection );
+               "%" SCNu16 ";0;%" SCNu64 ";%" SCNd32 ";%" SCNd32 ";%" SCNd32 ";%" SCNu16 ";%" SCNu16 ";%" SCNu8 ";",
+               &iIndex,
+               &timestamp,
+               &latitude,
+               &longitude,
+               &altitude,
+               &speed,
+               &heading,
+               &drivedirection );
 
-        ldm[iIndex][ldm_act_step[iIndex]].timestamp = timestamp;  
+        ldm[iIndex][ldm_act_step[iIndex]].timestamp = timestamp;
         ldm[iIndex][ldm_act_step[iIndex]].latitude = latitude;
         ldm[iIndex][ldm_act_step[iIndex]].longitude = longitude;
         ldm[iIndex][ldm_act_step[iIndex]].altitude = altitude;
@@ -216,10 +285,10 @@ void supervision_task()
                  ldm [ iIndex ] [ ldm_act_step [ iIndex ]].drivedirection );
         fflush(stdout);
 #endif
-	ldm_act_step[iIndex] = ++ldm_act_step[iIndex] % LDM_SIZE;
+        ldm_act_step[iIndex] = ++ldm_act_step[iIndex] % LDM_SIZE;
 
         /* Check if passing line */
-	
+
         ObjectPosition tObjectPos;
         double dP1Lat = 0;
         double dP1Long = 0;
@@ -248,7 +317,7 @@ void supervision_task()
           fflush(stdout);
         #endif
 
-	  UtilCalcPositionDelta(dP1Lat, dP1Long, dP2Lat, dP2Long, &tObjectPos);
+          UtilCalcPositionDelta(dP1Lat, dP1Long, dP2Lat, dP2Long, &tObjectPos);
 
         //double r = 6378137.0;
         //double latmid=(dP1Lat+dP2Lat)/2;
@@ -262,16 +331,16 @@ void supervision_task()
 
         //double dRes = (tObjectPos.x-41.2)*(26.1-55.2)-(tObjectPos.y-55.2)*(65.2-41.2);
 
-	  
-	  dRes = sqrt((x-tObjectPos.x)*(x-tObjectPos.x)+(y-tObjectPos.y)*(y-tObjectPos.y));
-	  
-	  dRes = 0.0;
-	  if(dRes > SAFETY_MARGIN)
-	    {
-	      printf("INF: Sending ABORT from supervisor\n");
-	      fflush(stdout);
-	      (void)iCommSend(COMM_ABORT,NULL);
-	    }
+
+          dRes = sqrt((x-tObjectPos.x)*(x-tObjectPos.x)+(y-tObjectPos.y)*(y-tObjectPos.y));
+
+          dRes = 0.0;
+          if(dRes > SAFETY_MARGIN)
+            {
+              printf("INF: Sending ABORT from supervisor\n");
+              fflush(stdout);
+              //              (void)iCommSend(COMM_ABORT,NULL);
+            }
 
     }
     else if(iCommand == COMM_REPLAY)
@@ -280,7 +349,7 @@ void supervision_task()
       }
     else if(iCommand == COMM_EXIT)
       {
-	iExit = 1;  
+        iExit = 1;
       }
     else
       {
@@ -289,8 +358,8 @@ void supervision_task()
         fflush(stdout);
 #endif
       }
-  }  
-  
+  }
+
   (void) iCommClose();
   fclose (fp);
 }
@@ -298,4 +367,3 @@ void supervision_task()
 /*------------------------------------------------------------
   -- Private functions
   ------------------------------------------------------------*/
-
