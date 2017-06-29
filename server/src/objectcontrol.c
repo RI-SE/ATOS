4aa7e4953e6c29bc86b5ffc0718af57766d97d9f
Sebastian Loh Lindholm <sebastian.lindholm@consat.se>
2017-06-29 13:32:22 +0200

    No recalculation of OSEM before sending

--- server/src/objectcontrol.c
+++ server/src/objectcontrol.c
@@ -787,9 +787,9 @@ int ObjectControlBuildOSEMMessage(char* MessageBuffer, char *Latitude, char *Lon
   {
     //printf("Heading: %s\n", Heading);
     //Data = atof("0.0") * 1e1;
-    Data = 450 - UtilRadToDeg(atof((char *)Heading) * 1e1);
-    while(Data<0) Data+=360;
-    while(Data>360) Data-=360;
+    Data = UtilRadToDeg(atof((char *)Heading) * 1e1);
+    while(Data<0) Data+=3600;
+    while(Data>3600) Data-=3600;
 
     MessageIndex = UtilAddTwoBytesMessageData(MessageBuffer, MessageIndex, (unsigned short)Data);
   }
