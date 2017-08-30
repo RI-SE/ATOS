3d7828345877c77895056d75aae17a48cdbb6d64
Sebastian Loh Lindholm <sebastian.lindholm@consat.se>
2017-08-30 14:14:56 +0200

    DeltaTime added to MTPS

--- server/src/objectcontrol.c
+++ server/src/objectcontrol.c
@@ -544,7 +544,7 @@ void objectcontrol_task()
                     
                     DeltaTime = CurrentTimeU64 - StartTimeU64 - OP[iIndex].TimeArr[OP[iIndex].BestFoundTrajectoryIndex];
                     printf("DeltaTime=%ld, Index=%d, TimeArr=%3.2f, %ld, %ld\n", DeltaTime, OP[iIndex].BestFoundTrajectoryIndex, OP[iIndex].TimeArr[OP[iIndex].BestFoundTrajectoryIndex], CurrentTimeU64 - StartTimeU64, (uint64_t)(TimeToSyncPoint*1000));
-                    MasterTimeToSyncPointU64 = StartTimeU64 + (uint64_t)ASP[i].MasterTrajSyncTime*1000;// + ((uint64_t)(TimeToSyncPoint*1000)); //StartTimeU64 + ((uint64_t)(TimeToSyncPoint*1000) - (CurrentTimeU64 - StartTimeU64));
+                    MasterTimeToSyncPointU64 = StartTimeU64 + (uint64_t)ASP[i].MasterTrajSyncTime*1000 + DeltaTime;// + ((uint64_t)(TimeToSyncPoint*1000)); //StartTimeU64 + ((uint64_t)(TimeToSyncPoint*1000) - (CurrentTimeU64 - StartTimeU64));
                   }
                   else
                   {
