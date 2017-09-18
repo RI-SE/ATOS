0fb90b60dd729816bbf750727e311f9aec34dea5
karljohanode <karljohanode@gmail.com>
2017-09-18 16:01:21 +0200

    Updated script

--- bsObject100.sh
+++ bsObject100.sh
@@ -6,7 +6,9 @@ for (( i=0; i<100; i++ ))
 do
     portA=$((i*2+53240))
     portB=$((i*2+53241))
-    echo "Start object $i with port $portA  $portB"
-    ./object.o 57.7775688 12.7812653 123.43 "$portA" "$portB" &
+    # Calculate lat even do it not used after recieveing OSEM and DOPM
+    lat=$(echo "scale=7; 57.7775688+$i*0.00001" | bc)
+    echo "Start object $i with port $portA  $portB lat $lat"
+    ./object.o "$lat" 12.7812653 123.43 "$portA" "$portB" &
 done
 
