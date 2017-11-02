5842e31d7d189ee79578b86393b7ca863991cbe1
Viktor Johansson <viktor.johansson@astazero.com>
2017-11-02 14:02:30 +0100

    Added tranformation functions for NMEA to ETSI

---
+++ websocket_visualization/nmea2etsi.h
@@ -0,0 +1,19 @@
+/*****************************************************/
+/* nmea2etsi.h NMEA to ETSI CDD conversion functions */
+/* Author: Henrik Eriksson                           */
+/* SP Technical Research Institute of Sweden         */
+/*****************************************************/
+
+#ifndef NMEA2ETSI_H
+#define NMEA2ETSI_H
+
+//#include "poti.h"
+
+uint32_t      ConvertLatitudeNMEAtoETSICDD(char *latitude, char *ns);
+uint32_t     ConvertLongitudeNMEAtoETSICDD(char *longitude, char *ew);
+uint16_t    ConvertSpeedValueNMEAtoETSICDD(char *spd);
+uint16_t  ConvertHeadingValueNMEAtoETSICDD(char *hdg);
+int32_t ConvertAltitudeValueNMEAtoETSICDD(char *alt);
+uint64_t  ConvertTimestapItsNMEAtoETSICDD(char *time, char *date);
+
+#endif
