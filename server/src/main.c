2f0c206403d202e65a163abe45f8933ec951fb49
Jesper Blidkvist <jesper.blidkvist@live.se>
2019-02-27 10:23:56 +0100

    removed simulaor and CIT from make and main

--- server/src/main.c
+++ server/src/main.c
@@ -30,8 +30,6 @@
 #include "supervision.h"
 #include "remotecontrol.h"
 #include "timecontrol.h"
-#include "simulatorcontrol.h"
-//#include "citscontrol.h"
 
 /*------------------------------------------------------------
 -- Defines
@@ -165,6 +163,7 @@ int main(int argc, char *argv[])
     }
     ++iIndex;
 
+/*
     pID[iIndex] = fork();
     if(pID[iIndex] < 0)
     {
@@ -179,6 +178,7 @@ int main(int argc, char *argv[])
       exit(EXIT_SUCCESS);
     }
     ++iIndex;
+  */
 
   /*
  pID[iIndex] = fork();
