94c6bf1e849a760517592c9c15c063528700ba10
Lukas Wikander <lukas.wikander@astazero.com>
2019-03-18 10:53:10 +0100

    Added byte raw file creator function

--- server/inc/logging.h
+++ server/inc/logging.h
@@ -6,65 +6,22 @@
 // Should this be OS independent?
 #include <linux/limits.h>
 
-#define LOG_DIR_PATH "log"
-#define LOG_MODULE_NAME_MAX_LENGTH 100
+#define LOG_DIR_PATH "log" /*!< Directory where log files are to be stored */
+#define LOG_MODULE_NAME_MAX_LENGTH 100 /*!< Maximum length of module name */
 
-// Used for selecting individual log entry urgency as well as log
-// filtering level
+/*! Used for selecting individual log entry urgency as well as log
+* filtering level */
 typedef enum
 {
-    LOG_LEVEL_ERROR=0,
-    LOG_LEVEL_WARNING=1,
-    LOG_LEVEL_INFO=2,
-    LOG_LEVEL_DEBUG=3
+    LOG_LEVEL_ERROR=0,      /*!< Error level message    */
+    LOG_LEVEL_WARNING=1,    /*!< Warning level message  */
+    LOG_LEVEL_INFO=2,       /*!< Info level message     */
+    LOG_LEVEL_DEBUG=3       /*!< Debug level message    */
 } LOG_LEVEL;
 
-/* LogInit(logName, logMinLevel):
-*   Initialize (or reinitialize) a log for writing. Creates a new file in the specified directory,
-*   including the specified logName and date of creation. If such a file already exists, a reference
-*   to that file is returned instead. Subsequent write operations append to the file and do not over-
-*   write previous entries. The second input allows specifying the minimum urgency a message must
-*   have to be printed to the file.
-*   Inputs:
-*     - logName: A null terminated string reference used for naming the log file
-*     - logMinLevel: Minimum urgency of messages to be printed to the returned log file
-*   Example:
-*     // This creates a log named "foo_log-2019-03-08.log" which filters out debug messages
-*     LogInit("foo_log", LOG_LEVEL_INFO);
-*/
 void LogInit(char* logName, LOG_LEVEL logMinLevel);
-
-/* LogMessage(messageLevel, format, ...):
-*   Write an entry into the specified log, with a specified message level attached to the message.
-*   The log entry contains the time and date of the entry (one second precision), the message level
-*   (e.g. IFO, ERR etc.) and the actual entry. The message is also printed to stdout if it has
-*   sufficient urgency.
-*   Inputs:
-*     - messageLevel: Urgency of the message to be written
-*     - format: A null terminated format string which may or may not contain a number of %d, %s etc..
-*     - ...: Any number of inputs which match format specifiers in the format string
-*   Example:
-*     LogInit("foo_log", LOG_LEVEL_INFO);
-*     // This enters the following string into foo_log: "[2019-03-08|09:58:23|ERR]: Division by zero (0)!"
-*     LogMessage(LOG_LEVEL_ERROR, "Division by %s (%d)!", "zero", 0);
-*/
 void LogMessage(LOG_LEVEL messageLevel, const char* format, ...);
-
 void LogPrintBytes(char * byteArray, unsigned long int firstIndex, unsigned long int length);
-
-/* LogPrint(format, ...):
-******  THIS FUNCTION IS INTENDED FOR QUICK DEBUGGING AND SHOULD NOT TO BE USED IN FINAL CODE ******
-*   Write an entry into the specified log, without specifying the log level. The log entry contains the
-*   time and date of the entry (one second precision), the tag 'N/A' for log level and the actual entry.
-*   The message is also printed to stdout.
-*   Inputs:
-*     - format: A null terminated format string which may or may not contain a number of %d, %s etc..
-*     - ...: Any number of inputs which match format specifiers in the format string
-*   Example:
-*     LogInit("foo_log", LOG_LEVEL_INFO);
-*     // This enters the following string into foo_log: "[2019-03-08|09:58:23|N/A]: Division by zero (0)!"
-*     LogPrint("Division by %s (%d)!", "zero", 0);
-*/
 void LogPrint(const char* format, ...);
 
 #endif
--- server/src/logging.c
+++ server/src/logging.c
@@ -4,38 +4,54 @@
 #include <string.h>
 
 
-// Colors for the log
+/*! Colors for the log */
 #define COLOR_ERROR "\x1b[31m"
 #define COLOR_WARNING "\x1b[33m"
 #define COLOR_INFO "\x1b[0m"
 #define COLOR_DEBUG "\x1b[32m"
-#define COLOR_RESET "\x1b[0m"
 #define COLOR_BYTES "\x1b[35m"
+#define COLOR_RESET "\x1b[0m"
 
 // String used to create the log file
-#define FILENAME_DATESTR_MAX_LENGTH 100
-#define FILENAME_DATESTR_FORMAT "%Y-%m-%d"
-#define LOG_FILE_WRITE_MODE "a"
+#define FILENAME_DATESTR_MAX_LENGTH 100 /*!< Maximum length of the date tag used for the log file */
+#define FILENAME_DATESTR_FORMAT "%Y-%m-%d" /*!< Format of the date tag used for the log file */
+#define LOG_FILE_WRITE_MODE "a" /*!< Mode of writing to the log file; 'a' appends to the end */
 
 // String used for each log entry
-#define LOG_DATESTR_MAX_LENGTH 100
-#define LOG_DATESTR_FORMAT "%Y-%m-%d|%H:%M:%S"
+#define LOG_DATESTR_MAX_LENGTH 100 /*!< Maximum length of the tag placed before each log entry */
+#define LOG_DATESTR_FORMAT "%Y-%m-%d|%H:%M:%S" /*!< Format of the tag placed before each log entry */
 
-// Used as reference to a log
+// Used for debug logs
+#define RECORD_DATESTR_MAX_LENGTH 100
+#define RECORD_DATESTR_FORMAT "%Y-%m-%d-%H%M%S"
+#define BYTE_RECORD_WRITE_MODE "wb"
+
+/*! Used as reference to a log file */
 typedef struct
 {
-    LOG_LEVEL level;
-    char logModuleName[LOG_MODULE_NAME_MAX_LENGTH];
-    char fullLogPath[PATH_MAX];
+    LOG_LEVEL level; /*!< Filter level for the log */
+    char logModuleName[LOG_MODULE_NAME_MAX_LENGTH]; /*!< Name of the module using the log */
+    char fullLogPath[PATH_MAX]; /*!< Path to the log file used */
 } LOG;
 
-// Local static variable (which will be different for every fork() of the process)
-static LOG moduleLog;
+static LOG moduleLog; /*!< Local log reference (different for each ::fork of the process */
 
-// For logging errors in writing to logs
+/*! Log used for reporting logging problems, common between all modules */
 static LOG logging_meta_info;
 #define META_LOG_NAME "logging-meta-info"
 
+/*!
+* \brief Initialize (or reinitialize) a log for writing. Creates a new file in the specified directory,
+*   including the specified logName and date of creation. If such a file already exists, a reference
+*   to that file is returned instead. Subsequent write operations append to the file and do not over-
+*   write previous entries. The second input allows specifying the minimum urgency a message must
+*   have to be printed to the file.
+* \param logName A null terminated string reference used for naming the log file
+* \param logMinLevel Minimum urgency of messages to be printed to the returned log file
+* \example
+*     // This creates a log named "foo_log-2019-03-08.log" which filters out debug messages
+*     LogInit("foo_log", LOG_LEVEL_INFO);
+*/
 void LogInit(char* logName, LOG_LEVEL logMinLevel)
 {
     char dateStr[FILENAME_DATESTR_MAX_LENGTH];
@@ -43,6 +59,7 @@ void LogInit(char* logName, LOG_LEVEL logMinLevel)
     struct tm * timeInfo;
 
     bzero(moduleLog.fullLogPath, PATH_MAX);
+
     // Build log path name string
     strcpy(moduleLog.fullLogPath, LOG_DIR_PATH);
     strcat(moduleLog.fullLogPath, "/");
@@ -74,6 +91,19 @@ void LogInit(char* logName, LOG_LEVEL logMinLevel)
 }
 
 
+
+/*!
+* \brief Write an entry into the specified log, with a specified message level attached to the message.
+*   The log entry contains the time and date of the entry (one second precision), the message level
+*   (e.g. IFO, ERR etc.) and the actual entry. The message is also printed to stdout if it has
+*   sufficient urgency.
+* \param messageLevel Urgency of the message to be written
+* \param format A null terminated format string which may or may not contain a number of %d, %s etc..
+* \example
+*     LogInit("foo_log", LOG_LEVEL_INFO);
+*     // This enters the following string into foo_log: "[2019-03-08|09:58:23|ERR]: Division by zero (0)!"
+*     LogMessage(LOG_LEVEL_ERROR, "Division by %s (%d)!", "zero", 0);
+*/
 void LogMessage(LOG_LEVEL messageLevel, const char* format, ...)
 {
     if (messageLevel == LOG_LEVEL_ERROR)
@@ -147,12 +177,71 @@ void LogMessage(LOG_LEVEL messageLevel, const char* format, ...)
     }
 }
 
-
-void LogPrintBytes(char * byteArray, unsigned long int firstIndex, unsigned long int length)
+/*!
+ * \brief Create a raw data file with the contents of a byteArray, starting at firstIndex and ending at nBytes-firstIndex.
+ *     The file is named according to logName with the date it was created (down to the second), ending with ".raw". If a
+ *     previous record exists with the same name, the old record is overwritten.
+ * \param byteArray Array of bytes to be written to file
+ * \param firstIndex Index of the first byte to be written
+ * \param nBytes Number of bytes to write
+ * \param logName Descriptive name of the byte record file
+ */
+void LogRecordBytes(char * byteArray, unsigned long int firstIndex, unsigned long int nBytes, const char * logName)
 {
-    if (firstIndex >= length)
+    time_t rawTime;
+    struct tm * timeInfo;
+    char dateStr[FILENAME_DATESTR_MAX_LENGTH];
+    FILE* fp;
+    char fullRecordPath[PATH_MAX];
+
+    // Include current date in record name
+    time(&rawTime);
+    timeInfo = localtime(&rawTime);
+    strftime(dateStr, RECORD_DATESTR_MAX_LENGTH, RECORD_DATESTR_FORMAT, timeInfo);
+
+    bzero(fullRecordPath, PATH_MAX);
+
+    // Build record path name string
+    strcpy(fullRecordPath, LOG_DIR_PATH);
+    strcat(fullRecordPath, "/");
+    strcat(fullRecordPath, logName);
+    strcat(fullRecordPath, "-");
+
+    strcat(fullRecordPath, dateStr);
+    strcat(fullRecordPath, ".raw");
+
+    // Try to open the specified record file
+    fp = fopen(fullRecordPath, BYTE_RECORD_WRITE_MODE);
+
+    if (fp == NULL)
     {
-        printf("[%s] Cannot start at index larger than byte array length\n", moduleLog.logModuleName);
+        fp = fopen(moduleLog.fullLogPath, LOG_FILE_WRITE_MODE);
+        printf("[%s] Unable to open log file for writing: %s\n", moduleLog.logModuleName, moduleLog.fullLogPath);
+        fclose(fp);
+        return;
+    }
+
+    // Write the bytes to the file
+    fwrite(byteArray+firstIndex, 1, nBytes-firstIndex, fp);
+    fclose(fp);
+
+    // Log the action
+    LogMessage(LOG_LEVEL_DEBUG,"Created raw byte file <%s>",fullRecordPath);
+}
+
+
+/*!
+ * \brief Print the ASCII hex byte array representation of byteArray to log file, starting at firstIndex
+ *     and printing nBytes. This function disregards the log level as it is intended for quick debugging.
+ * \param byteArray Array of bytes to be written to file
+ * \param firstIndex Index of the first byte to be written
+ * \param nBytes Number of bytes to write
+ */
+void LogPrintBytes(char * byteArray, unsigned long int firstIndex, unsigned long int nBytes)
+{
+    if (firstIndex >= nBytes)
+    {
+        LogMessage(LOG_LEVEL_ERROR,"Cannot start at index larger than byte array length");
         return;
     }
 
@@ -175,8 +264,9 @@ void LogPrintBytes(char * byteArray, unsigned long int firstIndex, unsigned long
 
     fprintf(fp, "[%s|" COLOR_BYTES "N/A" COLOR_RESET "]: ", dateStr);
     printf("[%s] ", moduleLog.logModuleName);
-    for (unsigned long int i = firstIndex; i < length; ++i)
+    for (unsigned long int i = firstIndex; i < nBytes; ++i)
     {
+        printf("%X ", byteArray[i]);
         fprintf(fp,"%X ", byteArray[i]);
     }
 
@@ -187,6 +277,17 @@ void LogPrintBytes(char * byteArray, unsigned long int firstIndex, unsigned long
 }
 
 
+/*!
+* \brief Write an entry into the specified log, without specifying the log level. The log entry contains the
+*   time and date of the entry (one second precision), the tag 'N/A' for log level and the actual entry.
+*   The message is also printed to stdout. This function is intended for quick debugging and should not be
+*   used for non-debugging purposes.
+* \param format A null terminated format string which may or may not contain a number of %d, %s etc..
+* \example
+*     LogInit("foo_log", LOG_LEVEL_INFO);
+*     // This enters the following string into foo_log: "[2019-03-08|09:58:23|N/A]: Division by zero (0)!"
+*     LogPrint("Division by %s (%d)!", "zero", 0);
+*/
 void LogPrint(const char* format, ...)
 {
     time_t rawTime;
