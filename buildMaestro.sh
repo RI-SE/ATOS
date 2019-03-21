85b7b44998c864d6a552505148870ce6a5d53c28
Lukas Wikander <lukas.wikander@astazero.com>
2019-03-21 18:11:52 +0100

    Changed CMakelists and build script to use new file structure

--- buildMaestro.sh
+++ buildMaestro.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
-cd server
-mkdir ./build
+MAESTRODIR=$(pwd)
+cd util/C
+cmake -G "Unix Makefiles" .
+make
+cd $MAESTRODIR/server
+mkdir build
 cd build
 cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug ..
-make
 cp -R ../conf/ .
-mkdir traj
+mkdir traj && mkdir log
 cp ../traj/0.traj ./traj/127.0.0.1
+cd build
+make
+cd $MAESTRODIR
