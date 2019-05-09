e4210b856117840378ec3b648c1c4aac18e777a8
Lukas Wikander <lukas.wikander@astazero.com>
2019-05-09 18:33:08 +0200

    Added MQ bus initialisation in main with the util library

--- server/src/main.c
+++ server/src/main.c
@@ -32,6 +32,7 @@
 #include "timecontrol.h"
 #include "supervisorcontrol.h"
 #include "logging.h"
+#include "mqbus.h"
 
 
 
@@ -69,6 +70,7 @@ static const size_t numberOfModules = sizeof(allModules) / sizeof(ModuleTask);
 ------------------------------------------------------------*/
 int readArgumentList(int argc, char *argv[], Options *opts);
 void printHelp(char* progName);
+int initializeMessageQueueBus(void);
 
 
 /*------------------------------------------------------------
@@ -80,9 +82,10 @@ int main(int argc, char *argv[])
     unsigned int moduleNumber = 0;
     Options options;
     pid_t pID[numberOfModules];
+    int result = 0;
 
     if (readArgumentList(argc, argv, &options))
-        return 0;
+        exit(EXIT_FAILURE);
 
     // Share time between child processes
     GPSTime = mmap(NULL, sizeof *GPSTime, PROT_READ | PROT_WRITE, MAP_SHARED | MAP_ANONYMOUS, -1, 0);
@@ -97,6 +100,10 @@ int main(int argc, char *argv[])
     LogPrint("Version %s", MaestroVersion);
     LogMessage(LOG_LEVEL_INFO, "Central started");
     LogMessage(LOG_LEVEL_DEBUG, "Verbose mode enabled");
+
+    // Initialise message queue bus
+    if(initializeMessageQueueBus())
+        exit(EXIT_FAILURE);
 
     // For all modules in allModules, start corresponding process in a fork
     for (moduleNumber = 0; moduleNumber < numberOfModules-1; ++moduleNumber)
@@ -119,7 +126,63 @@ int main(int argc, char *argv[])
     exit(EXIT_SUCCESS);
 }
 
+/*!
+ * \brief initialiseMessageQueueBus Wrapper function for MQBusInit with explanatory log printouts
+ * \return 0 upon success, -1 upon failure
+ */
+int initializeMessageQueueBus(void)
+{
+    int nbrOfQueues = numberOfModules;
+    enum MQBUS_ERROR result = MQBusInit(nbrOfQueues);
 
+    // Printouts according to result
+    switch (result)
+    {
+    case MQBUS_OK:
+        LogMessage(LOG_LEVEL_INFO, "Initialized message queue bus");
+        return 0;
+    case MQBUS_INVALID_INPUT_ARGUMENT:
+        LogMessage(LOG_LEVEL_ERROR, "Invalid number of message queues specified upon initialization: %d", nbrOfQueues);
+        break;
+    case MQBUS_QUEUE_NOT_EXIST:
+        LogMessage(LOG_LEVEL_ERROR, "Unable to initialize resource message queue: already exists");
+        break;
+    case MQBUS_RESOURCE_NOT_EXIST:
+        LogMessage(LOG_LEVEL_ERROR, "Resource message queue does not exist");
+        break;
+    case MQBUS_EMPTY:
+        LogMessage(LOG_LEVEL_ERROR, "Message queue empty");
+        break;
+    case MQBUS_MAX_QUEUES_LIMIT_REACHED:
+        LogMessage(LOG_LEVEL_ERROR, "Maximum number of message queues reached");
+        break;
+    case MQBUS_OPEN_FAIL:
+        LogMessage(LOG_LEVEL_ERROR, "Unable to open message queue");
+        break;
+    case MQBUS_CLOSE_FAIL:
+        LogMessage(LOG_LEVEL_ERROR, "Unable to close message queue");
+        break;
+    case MQBUS_DESCRIPTOR_NOT_FOUND:
+        LogMessage(LOG_LEVEL_ERROR, "Message queue descriptor not found");
+        break;
+    case MQBUS_NO_READABLE_MQ:
+        LogMessage(LOG_LEVEL_ERROR, "Connection to message queue bus does not exist");
+        break;
+    case MQBUS_MQ_COULD_NOT_BE_DESTROYED:
+        LogMessage(LOG_LEVEL_ERROR, "Could not delete message queue");
+        break;
+    }
+    return -1;
+}
+
+
+/*!
+ * \brief readArgumentList Decodes the list of input arguments and fills an options struct
+ * \param argc Length of argument vector
+ * \param argv String array, starting with the executable name
+ * \param opts Options struct
+ * \return 0 upon success, -1 upon failure, 1 when clarification is needed
+ */
 int readArgumentList(int argc, char *argv[], Options *opts)
 {
     char *progName = strrchr(argv[0],'/');
@@ -164,6 +227,10 @@ int readArgumentList(int argc, char *argv[], Options *opts)
     return 0;
 }
 
+/*!
+ * \brief printHelp
+ * \param progName
+ */
 void printHelp(char* progName)
 {
     printf("Usage: %s [OPTION]\n", progName);
