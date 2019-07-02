c0fb63d26da8487f36d88a6db4cd95520587f6e2
Lukas Wikander <lukas.wikander@astazero.com>
2019-07-02 15:31:56 +0200

    Implemented check on allowed parameters

--- modules/ScenarioControl/inc/braketrigger.h
+++ modules/ScenarioControl/inc/braketrigger.h
@@ -11,7 +11,12 @@ public:
 
     TriggerType_t getType() { return TRIGGER_BRAKE; }
 
-    TriggerReturnCode_t appendTriggerParameter(uint32_t triggerParameter);
+    TriggerReturnCode_t appendTriggerParameter(TriggerParameter_t triggerParameter);
+
+private:
+    std::set<TriggerParameter_t> triggerParameters;
+
+    TriggerReturnCode_t checkTriggerParameter(TriggerParameter_t triggerParameter);
 };
 
 #endif // BRAKETRIGGER_H
--- modules/ScenarioControl/src/braketrigger.cpp
+++ modules/ScenarioControl/src/braketrigger.cpp
@@ -1,6 +1,33 @@
 #include "braketrigger.h"
 
-Trigger::TriggerReturnCode_t BrakeTrigger::appendTriggerParameter(uint32_t triggerParameter)
-{
+static constexpr Trigger::TriggerParameter_t acceptedParameters[] = {
+    Trigger::TRIGGER_PARAMETER_FALSE,
+    Trigger::TRIGGER_PARAMETER_TRUE,
+    Trigger::TRIGGER_PARAMETER_RELEASED,
+    Trigger::TRIGGER_PARAMETER_PRESSED,
+    Trigger::TRIGGER_PARAMETER_LOW,
+    Trigger::TRIGGER_PARAMETER_HIGH,
+    Trigger::TRIGGER_PARAMETER_RISING_EDGE,
+    Trigger::TRIGGER_PARAMETER_FALLING_EDGE,
+    Trigger::TRIGGER_PARAMETER_ANY_EDGE,
+    Trigger::TRIGGER_PARAMETER_EQUAL_TO,
+    Trigger::TRIGGER_PARAMETER_NOT_EQUAL_TO
+};
 
+Trigger::TriggerReturnCode_t BrakeTrigger::appendTriggerParameter(Trigger::TriggerParameter_t triggerParameter)
+{
+    TriggerReturnCode_t retval = NOT_OK;
+
+    if( (retval = checkTriggerParameter(triggerParameter)) != OK)
+        return retval;
+
+}
+
+Trigger::TriggerReturnCode_t BrakeTrigger::checkTriggerParameter(TriggerParameter_t triggerParameter)
+{
+    for (const TriggerParameter_t param : acceptedParameters) {
+        if (triggerParameter == param)
+            return OK;
+    }
+    return NOT_OK;
 }
