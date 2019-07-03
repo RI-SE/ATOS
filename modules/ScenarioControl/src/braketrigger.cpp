ddebb46aae0f1c7e8b96616bb740d28a395db33b
Lukas Wikander <lukas.wikander@astazero.com>
2019-07-03 13:43:34 +0200

    Implemented the functions as specified by header file

--- modules/ScenarioControl/src/braketrigger.cpp
+++ modules/ScenarioControl/src/braketrigger.cpp
@@ -1,33 +1,28 @@
 #include "braketrigger.h"
 
-static constexpr Trigger::TriggerParameter_t acceptedParameters[] = {
-    Trigger::TRIGGER_PARAMETER_FALSE,
-    Trigger::TRIGGER_PARAMETER_TRUE,
-    Trigger::TRIGGER_PARAMETER_RELEASED,
-    Trigger::TRIGGER_PARAMETER_PRESSED,
-    Trigger::TRIGGER_PARAMETER_LOW,
-    Trigger::TRIGGER_PARAMETER_HIGH,
-    Trigger::TRIGGER_PARAMETER_RISING_EDGE,
-    Trigger::TRIGGER_PARAMETER_FALLING_EDGE,
-    Trigger::TRIGGER_PARAMETER_ANY_EDGE,
-    Trigger::TRIGGER_PARAMETER_EQUAL_TO,
-    Trigger::TRIGGER_PARAMETER_NOT_EQUAL_TO
-};
 
-Trigger::TriggerReturnCode_t BrakeTrigger::appendTriggerParameter(Trigger::TriggerParameter_t triggerParameter)
+BrakeTrigger::BrakeTrigger(Trigger::TriggerID_t triggerID) : Trigger (triggerID)
+{
+
+}
+
+BrakeTrigger::~BrakeTrigger()
+{
+    parameters.clear();
+}
+
+Trigger::TriggerReturnCode_t BrakeTrigger::appendParameter(Trigger::TriggerParameter_t triggerParameter)
 {
     TriggerReturnCode_t retval = NOT_OK;
 
     if( (retval = checkTriggerParameter(triggerParameter)) != OK)
         return retval;
 
+    parameters.push_back(triggerParameter);
+    return OK;
 }
 
-Trigger::TriggerReturnCode_t BrakeTrigger::checkTriggerParameter(TriggerParameter_t triggerParameter)
+Trigger::TriggerReturnCode_t BrakeTrigger::parseParameters()
 {
-    for (const TriggerParameter_t param : acceptedParameters) {
-        if (triggerParameter == param)
-            return OK;
-    }
-    return NOT_OK;
+
 }
