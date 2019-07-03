6419fe94cadfad05ff50bde1874d514f6e2a887c
Lukas Wikander <lukas.wikander@astazero.com>
2019-07-03 13:42:49 +0200

    Implemented acceptedParameters getter function

--- modules/ScenarioControl/inc/braketrigger.h
+++ modules/ScenarioControl/inc/braketrigger.h
@@ -17,9 +17,23 @@ public:
     TriggerReturnCode_t parseParameters();
 
 private:
-    std::set<TriggerParameter_t> triggerParameters;
-
-    TriggerReturnCode_t checkTriggerParameter(TriggerParameter_t triggerParameter);
+    std::list<TriggerParameter_t> parameters;
+    std::set<Trigger::TriggerParameter_t> getAcceptedParameters()
+    {
+        return {
+            Trigger::TRIGGER_PARAMETER_FALSE,
+            Trigger::TRIGGER_PARAMETER_TRUE,
+            Trigger::TRIGGER_PARAMETER_RELEASED,
+            Trigger::TRIGGER_PARAMETER_PRESSED,
+            Trigger::TRIGGER_PARAMETER_LOW,
+            Trigger::TRIGGER_PARAMETER_HIGH,
+            Trigger::TRIGGER_PARAMETER_RISING_EDGE,
+            Trigger::TRIGGER_PARAMETER_FALLING_EDGE,
+            Trigger::TRIGGER_PARAMETER_ANY_EDGE,
+            Trigger::TRIGGER_PARAMETER_EQUAL_TO,
+            Trigger::TRIGGER_PARAMETER_NOT_EQUAL_TO
+        };
+    }
 };
 
 #endif // BRAKETRIGGER_H
