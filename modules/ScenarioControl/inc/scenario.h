e6dc2624108d4488ea0a862b7faf08c497a5db2d
Lukas Wikander <lukas.wikander@astazero.com>
2019-07-15 17:45:08 +0200

    Missed a ctrl+s ...

--- modules/ScenarioControl/inc/scenario.h
+++ modules/ScenarioControl/inc/scenario.h
@@ -11,16 +11,16 @@ public:
     typedef enum {OK, NOT_OK, DUPLICATE_ELEMENT} ScenarioReturnCode_t;
 
     Scenario();
+    ~Scenario();
 
     ScenarioReturnCode_t linkTriggersWithActions(std::set<Trigger*> ts, std::set<Action*> as);
     ScenarioReturnCode_t linkTriggersWithAction(std::set<Trigger*> ts, Action* a);
-    ScenarioReturnCode_t linkTriggerWithActions(Trigger* t, std::set<Action*> a);
+    ScenarioReturnCode_t linkTriggerWithActions(Trigger* t, std::set<Action*> as);
     ScenarioReturnCode_t linkTriggerWithAction(Trigger* t, Action* a);
 
     ScenarioReturnCode_t addTrigger(Trigger &t);
     ScenarioReturnCode_t addAction(Action &a);
 
-    Trigger* getTriggerByID(Trigger::TriggerID_t id);
     std::set<Causality> getCausalities(void);
 
     void updateTrigger(Trigger::TriggerID_t id);
