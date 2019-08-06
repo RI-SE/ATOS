9e6a19fb9f17233fab3bb25541264df22718b060
Lukas Wikander <lukas.wikander@astazero.com>
2019-08-06 15:30:25 +0200

    Removed debug printout

--- server/src/systemcontrol.c
+++ server/src/systemcontrol.c
@@ -1084,7 +1084,7 @@ void systemcontrol_task(TimeType *GPSTime, GSDType *GSD, LOG_LEVEL logLevel)
                 if(RVSSConfigU32 & RVSS_TIME_CHANNEL)
                 {
                     SystemControlBuildRVSSTimeChannelMessage(RVSSData, &RVSSMessageLengthU32, GPSTime, 1);
-                    UtilSendUDPData("SystemControl", &RVSSChannelSocket, &RVSSChannelAddr, RVSSData, RVSSMessageLengthU32, 1);
+                    UtilSendUDPData("SystemControl", &RVSSChannelSocket, &RVSSChannelAddr, RVSSData, RVSSMessageLengthU32, 0);
                 }
 
                 if(RVSSConfigU32 & RVSS_MAESTRO_CHANNEL)
