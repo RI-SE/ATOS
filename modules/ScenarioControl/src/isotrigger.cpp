4a5cb7115cb387c56fab9dd8bb8deea4be9b891a
LukasWikander <lukas.wikander@astazero.com>
2019-08-28 10:05:54 +0200

    Update isotrigger.cpp
    Removed superfluous TODO

--- modules/ScenarioControl/src/isotrigger.cpp
+++ modules/ScenarioControl/src/isotrigger.cpp
@@ -4,7 +4,6 @@ Trigger::TriggerReturnCode_t ISOTrigger::update(TREOData data)
 {
     if (data.triggerID == getID() && data.ip == triggerObjectIP)
     {
-        // TODO: Check IP as well?
         wasTriggeredByLastUpdate = TRIGGER_OCCURRED;
         return checkIfTriggered();
     }
