92397373cb0967e2ce968171956f25b5da86b77c
Lukas Wikander <lukas.wikander@astazero.com>
2019-10-07 15:37:27 +0200

    Added first crash test

---
+++ server/integration-tests/StartupAndKillMainExecutable.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+EXECDIR=../build
+EXECNAME=TEServer
+SLEEP_TIME_START=3
+SLEEP_TIME_KILL=1
+RESULT=0
+MIN_EXPECTED_PIDS=6
+
+# Start the main executable
+rm -rf /dev/mqueue/*
+$EXECDIR/$EXECNAME &
+sleep $SLEEP_TIME_START
+RUNNING_PIDS="$(pgrep ${EXECNAME} | wc -l)"
+if [ $RUNNING_PIDS -lt $MIN_EXPECTED_PIDS ]; then
+	echo "All processes not running after ${SLEEP_TIME_START} s"
+	RESULT=1
+fi
+
+killall -s SIGINT $EXECNAME
+sleep $SLEEP_TIME_KILL
+RUNNING_PIDS="$(pgrep ${EXECNAME} | wc -l)"
+if [ $RUNNING_PIDS -ne 0 ]; then
+	echo "Failed to kill all processes via SIGINT. Still running:"
+	pgrep $EXECNAME
+	killall $EXECNAME
+	RESULT=1
+fi
+
+if [ $RESULT -eq 0 ]; then
+	echo "Success!"
+else
+	echo "Failure!"
+fi
+
+exit $RESULT
