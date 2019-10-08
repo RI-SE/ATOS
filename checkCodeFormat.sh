add92388a2566de6ed6155e39c4a2bcaf79f280d
Lukas Wikander <lukas.wikander@astazero.com>
2019-10-08 15:55:03 +0200

    Make code formatting script work

--- checkCodeFormat.sh
+++ checkCodeFormat.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
 if ! git diff-index --quiet HEAD --; then
-	echo "Then!"
+	echo "Uncommitted changes detected: please commit changes before checking code format"
+	exit 1
+fi
+
+./formatSourceFiles.sh
+git diff > formatDiff.patch
+# Todo: remove formatting diffs
+
+if [ ! -s formatDiff.patch ]; then
+	# It was empty
+	rm formatDiff.patch
+	echo "No code formatting errors detected"
+	exit 0
 else
-	echo "Else"
+	echo "Formatting errors detected:"
+	cat formatDiff.patch
+	rm formatDiff.patch
+	exit 1
 fi
