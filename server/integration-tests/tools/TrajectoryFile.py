b0c3c408d2739796e703e632a3924ad1477bebf5
Lukas Wikander <lukas.wikander@astazero.com>
2019-10-31 14:49:38 +0100

    Updated trajectory file reader

---
+++ server/integration-tests/tools/TrajectoryFile.py
@@ -0,0 +1,17 @@
+import random
+from os import listdir
+from os.path import isfile, join
+
+def ReadTrajectoryFile(trajectoryDir, fileName="random"):
+    allFiles = [f for f in listdir(trajectoryDir) if isfile(join(trajectoryDir, f))]
+    allFiles[:] = [f for f in allFiles if f[-5:] == ".traj"]
+    if fileName == "random":
+        fname = join(trajectoryDir, random.choice(allFiles))
+    elif join(trajectoryDir, fileName) in allFiles:
+        fname = join(trajectoryDir, fileName)
+    else:
+        raise ValueError("File " + fileName + " in directory " + trajectoryDir + " does not exist")
+    with open(fname, "r") as fp:
+        data = fp.read()    
+    return data
+
