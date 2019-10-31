1edffe1c63c701447236a4dc45304af5b938ee04
Lukas Wikander <lukas.wikander@astazero.com>
2019-10-31 10:29:34 +0100

    Started handling response codes, added timeout to waitForObjectControlState

--- server/integration-tests/tools/MSCP.py
+++ server/integration-tests/tools/MSCP.py
@@ -20,18 +20,19 @@ class MSCP:
         self.lastAbortReply["scenarioActive"] = 0
         self.lastStartReply["scenarioActive"] = 0
         self.quit = False
+        self.lastResponseCode = "No message received yet"
         self.listener = threading.Thread(target=self.listen)
         self.listener.start()
 
     def listen(self):
         replyPatterns = [
-                {"command": "init", "regex": re.compile(b'InitializeScenario:')},
-                {"command": "status", "regex": re.compile(b'GetServerStatus:(.)(.)(.)?(.)?')},
-                {"command": "abort", "regex": re.compile(b'AbortScenario:(.)')},
-                {"command": "arm", "regex": re.compile(b'ArmScenario:')},
-                {"command": "start", "regex": re.compile(b'StartScenario:(.)')},
-                {"command": "connect", "regex": re.compile(b'ConnectObject:')},
-                {"command": "disconnect", "regex": re.compile(b'DisconnectObject:')}
+                {"command": "init", "regex": re.compile(b'(..)InitializeScenario:')},
+                {"command": "status", "regex": re.compile(b'(..)GetServerStatus:(.)(.)(.)?(.)?')},
+                {"command": "abort", "regex": re.compile(b'(..)AbortScenario:(.)')},
+                {"command": "arm", "regex": re.compile(b'(..)ArmScenario:')},
+                {"command": "start", "regex": re.compile(b'(..)StartScenario:(.)')},
+                {"command": "connect", "regex": re.compile(b'(..)ConnectObject:')},
+                {"command": "disconnect", "regex": re.compile(b'(..)DisconnectObject:')}
         ]
 
         while not self.quit:
@@ -40,13 +41,14 @@ class MSCP:
                 match = re.search(replyPattern["regex"],data)
                 if match is not None:
                     matchPattern = replyPattern
+                    self.lastResponseCode = self.interpretResponseCode(match.group(1))
                     break
             if match is not None:
                 if matchPattern["command"] == "init":
                     print("Init reply received")
                 if matchPattern["command"] == "status":
                     print("Status reply received")
-                    num = int.from_bytes(match.group(1),byteorder='big')
+                    num = int.from_bytes(match.group(2),byteorder='big')
                     if num == 1:
                         self.lastStatusReply["systemControlState"] = "INITIALIZED"
                     elif num == 2:
@@ -58,7 +60,7 @@ class MSCP:
                     else:
                         self.lastStatusReply["systemControlState"] = "UNKNOWN"
 
-                    num = int.from_bytes(match.group(2),byteorder='big')
+                    num = int.from_bytes(match.group(3),byteorder='big')
                     if num == 1:
                         self.lastStatusReply["objectControlState"] = "IDLE"
                     elif num == 2:
@@ -75,16 +77,16 @@ class MSCP:
                         self.lastStatusReply["objectControlState"] = "UNKNOWN"
 
                     if match.group(3) is not None:
-                        self.lastStatusReply["systemControlErrorCode"] = int.from_bytes(match.group(3),byteorder='big')
+                        self.lastStatusReply["systemControlErrorCode"] = int.from_bytes(match.group(4),byteorder='big')
                     else:
                         self.lastStatusReply["systemControlErrorCode"] = 0
                     if match.group(4) is not None:
-                        self.lastStatusReply["objectControlErrorCode"] = int.from_bytes(match.group(4),byteorder='big')
+                        self.lastStatusReply["objectControlErrorCode"] = int.from_bytes(match.group(5),byteorder='big')
                     else:
                         self.lastStatusReply["objectControlErrorCode"] = 0
                 if matchPattern["command"] == "abort":
                     print("Abort reply received")
-                    num = int.from_bytes(match.group(1),byteorder='big')
+                    num = int.from_bytes(match.group(2),byteorder='big')
                     if num == 0:
                         self.lastAbortReply["scenarioActive"] = "NOT_ACTIVE"
                     elif num == 1:
@@ -95,7 +97,7 @@ class MSCP:
                     print("Arm reply received")
                 if matchPattern["command"] == "start":
                     print("Start reply received")
-                    num = int.from_bytes(match.group(1),byteorder='big')
+                    num = int.from_bytes(match.group(2),byteorder='big')
                     if num == 0:
                         self.lastStartReply["scenarioActive"] = "NOT_ACTIVE"
                     elif num == 1:
@@ -150,10 +152,42 @@ class MSCP:
         self.quit = True
         self.socket.close()
 
-    def waitForObjectControlState(self,state):
-        while self.lastStatusReply["objectControlState"] != state:
+    def waitForObjectControlState(self,state,timeout=3.0):
+        timeoutTime = time.time() + timeout
+        while self.lastStatusReply["objectControlState"] != state and time.time() < timeoutTime:
             time.sleep(0.005)
             print("Expecting: " + state + ", Current: " + self.lastStatusReply["objectControlState"])
             self.GetStatus()
         print("Expecting: " + state + ", Current: " + self.lastStatusReply["objectControlState"])
+        if self.lastStatusReply["objectControlState"] != state:
+            raise TimeoutError("Timed out while waiting for transition to " + state)
+
+    def interpretResponseCode(self,code):
+        num = int.from_bytes(code,byteorder='big')
+        if num == 0x0001:
+            return "OK"
+        elif num == 0x0002:
+            return "Logging data"
+        elif num = 0x0F10:
+            return "Error"
+        elif num = 0x0F20:
+            return "Function not available"
+        elif num = 0x0F25:
+            return "Incorrect state"
+        elif num = 0x0F30:
+            return "Invalid length"
+        elif num = 0x0F40:
+            return "Busy"
+        elif num = 0x0F42:
+            return "Timeout"
+        elif num = 0x0F50:
+            return "Invalid script"
+        elif num = 0x0F60:
+            return "Invalid encryption code"
+        elif num = 0x0F61:
+            return "Decryption error"
+        elif num = 0x0F62:
+            return "No data"
+        else:
+            raise ValueError("Response code " + str(num) " is not recognized")
 
