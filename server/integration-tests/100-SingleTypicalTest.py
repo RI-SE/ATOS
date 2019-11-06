6a26f85d8b887c62c996efad2b9f436b9e1213a7
Lukas Wikander <lukas.wikander@astazero.com>
2019-11-06 18:05:56 +0100

    Added step of test which starts object

--- server/integration-tests/100-SingleTypicalTest.py
+++ server/integration-tests/100-SingleTypicalTest.py
@@ -1,13 +1,15 @@
 from tools.MSCP import MSCP
 from tools.Executable import Executable
 from tools.TrajectoryFile import *
+from tools.PortChecker import *
 import time
 import subprocess
 import sys
 
 if __name__ == "__main__":
 
-    
+    # Note: server does not close sockets properly so this fails frequently (cross fingers for now):
+    #WaitForPortAvailable(54241,"TCP",timeout=0)
     server = Executable("../build/TEServer",["-m","0"])
     time.sleep(0.05)
     
@@ -18,10 +20,15 @@ if __name__ == "__main__":
         print("Connecting to the server caused a problem")
         server.stop()
         sys.exit(1)
+
     # 2: Load trajectory
     traj = ReadTrajectoryFile("resources/trajectories/",fileName="random")
 
     # 3: Start a test object
+    WaitForPortAvailable(53240,"UDP",timeout=0)
+    WaitForPortAvailable(53241,"TCP",timeout=0)
+    obj = Executable("VirtualObject",["-nogui"])
+    
     # 4: Upload trajectory
     userControl.UploadFile("traj/127.0.0.1",traj)
 
@@ -76,4 +83,5 @@ if __name__ == "__main__":
     # 11: Done!
     userControl.shutdown()
     server.stop()
+    obj.stop()
     sys.exit(0)
