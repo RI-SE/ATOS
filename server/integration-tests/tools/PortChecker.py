662077344ad4b5b7738c41a8c48faba4136ed740
Lukas Wikander <lukas.wikander@astazero.com>
2019-11-06 18:05:30 +0100

    Made a tool for checking if a port is available before starting a process

---
+++ server/integration-tests/tools/PortChecker.py
@@ -0,0 +1,30 @@
+import subprocess, signal
+import time
+import socket
+import sys
+
+host = ''
+
+def WaitForPortAvailable(port,protocol,timeout=5):
+    if protocol == "UDP":
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    elif protocol == "TCP":
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    else:
+        raise ValueError("The only supported network protocols are TCP and UDP")
+    
+    hasPrinted = False
+    timeoutTime = time.time() + timeout
+    while True:
+        try:
+            s.bind((host,port))
+            s.close()
+            return
+        except OSError:
+            if not hasPrinted:
+                print("=== Port " + str(port) + " occupied, waiting for " + str(timeout) + " seconds")
+                hasPrinted = True
+            time.sleep(0.25)
+            if time.time() > timeoutTime:
+                break
+    raise TimeoutError("Timed out while waiting for port " + str(port) + " to become available")
