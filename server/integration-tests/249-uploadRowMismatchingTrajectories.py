d293bc7827ff217bc50b7e7269f686cbc72018e5
Lukas Wikander <lukas.wikander@astazero.com>
2019-12-04 10:31:56 +0100

    Added missing parts of test

--- server/integration-tests/249-uploadRowMismatchingTrajectories.py
+++ server/integration-tests/249-uploadRowMismatchingTrajectories.py
@@ -44,7 +44,8 @@ if __name__ == "__main__":
     # 2: Load trajectory
     fewRowTraj = ReadTrajectoryFile("resources/trajectories/faulty",fileName="GarageplanInnerring_lessRowsThanSpecified.traj")
     manyRowTraj = ReadTrajectoryFile("resources/trajectories/faulty",fileName="GarageplanInnerring_moreRowsThanSpecified.traj")
-    
+    normalTraj = ReadTrajectoryFile("resources/trajectories")
+
     # 4: Upload short trajectory
     userControl.UploadFile("traj/127.0.0.1", fewRowTraj)
 
@@ -58,4 +59,42 @@ if __name__ == "__main__":
     except TimeoutError as e:
         # If there was a timeout while waiting for initialized that means everything went as intended
         pass
+
+    # 6: Upload normal trajectory, to verify we can still initialise
+    userControl.UploadFile("traj/127.0.0.1", normalTraj)
+
+    userControl.Init()
+    userControl.waitForObjectControlState("INITIALIZED")
+
+    userControl.Disconnect()
+    userControl.waitForObjectControlState("IDLE")
+
+    # 7: Upload long trajectory
+    userControl.UploadFile("traj/127.0.0.1", manyRowTraj)
+
+    # 8: Send init
+    try:
+        userControl.Init()
+        time.sleep(0.05)
+        checkProgramStatus("=== Sending init to the server after uploading trajectory with more rows than specified caused a problem")
+        userControl.waitForObjectControlState("INITIALIZED", timeout=0.5)
+        raise AssertionError("Transitioned to initialized even though malformed trajectory was uploaded")
+    except TimeoutError as e:
+        # If there was a timeout while waiting for initialized that means everything went as intended
+        pass
+
+    # 9: Upload normal trajectory, to verify we can still initialise
+    userControl.UploadFile("traj/127.0.0.1", normalTraj)
+
+    userControl.Init()
+    userControl.waitForObjectControlState("INITIALIZED")
+
+    userControl.Disconnect()
+    userControl.waitForObjectControlState("IDLE")
+
+    # 10: Done!
+    userControl.shutdown()
+    server.stop()
+    obj.stop()
+    sys.exit(0)
 
