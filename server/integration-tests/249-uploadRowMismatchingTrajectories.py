cf9063349a8262c6acc8513d4ea384e7586e6efc
Lukas Wikander <lukas.wikander@astazero.com>
2020-01-15 13:43:43 +0100

    Added a finally block which shuts down test components

--- server/integration-tests/249-uploadRowMismatchingTrajectories.py
+++ server/integration-tests/249-uploadRowMismatchingTrajectories.py
@@ -25,93 +25,81 @@ def checkProgramStatus(failurePrintout):
 
 if __name__ == "__main__":
 
-
-    # Note: server does not close sockets properly so this fails frequently (cross fingers for now):
-    #WaitForPortAvailable(54241,"TCP",timeout=0)
-    server = Executable("../build/TEServer",["-m","0"])
-    time.sleep(0.05)
-    checkProgramStatus("=== Starting the server caused a problem")
-    
-    # 1: Connect to the server
-    userControl = MSCP("127.0.0.1")
-    time.sleep(0.25)
-    checkProgramStatus("=== Connecting to the server caused a problem")
-
-    # 2: Load trajectory
-    fewRowTraj = ReadTrajectoryFile("resources/trajectories/faulty",fileName="GarageplanInnerring_lessRowsThanSpecified.traj")
-    manyRowTraj = ReadTrajectoryFile("resources/trajectories/faulty",fileName="GarageplanInnerring_moreRowsThanSpecified.traj")
-    normalTraj = ReadTrajectoryFile("resources/trajectories")
-
-    # 4: Upload short trajectory
     try:
+        # Note: server does not close sockets properly so this fails frequently (cross fingers for now):
+        #WaitForPortAvailable(54241,"TCP",timeout=0)
+        server = Executable("../build/TEServer",["-m","0"])
+        time.sleep(0.05)
+        checkProgramStatus("=== Starting the server caused a problem")
+        
+        # 1: Connect to the server
+        userControl = MSCP("127.0.0.1")
+        time.sleep(0.25)
+        checkProgramStatus("=== Connecting to the server caused a problem")
+
+        # 2: Load trajectory
+        fewRowTraj = ReadTrajectoryFile("resources/trajectories/faulty",fileName="GarageplanInnerring_lessRowsThanSpecified.traj")
+        manyRowTraj = ReadTrajectoryFile("resources/trajectories/faulty",fileName="GarageplanInnerring_moreRowsThanSpecified.traj")
+        normalTraj = ReadTrajectoryFile("resources/trajectories")
+
+        # 4: Upload short trajectory
         userControl.UploadFile("traj/127.0.0.1", fewRowTraj)
-    except TimeoutError as e:
-        print("tjenixen")
-        raise e
 
-    # 5: Send init
-    try:
-        userControl.Init()
-        time.sleep(0.05)
-        checkProgramStatus("=== Sending init to the server after uploading trajectory with less rows than specified caused a problem")
-        userControl.waitForObjectControlState("INITIALIZED", timeout=0.5)
-        raise AssertionError("Transitioned to initialized even though malformed trajectory was uploaded")
-    except TimeoutError as e:
-        # If there was a timeout while waiting for initialized that means everything went as intended
-        print("=== Timed out successfully while waiting for initialisation")
-    
-    time.sleep(0.05) 
-    # 6: Upload normal trajectory, to verify we can still initialise
-    try:
+        # 5: Send init
+        try:
+            userControl.Init()
+            time.sleep(0.05)
+            checkProgramStatus("=== Sending init to the server after uploading trajectory with less rows than specified caused a problem")
+            userControl.waitForObjectControlState("INITIALIZED", timeout=0.5)
+            raise AssertionError("Transitioned to initialized even though malformed trajectory was uploaded")
+        except TimeoutError as e:
+            # If there was a timeout while waiting for initialized that means everything went as intended
+            print("=== Timed out successfully while waiting for initialisation")
+        
+        time.sleep(0.05) 
+        
+        # 6: Upload normal trajectory, to verify we can still initialise
         userControl.UploadFile("traj/127.0.0.1", normalTraj)
-    except TimeoutError as e:
-        print("tjenixen")
-        raise e
 
-    userControl.Init()
-    userControl.waitForObjectControlState("INITIALIZED")
-    
-    time.sleep(0.05)
+        userControl.Init()
+        userControl.waitForObjectControlState("INITIALIZED")
+        
+        time.sleep(0.05)
 
-    userControl.Disconnect()
-    userControl.waitForObjectControlState("IDLE")
+        userControl.Disconnect()
+        userControl.waitForObjectControlState("IDLE")
 
-    # 7: Upload long trajectory
-    try:
+        # 7: Upload long trajectory
         userControl.UploadFile("traj/127.0.0.1", manyRowTraj)
-    except TimeoutError as e:
-        print("tjenixen")
-        raise e
 
-    # 8: Send init
-    try:
-        userControl.Init()
+        # 8: Send init
+        try:
+            userControl.Init()
+            time.sleep(0.05)
+            checkProgramStatus("=== Sending init to the server after uploading trajectory with more rows than specified caused a problem")
+            userControl.waitForObjectControlState("INITIALIZED", timeout=0.5)
+            raise AssertionError("Transitioned to initialized even though malformed trajectory was uploaded")
+        except TimeoutError as e:
+            # If there was a timeout while waiting for initialized that means everything went as intended
+            print("=== Timed out successfully while waiting for initialisation")
+
         time.sleep(0.05)
-        checkProgramStatus("=== Sending init to the server after uploading trajectory with more rows than specified caused a problem")
-        userControl.waitForObjectControlState("INITIALIZED", timeout=0.5)
-        raise AssertionError("Transitioned to initialized even though malformed trajectory was uploaded")
-    except TimeoutError as e:
-        # If there was a timeout while waiting for initialized that means everything went as intended
-        print("=== Timed out successfully while waiting for initialisation")
-
-    time.sleep(0.05)
-    # 9: Upload normal trajectory, to verify we can still initialise
-    try:
+        
+        # 9: Upload normal trajectory, to verify we can still initialise
         userControl.UploadFile("traj/127.0.0.1", normalTraj)
-    except TimeoutError as e:
-        print("tjenixen")
-        raise e
 
-    userControl.Init()
-    userControl.waitForObjectControlState("INITIALIZED")
+        userControl.Init()
+        userControl.waitForObjectControlState("INITIALIZED")
 
-    time.sleep(0.05)
+        time.sleep(0.05)
 
-    userControl.Disconnect()
-    userControl.waitForObjectControlState("IDLE")
+        userControl.Disconnect()
+        userControl.waitForObjectControlState("IDLE")
 
-    # 10: Done!
-    userControl.shutdown()
-    server.stop()
+    finally:
+        # 10: Done!
+        userControl.shutdown()
+        server.stop()
+    
     sys.exit(0)
 
