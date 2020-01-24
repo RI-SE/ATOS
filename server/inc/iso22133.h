e999cb95921228c152a0396777a17f5ef50544a4
Lukas Wikander <lukas.wikander@astazero.com>
2020-01-24 16:08:11 +0100

    Implemented HEAB encoder function in iso22133

--- server/inc/iso22133.h
+++ server/inc/iso22133.h
@@ -140,7 +140,31 @@ typedef struct
 #define VALUE_ID_STRT_GPS_WEEK 0x0003
 
 
+//! *************************** HEAB
+#define HEAB_FREQUENCY_HZ 100
+typedef struct
+{
+  HeaderType header;
+  uint16_t HEABStructValueID;
+  uint16_t HEABStructContentLength;
+  uint32_t GPSQmsOfWeek;
+  uint8_t controlCenterStatus;
+  FooterType footer;
+} HEABType; //16 bytes
+
+//! HEAB value IDs
+#define VALUE_ID_HEAB_STRUCT 0x0090
+typedef enum {
+	CONTROL_CENTER_STATUS_INIT = 0x00,
+	CONTROL_CENTER_STATUS_READY = 0x01,
+	CONTROL_CENTER_STATUS_ABORT = 0x02,
+	CONTROL_CENTER_STATUS_RUNNING = 0x03,
+	CONTROL_CENTER_STATUS_TEST_DONE = 0x04,
+	CONTROL_CENTER_STATUS_NORMAL_STOP = 0x05
+} ControlCenterStatusType;
+
 //! *************************** MONR
+#define MONR_EXPECTED_FREQUENCY_HZ 100
 typedef struct
 {
 	HeaderType header;
@@ -403,6 +427,7 @@ ISOMessageReturnValue decodeMONRMessage(const char * MonrData, const size_t leng
 ssize_t encodeSTRTMessage(const struct timeval* timeOfStart, char * strtDataBuffer, const size_t bufferLength, const char debug);
 ssize_t encodeOSEMMessage(const double * latitude_deg, const double * longitude_deg, const float * altitude_m, const float * maxPositionDeviation_m, const float * maxLateralDeviation_m, const float * minimumPositioningAccuracy_m, char * osemDataBuffer, const size_t bufferLength, const char debug);
 ssize_t encodeOSTMMessage(const ObjectCommandType command, char * ostmDataBuffer, const size_t bufferLength, const char debug);
+ssize_t encodeHEABMessage(const ControlCenterStatusType status, char * heabDataBuffer, const size_t bufferLength, const char debug);
 ISOMessageReturnValue MONRToASCII(const MONRType * MONRData, char * asciiBuffer, const size_t bufferLength, const char debug);
 ISOMessageReturnValue ASCIIToMONR(const char * asciiBuffer, MONRType * MONRData, const char debug);
 ISOMessageID getISOMessageType(const char * messageData, const size_t length, const char debug);
