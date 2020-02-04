5b737a1bfbae13b242037c22f42107ff21d54a8f
Lukas Wikander <lukas.wikander@astazero.com>
2020-02-04 13:21:01 +0100

    Commented all the typedefs

--- server/inc/positioning.h
+++ server/inc/positioning.h
@@ -8,6 +8,7 @@ extern "C" {
 #include <sys/time.h>
 #include <stdbool.h>
 
+/*! Struct describing a position and orientation in a local cartesian coordinate system */
 typedef struct {
 	double xCoord_m;
 	double yCoord_m;
@@ -17,24 +18,28 @@ typedef struct {
 	bool isHeadingValid;
 } CartesianPosition; // TODO: rename
 
+/*! Struct describing longitudinal and lateral speed of an object */
 typedef struct {
 	double longitudinal_m_s;
 	double lateral_m_s;
 	bool isValid;
 } SpeedType;
 
+/*! Struct describing longitudinal and lateral acceleration of an object */
 typedef struct {
 	double longitudinal_m_s2;
 	double lateral_m_s2;
 	bool isValid;
 } AccelerationType;
 
+/*! Enumeration of object driving direction descriptions */
 typedef enum {
 	OBJECT_DRIVE_DIRECTION_FORWARD,
 	OBJECT_DRIVE_DIRECTION_BACKWARD,
 	OBJECT_DRIVE_DIRECTION_UNAVAILABLE
 } DriveDirectionType;
 
+/*! Enumeration of observable object states */
 typedef enum {
 	OBJECT_STATE_UNKNOWN,
 	OBJECT_STATE_DISARMED,
@@ -45,12 +50,14 @@ typedef enum {
 	OBJECT_STATE_REMOTE_CONTROL
 } ObjectStateType;
 
+/*! Enumeration of ready to arm statuses of an object */
 typedef enum {
 	OBJECT_NOT_READY_TO_ARM,
 	OBJECT_READY_TO_ARM,
 	OBJECT_READY_TO_ARM_UNAVAILABLE
 } ObjectArmReadinessType;
 
+/*! Struct describing a range of possible errors reported by an object */
 typedef struct {
 	bool abortRequest;
 	bool outsideGeofence;
@@ -61,6 +68,7 @@ typedef struct {
 	bool unknownError;
 } ObjectErrorType;
 
+/*! Struct containing measured object data from a single point in time */
 typedef struct {
 	bool isTimestampValid;
 	struct timeval timestamp;
