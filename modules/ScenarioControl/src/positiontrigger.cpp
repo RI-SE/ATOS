cba775b306ca8033a23ede7328110357f21942d6
Lukas Wikander <lukas.wikander@astazero.com>
2020-04-28 10:07:25 +0200

    Added an abstract position trigger class

---
+++ modules/ScenarioControl/src/positiontrigger.cpp
@@ -0,0 +1,18 @@
+#include "positiontrigger.h"
+/*!
+ * \brief PositionTrigger::update Updates the tracked signal (i.e. which causes the trigger) to the value specified
+ * \param
+ * \param Unused time of measurement
+ * \return Value according to ::TriggerReturnCode_t
+ */
+Trigger::TriggerReturnCode_t PositionTrigger::update(bool CHANGE_THIS, struct timeval) {
+	// TODO
+}
+
+/*!
+ * \brief PositionTrigger::checkIfTriggered TODO
+ * \return Value according to ::TriggerReturnCode_t
+ */
+Trigger::TriggerReturnCode_t PositionTrigger::checkIfTriggered() const {
+	// TODO
+}
