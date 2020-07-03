35f0d3a33767cb7d70db319c7c853ea84522b326
Lukas Wikander <lukas.wikander@astazero.com>
2020-07-03 20:40:33 +0200

    Clarified printout

--- core/src/objectcontrol.c
+++ core/src/objectcontrol.c
@@ -445,8 +445,9 @@ void objectcontrol_task(TimeType * GPSTime, GSDType * GSD, LOG_LEVEL logLevel) {
 					uint32_t localTransmitterID;
 					if (DataDictionaryGetObjectTransmitterIDByIP(monitorData.ClientIP, &localTransmitterID) == READ_OK) {
 						if (monitorData.ClientID != localTransmitterID) {
+							LogMessage(LOG_LEVEL_WARNING, "Modifying local transmitter ID %u to %u in lieu of ability to set remote",
+									   localTransmitterID, monitorData.ClientID);
 							DataDictionaryModifyTransmitterID(localTransmitterID, monitorData.ClientID);
-							LogMessage(LOG_LEVEL_WARNING, "Modified local transmitter ID in lieu of ability to set it");
 						}
 					}
 					else {
