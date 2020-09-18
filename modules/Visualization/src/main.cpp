5ff445965d702e809faad00d20afa8db3016cbdf
eriadam36 <adameriksson36@gmail.com>
2020-09-18 15:02:10 +0200

    started adding a udp channel to vislaizer for monr

--- modules/Visualization/src/main.cpp
+++ modules/Visualization/src/main.cpp
@@ -9,16 +9,16 @@
 #include "datadictionary.h"
 #include "tcphandler.h"
 #include "iso22133.h"
-
+#include "udphandler.h"
 
 /*------------------------------------------------------------
   -- Defines
   ------------------------------------------------------------*/
 #define MODULE_NAME "Visualization"
 #define RECV_MESSAGE_BUFFER 1024
-#define MONR_BUFFER_LENGTH  1024
-#define VISUAL_SERVER_PORT  53250
-
+#define MONR_BUFFER_LENGTH 1024
+#define TCP_VISUAL_SERVER_PORT 53250
+#define UDP_VISUAL_SERVER_PORT 53251 
 /*------------------------------------------------------------
   -- Static variables
   ------------------------------------------------------------*/
@@ -40,15 +40,19 @@ static void signalHandler(int signo);
 
 int main(int argc, char const* argv[]){
 
-	std::vector<char> TCPBuffer(MONR_BUFFER_LENGTH);
+	//std::vector<char> TCPBuffer(MONR_BUFFER_LENGTH);
+	std::vector<char> UDPBuffer(MONR_BUFFER_LENGTH);
 	std::vector<uint32_t> transmitterids(MONR_BUFFER_LENGTH);
 	std::vector<char> chekingTCPconn(MONR_BUFFER_LENGTH);
 	ObjectMonitorType monitorData;
 	std::string IPaddr;
+	
 	COMMAND command = COMM_INV;
+	COMMAND SendLater= COMM_INV;
+
     char mqRecvData[MQ_MSG_SIZE];
 	const struct timespec sleepTimePeriod = {0,10000000};
-	const struct timespec sleepTimeTCP = {0,999999999};
+	
 	struct timespec remTime;
 	struct timeval monitorDataReceiveTime;
 	uint32_t transmitterId;
@@ -63,6 +67,7 @@ int main(int argc, char const* argv[]){
 	unsigned long flagsock = O_NONBLOCK;
 	int OnTCP = 0;	
 	LogInit(MODULE_NAME,LOG_LEVEL_DEBUG);
+
 	LogMessage(LOG_LEVEL_INFO, "Task running with PID: %u",getpid());
 
 	// Set up signal handlers
@@ -87,13 +92,13 @@ int main(int argc, char const* argv[]){
 	std::cout << "Awaiting TCP connection..." << std::endl;
 	while(!quit){
 		
-		TCPHandler ServerVisualizer(VISUAL_SERVER_PORT, "", "Server", 1, O_NONBLOCK);
-		OnTCP = ServerVisualizer.ConnectionON;
-		
+		TCPHandler TCPServerVisualizer(TCP_VISUAL_SERVER_PORT, "", "Server", 1, O_NONBLOCK);
+		UDPHandler UDPServerVisualizer (UDP_VISUAL_SERVER_PORT,"",0,"Server");
+		OnTCP = TCPServerVisualizer.ConnectionON;
 		while(OnTCP <= 0)
 		{
-			ServerVisualizer.TCPHandlerAccept();
-			OnTCP = ServerVisualizer.ConnectionON;
+			TCPServerVisualizer.TCPHandlerAccept(5);
+			OnTCP = TCPServerVisualizer.ConnectionON;
 			if (OnTCP<0){
 				break;
 			}
@@ -102,41 +107,81 @@ int main(int argc, char const* argv[]){
 			{
 				util_error("Message bus receive error");
 			}
+			switch (command)
+			{
+			case COMM_CONNECT:
+				SendLater = COMM_CONNECT; 
+				break;
 			
+			default:
+				break;
+			}
 
 		}
 		
 		
 		std::cout<<"Connection recived"<<std::endl;
+		if (SendLater == COMM_CONNECT)
+		{
+			std::cout <<"Sending trajectory to visualizer"<<std::endl;
+			// TO DO: swend traj with tcp
+		}
+		
 		while(bytesread >= 0 && OnTCP > 0){
 
+			
 			if (iCommRecv(&command,mqRecvData, MQ_MSG_SIZE,nullptr) < 0)
 			{
             	util_error("Message bus receive error");
         	}
-			bytesread = ServerVisualizer.receiveTCP(chekingTCPconn, 0);
+			switch (command)
+			{
+			case COMM_CONNECT:
+				//TODO send traj
+				break;
+			
+			default:
+				break;
+			}
+
+			if (command ==COMM_CONNECT)
+			{
+				//TO DO add so it sends traj with tcp to ar vis 				
+			}
+			
+			bytesread = TCPServerVisualizer.receiveTCP(chekingTCPconn, 0);
+			
 			DataDictionaryGetNumberOfObjects(&nOBJ);
 			DataDictionaryGetObjectTransmitterIDs(transmitterids.data(),transmitterids.size());
 			transmitterids.resize(nOBJ);
+			
 			for (auto &transmitterID : transmitterids) {
-				std::cout<<"transmitterid"<<transmitterID<<std::endl;
+			
 				if (transmitterID <= 0){
 					break;
 				}
+			
 				DataDictionaryGetMonitorData(transmitterID, &monitorData);
-				DataDictionaryGetMonitorDataReceiveTime(transmitterID, &monitorDataReceiveTime);
-				if (monitorDataReceiveTime.tv_sec > 0&&monitorData.position.isPositionValid && monitorData.position.isHeadingValid) { // this dose not seam to work, it looks like the datastructs are initlized before data is comming from virtual obj
+				DataDictionaryGetMonitorDataReceiveTime(transmitterID, &monitorDataReceiveTime);// i am getting duplicates from this one
+			
+				if (monitorDataReceiveTime.tv_sec > 0&& monitorData.position.isPositionValid && monitorData.position.isHeadingValid) {
+					// get transmitterid and encode monr to iso.
 					isoTransmitterID = (uint8_t) transmitterID;
 					setTransmitterID(isoTransmitterID);
-					long retval = encodeMONRMessage(&monitorData.timestamp,monitorData.position,monitorData.speed,monitorData.acceleration,monitorData.drivingDirection,monitorData.state, monitorData.armReadiness, objectErrorState,TCPBuffer.data(),TCPBuffer.size(),debug);
-					TCPBuffer.resize(static_cast<unsigned long>(retval));
-					bytesread = ServerVisualizer.receiveTCP(chekingTCPconn, 0);
+					long retval = encodeMONRMessage(&monitorData.timestamp,monitorData.position,monitorData.speed,monitorData.acceleration,monitorData.drivingDirection,monitorData.state, monitorData.armReadiness, objectErrorState,UDPBuffer.data(), UDPBuffer.size(),debug);
+					std::cout << monitorData.position.yCoord_m<<std::endl;
+					UDPBuffer.resize(static_cast<unsigned long>(retval));
+					// Cheking so we still connected to visualizer
+
+					bytesread = TCPServerVisualizer.receiveTCP(chekingTCPconn, 0);
 					if (bytesread < 0) {
 						std::cout<<"Error when reading from Maestro TCP socket"<<std::endl;
 						break;
 
            			}
-					bytesent = ServerVisualizer.sendTCP(TCPBuffer);	
+					//n sending message with udp.
+
+					bytesent = UDPServerVisualizer.sendUDP(UDPBuffer);	
 				}
 				
 			}
@@ -148,7 +193,8 @@ int main(int argc, char const* argv[]){
         std::cout << "Awaiting TCP connection..." << std::endl;
 		OnTCP = 0;
 		bytesread = 1;
-        ServerVisualizer.TCPHandlerclose();
+        TCPServerVisualizer.TCPHandlerclose();
+		UDPServerVisualizer.UDPHandlerclose();
 	}
 
 	//ServerVisualizer.TCPHandlerclose();
