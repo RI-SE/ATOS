4f814301186e562fc4ed33657fdf5c01f78ce84c
Lukas Wikander <lukas.wikander@astazero.com>
2020-09-21 11:22:27 +0200

    Expanded OSEM info printout

--- core/src/objectcontrol.c
+++ core/src/objectcontrol.c
@@ -1628,7 +1628,7 @@ int configureAllObjects(ObjectConnection objectConnections[],
 				break;
 			}
 
-			LogMessage(LOG_LEVEL_INFO, "Sending OSEM");
+			LogMessage(LOG_LEVEL_INFO, "Sending OSEM to object ID %u with IP %s", transmitterIDs[i], ipString);
 
 			DataDictionaryGetTransmitterID(&serverTransmitterID);
 			isoTransmitterID = (uint8_t) serverTransmitterID;
