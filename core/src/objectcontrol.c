dd09207d6d83bee14b11f5bc9ed72f204cfc0936
Lukas Wikander <lukas.wikander@astazero.com>
2020-09-21 13:31:48 +0200

    Ran code formatter

--- core/src/objectcontrol.c
+++ core/src/objectcontrol.c
@@ -1628,7 +1628,8 @@ int configureAllObjects(ObjectConnection objectConnections[],
 				break;
 			}
 
-			LogMessage(LOG_LEVEL_INFO, "Sending OSEM to object ID %u with IP %s", transmitterIDs[i], ipString);
+			LogMessage(LOG_LEVEL_INFO, "Sending OSEM to object ID %u with IP %s", transmitterIDs[i],
+					   ipString);
 
 			DataDictionaryGetTransmitterID(&serverTransmitterID);
 			isoTransmitterID = (uint8_t) serverTransmitterID;
