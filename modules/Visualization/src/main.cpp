a4bda15842b926d0afad73ef680c43c7d81425b6
eriadam36 <adameriksson36@gmail.com>
2020-09-21 16:26:56 +0200

    adding traj to tcp send

--- modules/Visualization/src/main.cpp
+++ modules/Visualization/src/main.cpp
@@ -3,6 +3,8 @@
 #include <fstream>
 #include <sstream>
 #include <string>
+#include <iostream>
+#include <experimental/filesystem>
 
 #include "logging.h"
 #include "util.h"
@@ -19,6 +21,7 @@
 #define MONR_BUFFER_LENGTH 1024
 #define TCP_VISUAL_SERVER_PORT 53250
 #define UDP_VISUAL_SERVER_PORT 53251 
+#define TRAJECTORY_TX_BUFFER_SIZE 2048
 /*------------------------------------------------------------
   -- Static variables
   ------------------------------------------------------------*/
@@ -95,6 +98,7 @@ int main(int argc, char const* argv[]){
 		TCPHandler TCPServerVisualizer(TCP_VISUAL_SERVER_PORT, "", "Server", 1, O_NONBLOCK);
 		UDPHandler UDPServerVisualizer (UDP_VISUAL_SERVER_PORT,"",0,"Server");
 		OnTCP = TCPServerVisualizer.ConnectionON;
+		
 		while(OnTCP <= 0)
 		{
 			TCPServerVisualizer.TCPHandlerAccept(5);
@@ -120,13 +124,126 @@ int main(int argc, char const* argv[]){
 		}
 		
 		
+		
 		std::cout<<"Connection recived"<<std::endl;
+		/*
 		if (SendLater == COMM_CONNECT)
 		{
+			//TO DO: check if transmitterid's are bound to ip in datadictonary
+			
 			std::cout <<"Sending trajectory to visualizer"<<std::endl;
-			// TO DO: swend traj with tcp
-		}
+			TrajectoryFileHeader fileHeader;
+			TrajectoryFileLine fileLine;
+			char trajPathDir[MAX_FILE_PATH];
 		
+			UtilGetTrajDirectoryPath(trajPathDir,sizeof(trajPathDir));
+			std::string str(trajPathDir);
+			
+			for (auto& entry : std::experimental::filesystem::directory_iterator(trajPathDir)){
+				memset(&fileHeader, 0, sizeof (fileHeader));
+				memset(&fileLine, 0, sizeof (fileLine));
+				char messageBuffer[TRAJECTORY_TX_BUFFER_SIZE];
+				size_t remainingBufferSpace = sizeof (messageBuffer);
+				char *messageBufferPosition = messageBuffer;
+				
+				if (UtilCheckTrajectoryFileFormat(entry.path().string().c_str(), strlen(entry.path().string().c_str())) == -1) {
+					LogMessage(LOG_LEVEL_ERROR, "Incorrect trajectory file format - cannot proceed to send message");
+					break;
+				}
+
+				std::ifstream infile(entry.path().string());
+				std::string line;
+				std::getline(infile, line);
+				std::vector<char> cstr(line.c_str(), line.c_str() + line.size() + 1);
+				if(UtilParseTrajectoryFileHeader(cstr.data(), &fileHeader) == -1){
+					LogMessage(LOG_LEVEL_ERROR, "Failed to parse header of file <%s>", entry.path().string());
+					break;
+				}
+				
+				if ((encodeTRAJMessageHeader(fileHeader.ID > UINT16_MAX ? 0 : (uint16_t) fileHeader.ID,
+					fileHeader.majorVersion, fileHeader.name,
+					strlen(fileHeader.name), fileHeader.numberOfLines,
+					messageBufferPosition, remainingBufferSpace, debug)) == -1) {
+					LogMessage(LOG_LEVEL_ERROR, "Unable to encode trajectory message");
+					break;
+				}
+
+				while (std::getline(infile, line)){
+					std::vector<char> cstr(line.c_str(), line.c_str() + line.size() + 1);
+					struct timeval relTime;
+					CartesianPosition position;
+					SpeedType speed;
+					AccelerationType acceleration;
+					if (UtilParseTrajectoryFileLine(cstr.data(), &fileLine) == -1) {
+						// TODO: how to terminate an ISO message when an error has occurred?
+						LogMessage(LOG_LEVEL_ERROR, "Unable to parse line %u of trajectory file <%s>", i + 1, Filename);
+						break;
+					}
+					relTime.tv_sec = (time_t) fileLine.time;
+					relTime.tv_usec = (time_t) ((fileLine.time - relTime.tv_sec) * 1000000);
+					position.xCoord_m = fileLine.xCoord;
+					position.yCoord_m = fileLine.yCoord;
+					position.isPositionValid = fileLine.zCoord != NULL;
+					position.zCoord_m = position.isPositionValid ? *fileLine.zCoord : 0;
+					position.heading_rad = fileLine.heading;
+					position.isHeadingValid = true;
+					speed.isLongitudinalValid = fileLine.longitudinalVelocity != NULL;
+					speed.isLateralValid = fileLine.lateralVelocity != NULL;
+					speed.longitudinal_m_s = fileLine.longitudinalVelocity != NULL ? *fileLine.longitudinalVelocity : 0;
+					speed.lateral_m_s = fileLine.lateralVelocity != NULL ? *fileLine.lateralVelocity : 0;
+					acceleration.isLongitudinalValid = fileLine.longitudinalAcceleration != NULL;
+					acceleration.isLateralValid = fileLine.lateralAcceleration != NULL;
+					acceleration.longitudinal_m_s2 =
+						fileLine.longitudinalAcceleration != NULL ? *fileLine.longitudinalAcceleration : 0;
+
+					acceleration.lateral_m_s2 = fileLine.lateralAcceleration != NULL ? *fileLine.lateralAcceleration : 0;
+
+					// Print to buffer
+					if ((printedBytes = encodeTRAJMessagePoint(&relTime, position, speed, acceleration,
+															(float)fileLine.curvature, messageBufferPosition,
+															remainingBufferSpace, debug)) == -1) {
+
+						if (errno == ENOBUFS) {
+							// Reached the end of buffer, send buffered data and
+							// try again
+							UtilSendTCPData(MODULE_NAME, messageBuffer, messageBufferPosition - messageBuffer, Socket,
+											debug);
+
+							messageBufferPosition = messageBuffer;
+							remainingBufferSpace = sizeof (messageBuffer);
+							if ((printedBytes =
+								encodeTRAJMessagePoint(&relTime, position, speed, acceleration, fileLine.curvature,
+														messageBufferPosition, remainingBufferSpace, debug)) == -1) {
+								// TODO how to terminate an ISO message when an error has occurred?
+								LogMessage(LOG_LEVEL_ERROR, "Error encoding trajectory message point");
+								fclose(fd);
+								return -1;
+							}
+							messageBufferPosition += printedBytes;
+							totalPrintedBytes += printedBytes;
+							remainingBufferSpace -= (size_t) printedBytes;
+						}
+						else {
+							// TODO how to terminate an ISO message when an error has occurred?
+							LogMessage(LOG_LEVEL_ERROR, "Error encoding trajectory message point");
+							fclose(fd);
+							return -1;
+						}
+					}
+					else {
+						totalPrintedBytes += printedBytes;
+						messageBufferPosition += printedBytes;
+						remainingBufferSpace -= (size_t) printedBytes;
+					}
+
+
+				}
+				
+			}
+
+		}
+		*/
+
 		while(bytesread >= 0 && OnTCP > 0){
 
 			
@@ -154,7 +271,7 @@ int main(int argc, char const* argv[]){
 			DataDictionaryGetNumberOfObjects(&nOBJ);
 			DataDictionaryGetObjectTransmitterIDs(transmitterids.data(),transmitterids.size());
 			transmitterids.resize(nOBJ);
-			
+			std::cout <<nOBJ<<std::endl;
 			for (auto &transmitterID : transmitterids) {
 			
 				if (transmitterID <= 0){
@@ -163,7 +280,7 @@ int main(int argc, char const* argv[]){
 			
 				DataDictionaryGetMonitorData(transmitterID, &monitorData);
 				DataDictionaryGetMonitorDataReceiveTime(transmitterID, &monitorDataReceiveTime);// i am getting duplicates from this one
-			
+				std::cout<<monitorDataReceiveTime.tv_sec<< "\nposvalid "<<monitorData.position.isPositionValid <<"\nheadingvalid" <<monitorData.position.isHeadingValid <<std::endl;
 				if (monitorDataReceiveTime.tv_sec > 0&& monitorData.position.isPositionValid && monitorData.position.isHeadingValid) {
 					// get transmitterid and encode monr to iso.
 					isoTransmitterID = (uint8_t) transmitterID;
