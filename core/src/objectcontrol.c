1ec59aa9b5d82a3b6b0b806abb3bfa232e80f375
sepast <sebastian.lindholm@astazero.com>
2020-09-24 16:06:06 +0200

    Update core/src/objectcontrol.c
    Co-authored-by: Lukas <lukas.wikander@astazero.com>

--- core/src/objectcontrol.c
+++ core/src/objectcontrol.c
@@ -2173,8 +2173,8 @@ int sendDataInjectionMessages(const ObjectDataType * objectData,
 	for (unsigned int i = 0; i < relevantMap->numberOfTargets; ++i) {
 		for (unsigned int j = 0; j < numberOfObjects; j++) {
 			if (transmitterIDs[j] == relevantMap->targetIDs[i]) {
-				UtilSendUDPData(MODULE_NAME, &objectConnections[0].monitorSocket,
-									&objectConnections[0].objectMonitorAddress,
+				UtilSendUDPData(MODULE_NAME, &objectConnections[j].monitorSocket,
+									&objectConnections[j].objectMonitorAddress,
 									transmissionBuffer, messageSize, 0);
 			}
 		}
