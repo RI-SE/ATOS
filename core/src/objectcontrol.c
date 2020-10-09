dd2bf640838a4661a9019983b6df7300d57d2f1b
Lukas Wikander <lukas.wikander@astazero.com>
2020-10-09 12:10:25 +0200

    Ran code formatter

--- core/src/objectcontrol.c
+++ core/src/objectcontrol.c
@@ -447,7 +447,8 @@ void objectcontrol_task(TimeType * GPSTime, GSDType * GSD, LOG_LEVEL logLevel) {
 						timersub(&monitorData.lastDataUpdate, &monitorData.MonrData.timestamp,
 								 &monitorDataAge);
 						if (monitorDataAge.tv_sec || labs(monitorDataAge.tv_usec) > MAX_NETWORK_DELAY_USEC) {
-							LogMessage(LOG_LEVEL_WARNING, "Network delay from object %u exceeds 100 ms (%ld ms delay)",
+							LogMessage(LOG_LEVEL_WARNING,
+									   "Network delay from object %u exceeds 100 ms (%ld ms delay)",
 									   object_transmitter_ids[iIndex], TimeGetAsUTCms(&monitorDataAge));
 						}
 					}
