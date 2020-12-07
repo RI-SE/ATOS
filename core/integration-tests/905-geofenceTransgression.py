c19e6087f589d77dbcfa00e74800375b0c277a83
Lukas Wikander <lukas.wikander@astazero.com>
2020-12-07 15:17:51 +0100

    Moved files

--- core/integration-tests/905-geofenceTransgression.py
+++ core/integration-tests/905-geofenceTransgression.py
@@ -2,34 +2,34 @@ from tools.MSCP import MSCP
 from tools.Executable import Executable
 #from tools.GeofenceFile import *
 from tools.TrajectoryFile import *
+from tools.ObjectFile import *
 from tools.ISO import ISO, ISOObject
 import time
 import subprocess
 import sys
+import os
 
-#core = Executable("../../build/bin/Core",["-m","1"])
-#sup = Executable("../../build/bin/Supervision")
-#time.sleep(0.05)
-#mscp = MSCP("127.0.0.1")
-#time.sleep(0.25)
+core = Executable("../../build/bin/Core",["-m","1"])
+sup = Executable("../../build/bin/Supervision")
+time.sleep(0.05)
+mscp = MSCP("127.0.0.1")
+time.sleep(0.25)
+obj = ISOObject()
 
 def geofenceTransgressionTest():
     assert core.alive()
     assert sup.alive()
 
+    # 2: Load trajectory
+    testcases = [x[0] for x in os.walk("resources/geofence-tests")]
+    # TODO pick random test case
+    [traj,fileName] = ReadTrajectoryFile("/",fileName="random")
+    objID = random.randint(1,100)
+    objData = ConstructObjectFileData("127.0.0.1", fileName, objID)
+
 
 
 if __name__ == "__main__":
-    obj = ISOObject()
-    print("Sending a bunch of MONR")
-    while True:
-        try:
-            obj.MONR(position={'x':10.0,'y':15.0,'z':4.5})
-        except ConnectionError:
-            pass
-        time.sleep(0.01)
-    exit(1)
-    failed = False
     try:
         failed = geofenceTransgressionTest()
     finally:
