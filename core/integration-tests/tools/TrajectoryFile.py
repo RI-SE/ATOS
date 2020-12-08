9e03c8a7f9fb10e3b19091ec4e2e53fb7ac1540c
Lukas Wikander <lukas.wikander@astazero.com>
2020-12-08 10:33:46 +0100

    Added geofence file to tools

--- core/integration-tests/tools/TrajectoryFile.py
+++ core/integration-tests/tools/TrajectoryFile.py
@@ -3,19 +3,27 @@ from os import listdir
 from os.path import isfile, join
 
 def ReadTrajectoryFile(trajectoryDir, fileName="random"):
-    if trajectoryDir[-1] != '/':
-        trajectoryDir += '/'
-    allFiles = [f for f in listdir(trajectoryDir) if isfile(join(trajectoryDir, f))]
-    allFiles[:] = [f for f in allFiles if f[-5:] == ".traj"]
+    return ReadFile(trajectoryDir, fileName, ".traj")
+
+
+def ReadGeofenceFile(geofenceDir, fileName="random"):
+    return ReadFile(geofenceDir, fileName, ".geofence")
+
+
+def ReadFile(directory,fileName="random",fileEnding=".traj"):
+    if directory[-1] != '/':
+        directory += '/'
+    allFiles = [f for f in listdir(directory) if isfile(join(directory, f))]
+    allFiles[:] = [f for f in allFiles if f[-len(fileEnding):] == fileEnding]
     print(allFiles)
     if fileName == "random":
-        fname = join(trajectoryDir, random.choice(allFiles))
+        fname = join(directory, random.choice(allFiles))
     elif fileName in allFiles:
-        fname = join(trajectoryDir, fileName)
+        fname = join(directory, fileName)
     else:
-        raise ValueError("File " + fileName + " in directory " + trajectoryDir + " does not exist")
-    print("=== Reading trajectory file " + fname)
+        raise ValueError("File " + fileName + " in directory " + directory + " does not exist")
+    print("=== Reading " + fileEnding + " file " + fname)
     with open(fname, "r") as fp:
-        data = fp.read()    
+        data = fp.read()
     return [data, fname.split('/')[-1]]
 
