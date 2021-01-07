15c6fc32e9a98992fea49d272faff012abeec58b
Lukas Wikander <lukas.wikander@astazero.com>
2021-01-07 13:42:25 +0100

    Moved uninteresting function definitions to the bottom

--- core/src/journalcontrol.cpp
+++ core/src/journalcontrol.cpp
@@ -175,55 +175,6 @@ void journalcontrol_task(TimeType * GPSTime, GSDType * GSD, LOG_LEVEL logLevel) 
 }
 
 
-void signalHandler(int signo) {
-	if (signo == SIGINT) {
-		LogMessage(LOG_LEVEL_WARNING, "Caught keyboard interrupt");
-		quit = true;
-	}
-	else {
-		LogMessage(LOG_LEVEL_ERROR, "Caught unhandled signal");
-	}
-}
-
-/*!
- * \brief initializeModule Initializes this module by creating log, connecting to the message queue bus,
- *			setting up signal handers etc.
- * \param logLevel Level of the module log to be used.
- * \return 0 on success, -1 otherwise
- */
-int initializeModule(const LOG_LEVEL logLevel) {
-	int retval = 0;
-	struct timespec sleepTimePeriod, remTime;
-	sleepTimePeriod.tv_sec = 0;
-	sleepTimePeriod.tv_nsec = 1000000;
-	int maxRetries = 10, retryNumber;
-
-	// Initialize log
-	LogInit(MODULE_NAME, logLevel);
-	LogMessage(LOG_LEVEL_INFO, MODULE_NAME " task running with PID: %d", getpid());
-
-	// Set up signal handlers
-	LogMessage(LOG_LEVEL_DEBUG, "Initializing signal handler");
-	if (signal(SIGINT, signalHandler) == SIG_ERR) {
-		perror("signal");
-		retval = -1;
-		LogMessage(LOG_LEVEL_ERROR, "Unable to initialize signal handler");
-	}
-
-	// Initialize message bus connection
-	LogMessage(LOG_LEVEL_DEBUG, "Initializing connection to message bus");
-	for (retryNumber = 0; iCommInit() != 0 && retryNumber < maxRetries; ++retryNumber) {
-		nanosleep(&sleepTimePeriod, &remTime);
-	}
-	if (retryNumber == maxRetries) {
-		retval = -1;
-		LogMessage(LOG_LEVEL_ERROR, "Unable to initialize connection to message bus");
-	}
-
-	return retval;
-}
-
-
 /*!
  * \brief placeStartBookmarks Stores references to the current end of file of all journals.
  */
@@ -533,4 +484,53 @@ std::vector<fs::path> getJournalFilesFrom(const std::chrono::system_clock::time_
 		}
 	}
 	return journalsFromDate;
+}
+
+
+void signalHandler(int signo) {
+	if (signo == SIGINT) {
+		LogMessage(LOG_LEVEL_WARNING, "Caught keyboard interrupt");
+		quit = true;
+	}
+	else {
+		LogMessage(LOG_LEVEL_ERROR, "Caught unhandled signal");
+	}
+}
+
+/*!
+ * \brief initializeModule Initializes this module by creating log, connecting to the message queue bus,
+ *			setting up signal handers etc.
+ * \param logLevel Level of the module log to be used.
+ * \return 0 on success, -1 otherwise
+ */
+int initializeModule(const LOG_LEVEL logLevel) {
+	int retval = 0;
+	struct timespec sleepTimePeriod, remTime;
+	sleepTimePeriod.tv_sec = 0;
+	sleepTimePeriod.tv_nsec = 1000000;
+	int maxRetries = 10, retryNumber;
+
+	// Initialize log
+	LogInit(MODULE_NAME, logLevel);
+	LogMessage(LOG_LEVEL_INFO, MODULE_NAME " task running with PID: %d", getpid());
+
+	// Set up signal handlers
+	LogMessage(LOG_LEVEL_DEBUG, "Initializing signal handler");
+	if (signal(SIGINT, signalHandler) == SIG_ERR) {
+		perror("signal");
+		retval = -1;
+		LogMessage(LOG_LEVEL_ERROR, "Unable to initialize signal handler");
+	}
+
+	// Initialize message bus connection
+	LogMessage(LOG_LEVEL_DEBUG, "Initializing connection to message bus");
+	for (retryNumber = 0; iCommInit() != 0 && retryNumber < maxRetries; ++retryNumber) {
+		nanosleep(&sleepTimePeriod, &remTime);
+	}
+	if (retryNumber == maxRetries) {
+		retval = -1;
+		LogMessage(LOG_LEVEL_ERROR, "Unable to initialize connection to message bus");
+	}
+
+	return retval;
 }
