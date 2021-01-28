ebd65dd08f292e01a3af9c15632bc6534e8ebdaa
Jesper Blidkvist <jesper.blidkvist@live.se>
2021-01-28 15:51:45 +0100

    200ms

--- core/integration-tests/905-geofenceTransgression.py
+++ core/integration-tests/905-geofenceTransgression.py
@@ -29,7 +29,7 @@ def geofenceTransgressionTest():
                (50.001,49.999),
                ( 1.000, 1.000),
                ( 0.000, 0.000)]
-    maxAbortDelay = 0.15
+    maxAbortDelay = 0.2
 
     # Load trajectory
     trajPts = [{'time': 0.00, 'x': 0.0,  'y': 0.0, 'heading': 0.0},
