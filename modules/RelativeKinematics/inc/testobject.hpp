35183eef6fc94440527da8151c3197c5a6487b51
Lukas Wikander <lukas.wikander@astazero.com>
2021-02-15 11:47:13 +0100

    Test object class

---
+++ modules/RelativeKinematics/inc/testobject.hpp
@@ -0,0 +1,39 @@
+#include <netinet/in.h>
+
+// GCC version 8.1 brings non-experimental support for std::filesystem
+#if __GNUC__ > 8 || (__GNUC__ == 8 && __GNUC_MINOR__ >= 1)
+#include <filesystem>
+namespace fs = std::filesystem;
+#else
+#include <experimental/filesystem>
+namespace fs = std::experimental::filesystem;
+#endif
+
+class TestObject {
+public:
+	TestObject();
+	void setCommandAddress(const sockaddr_in& newAddr);
+	void setMonitorAddress(const sockaddr_in& newAddr);
+	void parseConfigurationFile(const fs::path& file);
+	void parseTrajectoryFile(const fs::path& file);
+
+	uint32_t getTransmitterID() const { return transmitterID; }
+	fs::path getTrajectoryFile() const { return trajectoryFile; }
+
+	std::string toString() const;
+private:
+	struct sockaddr_in commandAddress;
+	struct sockaddr_in monitorAddress;
+	fs::path objectFile;
+	fs::path trajectoryFile;
+	uint32_t transmitterID;
+};
+
+// Template specialisation of std::less for TestObject
+namespace std {
+	template<> struct less<TestObject> {
+		bool operator() (const TestObject& lhs, const TestObject& rhs) const {
+			return lhs.getTransmitterID() < rhs.getTransmitterID();
+		}
+	};
+}
