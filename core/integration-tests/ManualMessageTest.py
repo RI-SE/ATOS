835b797278299e14bd12cd9fd7f8887495eeec89
Viktor Johansson <viktor.johansson@astazero.com>
2021-03-29 14:46:44 +0200

    Improved the manual python testing scripts to enable testing of Maestro without using the GUC

--- core/integration-tests/ManualMessageTest.py
+++ core/integration-tests/ManualMessageTest.py
@@ -4,26 +4,36 @@ from tools.Executable import Executable
 import time
 import subprocess
 import sys
+import getopt
 
+MSCPTests = ["INIT",
+             "CONNECTOBJECT",
+             "ARM",
+             "START",
+             "ABORT",
+             "GETSERVERSTATUS",
+             "CREATEOBJECTS",
+             "DISARM",
+             "GETSERVERTIME",
+             "UPLOADFILE",
+             "DOWNLOADFILE",
+             "DELETEFILE",
+             "CHECKFILEEXISTS",
+             "REMOTECONTROL",
+             "SETSERVERPARAMETER",
+             "GETSERVERPARAMETERS",
+             "GETDIRECTORYCONTENT",
+             "DISCONNECTOBJECT"]
 
-MSCPTests = ["INIT", 
-        "CONNECTOBJECT", 
-        "ARM", 
-        "START", 
-        "ABORT", 
-        "GETSERVERSTATUS", 
-        "CREATEOBJECTS", 
-        "DISARM", 
-        "GETSERVERTIME", 
-        "UPLOADFILE", 
-        "DOWNLOADFILE", 
-        "DELETEFILE", 
-        "CHECKFILEEXISTS", 
-        "REMOTECONTROL", 
-        "SETSERVERPARAMETER", 
-        "GETSERVERPARAMETERS", 
-        "GETDIRECTORYCONTENT",
-        "DISCONNECTOBJECT" ]
+MSCPCustomTests = [
+    "INIT",
+    "CONNECTOBJECT",
+    "ARM",
+    "START",
+    "ABORT",
+    "DISARM",
+    "DISCONNECTOBJECT"
+]
 
 ISOTests = ["TRAJ",
             "OSEM",
@@ -43,14 +53,13 @@ ISOTests = ["TRAJ",
             "MTSP"]
 
 
-if __name__ == "__main__":
-
-    S = Executable("../build/TEServer",["-m","0"])
+def old_run():
+    S = Executable("../build/TEServer", ["-m", "0"])
     time.sleep(0.05)
     print("Choose what you would like to test: \n MSCP(1) \n ISO(2) \n:")
     ans = input()
 
-    if(ans == "1" or ans == "MSCP"):
+    if (ans == "1" or ans == "MSCP"):
 
         M = MSCP("127.0.0.1")
         time.sleep(1)
@@ -59,60 +68,59 @@ if __name__ == "__main__":
             print("\n Press enter to test {}".format(t))
             s = input()
 
-            if(t == "INIT"):
+            if (t == "INIT"):
                 M.Init()
 
-            if(t == "CONNECTOBJECT"):
+            if (t == "CONNECTOBJECT"):
                 M.Connect()
 
-            if(t == "ARM"):
+            if (t == "ARM"):
                 M.Arm()
 
-            if(t == "START"):
+            if (t == "START"):
                 M.Start(0)
 
-            if(t == "ABORT"):
+            if (t == "ABORT"):
                 M.Abort()
-            
-            if(t == "GETSERVERSTATUS"):
+
+            if (t == "GETSERVERSTATUS"):
                 M.GetStatus()
-            
-            if(t == "CREATEOBJECTS"):
+
+            if (t == "CREATEOBJECTS"):
                 M.CreateObjects(1)
 
-            if(t == "DISARM"):
+            if (t == "DISARM"):
                 M.Disarm()
 
-            if(t == "GETSERVERTIME"):
+            if (t == "GETSERVERTIME"):
                 M.GetServerTime()
 
-            #if(t == "UPLOADFILE"):
+            # if(t == "UPLOADFILE"):
             #    M.UploadFile("","")
-            
-            if(t == "DOWNLOADFILE"):
+
+            if (t == "DOWNLOADFILE"):
                 M.DownloadFile()
-            
-            if(t == "DELETEFILE"):
+
+            if (t == "DELETEFILE"):
                 M.DeleteFile()
 
-            if(t == "CHECKFILEEXISTS"):
+            if (t == "CHECKFILEEXISTS"):
                 M.CheckFileExists()
-        
-            if(t == "REMOTECONTROL"):
+
+            if (t == "REMOTECONTROL"):
                 M.RemoteControl()
-                
-            if(t == "SETSERVERPARAMETER"):
+
+            if (t == "SETSERVERPARAMETER"):
                 M.SetServerParameter()
 
-            if(t == "GETSERVERPARAMETERS"):
+            if (t == "GETSERVERPARAMETERS"):
                 M.GetServerParameters()
 
-            if(t == "GETDIRECTORYCONTENT"):
+            if (t == "GETDIRECTORYCONTENT"):
                 M.GetDirectoryContent()
 
-            if(t == "DISCONNECTOBJECT"):
+            if (t == "DISCONNECTOBJECT"):
                 M.Disconnect()
-
 
             print("Checking server...")
             time.sleep(1)
@@ -120,11 +128,11 @@ if __name__ == "__main__":
                 S.stop()
                 M.shutdown()
                 sys.exit(1)
-        
+
         M.shutdown()
 
     elif (ans == "2" or ans == "ISO"):
-        
+
         I = ISO("127.0.0.1")
 
         for t in ISOTests:
@@ -132,33 +140,152 @@ if __name__ == "__main__":
             print("\n Press enter to test {}".format(t))
             s = input()
 
-            if(t == "MONR"):
+            if (t == "MONR"):
                 I.MONR()
-            if(t == "TREO"):
+            if (t == "TREO"):
                 I.StringTest()
-            if(t == "OSEM"):
+            if (t == "OSEM"):
                 I.OSEM()
-            if(t == "OSTM"):
+            if (t == "OSTM"):
                 I.OSTM()
-            if(t == "STRT"):
+            if (t == "STRT"):
                 I.STRT()
-            if(t == "HEAB"):
+            if (t == "HEAB"):
                 I.HEAB()
-            if(t == "TRCM"):
+            if (t == "TRCM"):
                 I.TRCM()
-            if(t == "ACCM"):
+            if (t == "ACCM"):
                 I.ACCM()
-            if(t == "TRAJ"):
+            if (t == "TRAJ"):
                 I.TRAJ()
 
-
         I.shutdown()
-
-
 
     S.stop()
     sys.exit(1)
 
 
+def printHelpText(sysArgs):
+    print("""usage: %s [OPTION]...
+Manual python test of Maestro.
+
+Possible OPTION values are listed below.
+	-h, --help           print this text and exit
+	""" % sysArgs[0])
 
 
+def parseArguments(sysArgs):
+    try:
+        opts, args = getopt.getopt(sysArgs[1:], 'hr:', ["help","remote"])
+    except getopt.error as msg:
+        sys.stdout = sys.stderr
+        print(msg)
+        printHelpText(sysArgs)
+        sys.exit(0)
+
+    return_val = {
+        "ip": "localhost",
+        "remote": False
+    }
+
+    for o, a in opts:
+        print(o)
+        if o in ("-h", "--help"):
+            printHelpText(args)
+            sys.exit(0)
+        if o in ("-r", "--remote"):
+            return_val["remote"] = True
+            return_val["ip"] = a
+
+    return return_val
+
+
+def connect_maestro(opts):
+    m_process = None
+    if not opts["remote"]:
+        m_process = Executable("../../build/bin/Core", ["-m", "0"])
+        time.sleep(0.05)
+
+    m_connection = MSCP(opts["ip"])
+    return m_process, m_connection
+
+def shutdown(app, conn):
+    if app:
+        app.stop()
+    if conn:
+        conn.shutdown()
+    sys.exit(0)
+
+def user_execute(app, m_conn, test):
+    for t in test:
+        print("\n Press enter to test {}".format(t))
+        s = input()
+
+        if (t == "INIT"):
+            m_conn.Init()
+
+        if (t == "CONNECTOBJECT"):
+            m_conn.Connect()
+
+        if (t == "ARM"):
+            m_conn.Arm()
+
+        if (t == "START"):
+            m_conn.Start(0)
+
+        if (t == "ABORT"):
+            m_conn.Abort()
+
+        if (t == "GETSERVERSTATUS"):
+            m_conn.GetStatus()
+
+        if (t == "CREATEOBJECTS"):
+            m_conn.CreateObjects(1)
+
+        if (t == "DISARM"):
+            m_conn.Disarm()
+
+        if (t == "GETSERVERTIME"):
+            m_conn.GetServerTime()
+
+        # if(t == "UPLOADFILE"):
+        #    m_conn.UploadFile("","")
+
+        if (t == "DOWNLOADFILE"):
+            m_conn.DownloadFile()
+
+        if (t == "DELETEFILE"):
+            m_conn.DeleteFile()
+
+        if (t == "CHECKFILEEXISTS"):
+            m_conn.CheckFileExists()
+
+        if (t == "REMOTECONTROL"):
+            m_conn.RemoteControl()
+
+        if (t == "SETSERVERPARAMETER"):
+            m_conn.SetServerParameter()
+
+        if (t == "GETSERVERPARAMETERS"):
+            m_conn.GetServerParameters()
+
+        if (t == "GETDIRECTORYCONTENT"):
+            m_conn.GetDirectoryContent()
+
+        if (t == "DISCONNECTOBJECT"):
+            m_conn.Disconnect()
+
+        if app:
+            print("Checking server...")
+            time.sleep(1)
+            if app.poll():
+                app.stop()
+                m_conn.shutdown()
+                sys.exit(1)
+
+
+if __name__ == "__main__":
+    opts = parseArguments(sys.argv)
+    app, conn = connect_maestro(opts)
+    user_execute(app, conn, MSCPCustomTests)
+    shutdown(app, conn)
