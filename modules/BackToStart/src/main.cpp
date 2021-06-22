373b210ccb813294d150981ef4f61a816527cfe0
Lukas Wikander <lukas.wikander@astazero.com>
2021-06-22 14:45:24 +0200

    Indentation and CMakeLists

--- modules/BackToStart/src/main.cpp
+++ modules/BackToStart/src/main.cpp
@@ -4,42 +4,38 @@
 #include "logging.h"
 #include "util.h"
 
-#define MODULE_NAME "Back_to_start"
+#define MODULE_NAME "BackToStart"
 
 int main()
 {
-    COMMAND command = COMM_INV;
-    char mqRecvData[MQ_MSG_SIZE];
-    const struct timespec sleepTimePeriod = {0,10000000};
-    const struct timespec abortWaitTime = {1,0};
-    struct timespec remTime;
+	COMMAND command = COMM_INV;
+	char mqRecvData[MQ_MSG_SIZE];
+	const struct timespec sleepTimePeriod = {0,10000000};
+	struct timespec remTime;
 
-    LogInit(MODULE_NAME,LOG_LEVEL_DEBUG);
-    LogMessage(LOG_LEVEL_INFO, "Task running with PID: %u",getpid());
+	LogInit(MODULE_NAME,LOG_LEVEL_DEBUG);
+	LogMessage(LOG_LEVEL_INFO, "Task running with PID: %u", getpid());
 
-    // Initialize message bus connection
-    while(iCommInit())
-    {
-        nanosleep(&sleepTimePeriod,&remTime);
-    }
+	// Initialize message bus connection
+	while (iCommInit()) {
+		nanosleep(&sleepTimePeriod,&remTime);
+	}
 
-    while(true)
-    {
-        if (iCommRecv(&command,mqRecvData,MQ_MSG_SIZE,nullptr) < 0)
-        {
-            util_error("Message bus receive error");
-        }
+	while (true) {
+		if (iCommRecv(&command,mqRecvData,MQ_MSG_SIZE,nullptr) < 0) {
+			util_error("Message bus receive error");
+		}
 
-        switch (command) {
-        case COMM_INV:
-            nanosleep(&sleepTimePeriod,&remTime);
-            break;
-        case COMM_OBC_STATE:
-            break;
-        default:
-            LogMessage(LOG_LEVEL_INFO,"Received command %u",command);
-        }
-    }
+		switch (command) {
+		case COMM_INV:
+			nanosleep(&sleepTimePeriod,&remTime);
+			break;
+		case COMM_OBC_STATE:
+			break;
+		default:
+			LogMessage(LOG_LEVEL_INFO,"Received command %u",command);
+		}
+	}
 
-    return 0;
+	return 0;
 }
