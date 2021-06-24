5bad90a97b0b65b9914fd56c4b57fd2867876fef
konglobemeralt <Jesper.Blidkvist@live.se>
2021-06-24 13:07:42 +0200

    reverse WIP

--- modules/BackToStart/src/main.cpp
+++ modules/BackToStart/src/main.cpp
@@ -1,10 +1,63 @@
 #include <iostream>
 #include <unistd.h>
-
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <iterator>
 #include "logging.h"
 #include "util.h"
 
 #define MODULE_NAME "BackToStart"
+
+int reverseTraj()
+{
+	std::vector<std::string> fileVector;
+	std::vector<std::string> timeVector;
+
+	std::ifstream in("/home/jesper/Documents/gitz/Maestro/core/traj/GarageplanInnerring.traj");
+	std::string str;
+
+	while (std::getline(in, str))
+	{
+		// Line contains string of length > 0 then save it in vector
+		std::cout << str << "\n";
+		if(str.size() > 0){
+			fileVector.push_back(str);
+		}
+	}
+
+	std::string header = fileVector.front();
+	fileVector.erase(fileVector.begin());
+	std::string endLine = fileVector.back();
+	fileVector.pop_back();
+
+	std::string delimiter = ";";
+
+	for(int i=0; i < fileVector.size(); i++){
+		std::string str = fileVector.at(i);
+		size_t pos = 0;
+		std::string token;
+		while ((pos = str.find(delimiter)) != std::string::npos) {
+			token = str.substr(0, pos);
+			if(pos == 5){
+				std::cout << token << std::endl;
+				timeVector.push_back(token);
+			}
+			str.erase(0, pos + delimiter.length());
+		}
+	}
+
+	std::cout << "REVERSED: \n";
+
+	std::cout << header << "\n";
+	while (!fileVector.empty())
+	  {
+		std::cout << fileVector.back() << "\n";
+		fileVector.pop_back();
+	  }
+	std::cout << endLine << "\n";
+
+}
 
 int main()
 {
@@ -32,6 +85,9 @@ int main()
 			break;
 		case COMM_OBC_STATE:
 			break;
+		case COMM_INIT:
+			reverseTraj();
+
 		default:
 			LogMessage(LOG_LEVEL_INFO,"Received command %u",command);
 		}
