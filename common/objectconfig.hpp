6e370c9c33853598b48be7f91028d6ee3bd752c7
Lukas Wikander <lukas.wikander@astazero.com>
2021-06-29 18:46:37 +0200

    Fixed ObjectConfig to build, moved util

--- common/objectconfig.hpp
+++ common/objectconfig.hpp
@@ -14,7 +14,6 @@ namespace fs = std::experimental::filesystem;
 
 struct DataInjectionMap {
 	uint32_t sourceID;
-	unsigned int numberOfTargets;
 	std::vector<uint32_t> targetIDs;
 	bool isActive;
 };
@@ -26,31 +25,38 @@ public:
 
 	void parseConfigurationFile(const fs::path& file);
 
-	bool isAnchor() { return isAnchorObject; }
+	bool isAnchor() const { return isAnchorObject; }
 	bool isOSI() const { return isOSICompatible; }
 	DataInjectionMap getInjectionMap() const { return injectionMap; }
-	in_addr_t getIP(void) const { return ip_addr; }
+	in_addr_t getIP(void) const { return remoteIP; }
 	double getMaximumSpeed() const { return maximumSpeed; }
 	GeographicPositionType getOrigin() const { return origin; }
 	Trajectory getTrajectory() const { return trajectory; }
+	void setTrajectory(const Trajectory& newTraj) { trajectory = newTraj; } // TODO danger danger - don't do this
 	uint32_t getTransmitterID() const { return transmitterID; }
 	double getTurningDiameter() const { return turningDiameter; }
+	std::string getObjectFileName() const { return objectFile.filename().string(); }
+	std::string getTrajectoryFileName() const { return trajectoryFile.filename().string(); }
+
 
 	std::string toString() const;
 
 private:
 	fs::path objectFile;
+	fs::path trajectoryFile;
 	bool isAnchorObject = false;
 	bool isOSICompatible = false;
-	in_addr_t ip_addr = 0;
+	in_addr_t remoteIP = 0;
 	double maximumSpeed = 0;
+	bool hasMaximumSpeed = false;
 	GeographicPositionType origin;
 	Trajectory trajectory;
 	uint32_t transmitterID = 0;
+	bool turningDiameterKnown = false;
 	double turningDiameter = 0;
 	DataInjectionMap injectionMap;
 
 	template<size_t N>
 	bool isSettingTrue(char (&setting)[N]);
-	void split(std::string &str, char delim, std::vector<int> &out);
+	void split(std::string &str, char delim, std::vector<int>& out);
 };
