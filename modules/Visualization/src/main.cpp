ae8921f619c233eba75bff119111faff2dc798c9
eriadam <adameriksson36@gmail.com>
2021-09-07 11:03:42 +0200

    removed while loops in visualizer and send osem on connect becouse no allobjectsconnected happens in mqbus when running realtimekinematics

--- modules/Visualization/src/main.cpp
+++ modules/Visualization/src/main.cpp
@@ -5,9 +5,9 @@
  * @brief  Sends UDP and TCP data for about objectmonitor and trajectory data to a visualizer
  * @version 0.1
  * @date 2020-10-16
- * 
+ *
  * @copyright Copyright (c) 2020
- * 
+ *
  */
 #include <signal.h>
 #include <fstream>
@@ -44,9 +44,9 @@ namespace fs = std::experimental::filesystem;
 #define UDP_VISUALIZATION_SERVER_PORT 53251
 #define TRAJECTORY_TX_BUFFER_SIZE 2048
 
-/*------------------------------------------------------------
-  -- Static variables
-  ------------------------------------------------------------*/
+  /*------------------------------------------------------------
+	-- Static variables
+	------------------------------------------------------------*/
 static bool quit = false;
 
 /*------------------------------------------------------------
@@ -54,14 +54,15 @@ static bool quit = false;
   ------------------------------------------------------------*/
 
 
-/*------------------------------------------------------------
-  -- Private functions
-  ------------------------------------------------------------*/
+  /*------------------------------------------------------------
+	-- Private functions
+	------------------------------------------------------------*/
 static void signalHandler(int signo);
-static int awaitConnection(TCPHandler &tcpPort, enum COMMAND &receivedCommand, bool& areObjectsConnected);
-static int transmitTrajectories(TCPHandler &tcpPort);
-static int transmitObjectData(TCPHandler &tcpPort, UDPHandler &udpPort);
-static int transmitOSEM(TCPHandler &tcpPort);
+static int awaitConnection(TCPHandler& tcpPort, enum COMMAND& receivedCommand, bool& areObjectsConnected);
+static int awaitMQCommand(TCPHandler& tcpPort, enum COMMAND& receivedCommand);
+static int transmitTrajectories(TCPHandler& tcpPort);
+static int transmitObjectData(TCPHandler& tcpPort, UDPHandler& udpPort, bool& areObjectsConnected);
+static int transmitOSEM(TCPHandler& tcpPort);
 
 /*------------------------------------------------------------
   -- Main task
@@ -72,7 +73,7 @@ int main(int argc, char const* argv[]) {
 	COMMAND command = COMM_INV;
 	std::vector<char> buffer(MONR_BUFFER_LENGTH);
 	char mqRecvData[MBUS_MAX_DATALEN];
-	const struct timespec sleepTimePeriod = {0,10000000};
+	const struct timespec sleepTimePeriod = { 0,10000000 };
 	struct timespec remTime;
 	int bytesread = 0;
 	char debug = 0;
@@ -86,13 +87,9 @@ int main(int argc, char const* argv[]) {
 		util_error("Unable to initialize signal handler");
 
 	// Initialize message bus connection
-	while(iCommInit() && !quit) {
-		nanosleep(&sleepTimePeriod,&remTime);
+	while (iCommInit() && !quit) {
+		nanosleep(&sleepTimePeriod, &remTime);
 	}
-
-	// Notify service handler that startup was successful
-	
-	sd_notify(0, "READY=1");
 
 	ReadWriteAccess_t retval = DataDictionaryInitObjectData();
 
@@ -100,80 +97,51 @@ int main(int argc, char const* argv[]) {
 		exit(EXIT_FAILURE);
 	}
 
-	while(!quit){
-		TCPHandler visualizerTCPPort(TCP_VISUALIZATION_SERVER_PORT , "", "Server", 1, O_NONBLOCK);
-		UDPHandler visualizerUDPPort(UDP_VISUALIZATION_SERVER_PORT, "", 0, "Server");
+	TCPHandler visualizerTCPPort(TCP_VISUALIZATION_SERVER_PORT, "", "Server", 1, O_NONBLOCK);
+	UDPHandler visualizerUDPPort(UDP_VISUALIZATION_SERVER_PORT, "", 0, "Server");
+	while (!quit) {
 
 		if (awaitConnection(visualizerTCPPort, command, areObjectsConnected) == -1) {
-			LogMessage(LOG_LEVEL_ERROR, "Failed to establish connection");
+			LogMessage(LOG_LEVEL_ERROR, "Exit command sent, Visualization module shutting down");
 			if (command == COMM_EXIT) {
 				quit = true;
 			}
-			continue;
 		}
-		visualizerUDPPort.receiveUDP(buffer);
-		buffer.clear();
-		// have to read inbetween loops or we could end in limbo becouse getonconnection only looks att acceept not recive atm, 
-		// TO DO: fix so that get connection also is looked at on recv andchange that to a bool but that means we have to change somestuff in tcphandler.
-		visualizerTCPPort.receiveTCP(buffer, 0);
 
 		if (areObjectsConnected) {
-			//LogMessage(LOG_LEVEL_INFO, "Sending Traj");
-			//transmitTrajectories(visualizerTCPPort);
 			LogMessage(LOG_LEVEL_INFO, "Sending OSEM");
 			transmitOSEM(visualizerTCPPort);
+			areObjectsConnected = false;
 		}
 
+		transmitObjectData(visualizerTCPPort, visualizerUDPPort, areObjectsConnected);
 
-		
-		while(!quit && visualizerTCPPort.getConnectionOn() > 0 && bytesread>=0){
-			do {
-				if (iCommRecv(&command, mqRecvData, sizeof (mqRecvData), nullptr) < 0) {
-					util_error("Message bus receive error");
-				}
-				switch (command) {
-				case COMM_EXIT:
-					quit = true;
-					break;
-				case COMM_OBJECTS_CONNECTED:
-					//LogMessage(LOG_LEVEL_INFO, "Sending Traj");
-					//transmitTrajectories(visualizerTCPPort);
-					LogMessage(LOG_LEVEL_INFO, "Sending OSEM");
-					transmitOSEM(visualizerTCPPort);
-					areObjectsConnected = true;
-					break;
-				}
-			} while (command != COMM_INV);
-
-			// TODO fix the timing we send etc
-				
-
-			if (transmitObjectData(visualizerTCPPort, visualizerUDPPort) < 0) {
-				LogMessage(LOG_LEVEL_ERROR, "Failed to transmit object data");
-				break;
+		if (awaitMQCommand(visualizerTCPPort, command) == -1) {
+			LogMessage(LOG_LEVEL_ERROR, "Exit command sent, Visualization module shutting down");
+			if (command == COMM_EXIT) {
+				quit = true;
 			}
-			nanosleep(&sleepTimePeriod,&remTime); // sleep might not be needed! but added it becouse i am not sure how much it will use sharedmemory resources
 		}
-		visualizerTCPPort.TCPHandlerclose();
-		visualizerUDPPort.UDPHandlerclose();
-		LogMessage(LOG_LEVEL_INFO, "Disconnected");
+		nanosleep(&sleepTimePeriod, &remTime);
 	}
+
+	visualizerTCPPort.TCPHandlerclose();
+	visualizerUDPPort.UDPHandlerclose();
 }
 
 
-void signalHandler(int signo){
+void signalHandler(int signo) {
 	if (signo == SIGINT) {
 		LogMessage(LOG_LEVEL_WARNING, "Caught keyboard interrupt");
 		quit = true;
 		exit(EXIT_FAILURE);
-	}
-	else {
+	} else {
 		LogMessage(LOG_LEVEL_ERROR, "Caught unhandled signal");
 	}
 }
 
 
-int transmitObjectData(TCPHandler &tcpPort, UDPHandler &udpPort) {
+int transmitObjectData(TCPHandler& tcpPort, UDPHandler& udpPort, bool& areObjectsConnected) {
 	uint32_t numberOfObjects;
 	std::vector<uint32_t> transmitterIDs;
 	ObjectMonitorType monitorData;
@@ -183,95 +151,107 @@ int transmitObjectData(TCPHandler &tcpPort, UDPHandler &udpPort) {
 	std::vector<char> trashBuffer(MONR_BUFFER_LENGTH);
 	std::vector<char> udpTransmitBuffer(MONR_BUFFER_LENGTH);
 
+	if (tcpPort.receiveTCP(trashBuffer, 0) < 0) {
+		LogMessage(LOG_LEVEL_INFO, "Disconnected");
+		areObjectsConnected = true;
+		return -1;
+	}
+
 	DataDictionaryGetNumberOfObjects(&numberOfObjects);
-	if (numberOfObjects<=0){
+	if (numberOfObjects <= 0) {
 		return 0;
 	}
 	transmitterIDs.resize(numberOfObjects);
+
+	// get transmitterid and encode monr to iso.
 	DataDictionaryGetObjectTransmitterIDs(transmitterIDs.data(), transmitterIDs.size());
-	for (const auto &transmitterID : transmitterIDs) {
-		if (transmitterID == 0){
+	for (const auto& transmitterID : transmitterIDs) {
+		if (transmitterID == 0) {
 			continue;
 		}
-		
+
 		DataDictionaryGetMonitorData(transmitterID, &monitorData);
 		DataDictionaryGetMonitorDataReceiveTime(transmitterID, &monitorDataReceiveTime);
 		if (timerisset(&monitorDataReceiveTime)
-				&& monitorData.position.isPositionValid
-				&& monitorData.position.isHeadingValid) {
-			// get transmitterid and encode monr to iso.
-			// Checking so we still connected to visualizer
+			&& monitorData.position.isPositionValid
+			&& monitorData.position.isHeadingValid) {
 			if (tcpPort.receiveTCP(trashBuffer, 0) < 0) {
-				LogMessage(LOG_LEVEL_ERROR, "Error when checking visualizer socket");
+				LogMessage(LOG_LEVEL_INFO, "Disconnected");
+				areObjectsConnected = true;
 				return -1;
 			}
-			// only here to get server/client behavior
-			
 
-			// TODO PODI - now we cheat with header transmitter ID
+			//TODO: PODI - now we cheat with header transmitter ID
 			isoTransmitterID = static_cast<uint8_t>(transmitterID);
 			setTransmitterID(isoTransmitterID);
 			long retval = encodeMONRMessage(&monitorDataReceiveTime,
-											monitorData.position,
-											monitorData.speed,
-											monitorData.acceleration,
-											monitorData.drivingDirection,
-											monitorData.state,
-											monitorData.armReadiness,
-											0, // TODO
-											udpTransmitBuffer.data(),
-											udpTransmitBuffer.size(), 0);
+				monitorData.position,
+				monitorData.speed,
+				monitorData.acceleration,
+				monitorData.drivingDirection,
+				monitorData.state,
+				monitorData.armReadiness,
+				0, // TODO:
+				udpTransmitBuffer.data(),
+				udpTransmitBuffer.size(), 0);
 			if (retval < 0) {
 				LogMessage(LOG_LEVEL_ERROR, "Failed when encoding MONR message");
-				return 0; //TO DO fix this, we can't break out of the while look becouse we failed with encoding monr, that will end up killing tcp and udp becouse a bad encode, should be handled in another way 
+				return 0; //TODO: fix this, we can't break out of the while look becouse we failed with encoding monr, that will end up killing tcp and udp becouse a bad encode, should be handled in another way 
 			}
 			udpTransmitBuffer.resize(static_cast<unsigned long>(retval));
 			udpPort.receiveUDP(trashBuffer);
 			bytesSent = udpPort.sendUDP(udpTransmitBuffer);
 			if (bytesSent < 0) {
-				LogMessage(LOG_LEVEL_ERROR, "Error when sending on visualizer UDP socket");
-				return 0;//TO DO fix this, we can't break just becouse we couldent send a UDP message, even do it should not happen, but a example when it will happen is depending on which task started first visualizer or app etc.
+				LogMessage(LOG_LEVEL_INFO, "Unable to send ObjectData to Visualizer");
 			}
+
 		}
 	}
 	return 0;
 }
 
 int awaitConnection(
-		TCPHandler &tcpPort,
-		enum COMMAND &receivedCommand,
-		bool &areObjectsConnected) {
+	TCPHandler& tcpPort,
+	enum COMMAND& receivedCommand,
+	bool& areObjectsConnected) {
 
 	receivedCommand = COMM_INV;
 	char mqRecvData[MBUS_MAX_DATALEN];
-
-	LogMessage(LOG_LEVEL_INFO, "Awaiting TCP connection...");
-	while(tcpPort.getConnectionOn() != 1) {
+	if (tcpPort.getConnectionOn() != 1) {
+		LogMessage(LOG_LEVEL_INFO, "Awaiting TCP connection...");
+	}
+	while (tcpPort.getConnectionOn() != 1) {
 		tcpPort.TCPHandlerAccept(5);
 
-		do {
-			if (iCommRecv(&receivedCommand, mqRecvData, sizeof (mqRecvData), nullptr) < 0) {
-				util_error("Message bus receive error");
-			}
+		if (tcpPort.getConnectionOn() == 1) {
+			LogMessage(LOG_LEVEL_INFO, "TCP connection established");
+		}
 
-			switch (receivedCommand) {
-			case COMM_EXIT:
-				return -1;
-			case COMM_OBJECTS_CONNECTED:
-				areObjectsConnected = true;
-				break;
-			case COMM_DISCONNECT: // TODO what happens when an object forces the disconnection?
-				areObjectsConnected = false;
-				break;
-			}
-		} while (receivedCommand != COMM_INV);
+		if (iCommRecv(&receivedCommand, mqRecvData, sizeof(mqRecvData), nullptr) < 0) {
+			util_error("Message bus receive error");
+		}
+
+		switch (receivedCommand) {
+		case COMM_EXIT:
+			return -1;
+		case COMM_OBJECTS_CONNECTED:
+			areObjectsConnected = true;
+			break;
+		case COMM_DISCONNECT: // TODO what happens when an object forces the disconnection?
+			areObjectsConnected = false;
+			break;
+		case COMM_CONNECT:
+			areObjectsConnected = true;
+			break;
+		default:
+			continue;
+		}
+
 	}
-
-	LogMessage(LOG_LEVEL_INFO, "TCP connection established");
 	return 0;
 }
 
-int transmitTrajectories(TCPHandler &tcpPort) {
+int transmitTrajectories(TCPHandler& tcpPort) {
 
 	std::vector<char> trajPath(PATH_MAX, '\0');
 	std::vector<char> transmitBuffer;
@@ -279,7 +259,7 @@ int transmitTrajectories(TCPHandler &tcpPort) {
 
 	UtilGetTrajDirectoryPath(trajPath.data(), trajPath.size());
 
-	for (const auto &entry : fs::directory_iterator(trajPath.data())) {
+	for (const auto& entry : fs::directory_iterator(trajPath.data())) {
 		/* TO DO:
 		should have a checker
 		here to see that all the objects are connected
@@ -288,14 +268,13 @@ int transmitTrajectories(TCPHandler &tcpPort) {
 		std::ifstream file(entry.path());
 		std::string line;
 		int retval = 0;
-		while (std::getline(file,line)) {
+		while (std::getline(file, line)) {
 			rc = parseTraj(line, transmitBuffer);
 
 			if (rc == -1) {
 				retval = -1;
 				break;
-			}
-			else if ((MONR_BUFFER_LENGTH-retval) < transmitBuffer.size()) {
+			} else if ((MONR_BUFFER_LENGTH - retval) < transmitBuffer.size()) {
 				tcpPort.sendTCP(transmitBuffer);
 				transmitBuffer.clear();
 			}
@@ -305,13 +284,13 @@ int transmitTrajectories(TCPHandler &tcpPort) {
 }
 
 
-int transmitOSEM(TCPHandler &tcp){
+int transmitOSEM(TCPHandler& tcp) {
 	ObjectSettingsType osem;
 	GeoPosition originPosition;
 	std::vector<char> tcpTransmitBuffer(MONR_BUFFER_LENGTH);
 	std::vector<uint32_t> transmitterIDs;
 	int bytesSent;
-	uint32_t numberOfObjects;	
+	uint32_t numberOfObjects;
 
 	if (DataDictionaryGetNumberOfObjects(&numberOfObjects) != READ_OK) {
 		throw std::ios_base::failure("Data dictionary number of objects read error");
@@ -325,11 +304,11 @@ int transmitOSEM(TCPHandler &tcp){
 		throw std::ios_base::failure("Data dictionary transmitter ID read error");
 	}
 
-	for (const auto &transmitterID : transmitterIDs) {
+	for (const auto& transmitterID : transmitterIDs) {
 		if (transmitterID == 0) {
 			continue;
 		}
-		
+
 		if (DataDictionaryGetOrigin(transmitterID, &originPosition) != READ_OK) {
 			throw std::ios_base::failure("Data dictionary origin read error for transmitter ID " + std::to_string(transmitterID));
 		}
@@ -351,10 +330,31 @@ int transmitOSEM(TCPHandler &tcp){
 		tcpTransmitBuffer.resize(static_cast<unsigned long>(retval));
 		bytesSent = tcp.sendTCP(tcpTransmitBuffer);
 
-		if (bytesSent < 0){
+		if (bytesSent < 0) {
 			throw std::ios_base::failure("Error when sending on the visualizer tcp socket");
 		}
 	}
 
 	return 0;
+}
+
+int awaitMQCommand(TCPHandler& tcpPort, COMMAND& receivedCommand) {
+	receivedCommand = COMM_INV;
+	char mqRecvData[MBUS_MAX_DATALEN];
+
+	if (iCommRecv(&receivedCommand, mqRecvData, sizeof(mqRecvData), nullptr) < 0) {
+		util_error("Message bus receive error");
+	}
+	switch (receivedCommand) {
+	case COMM_EXIT:
+		return -1;
+	case COMM_OBJECTS_CONNECTED:
+		LogMessage(LOG_LEVEL_INFO, "Sending OSEM");
+		transmitOSEM(tcpPort);
+		break;
+	case COMM_CONNECT:
+		LogMessage(LOG_LEVEL_INFO, "Sending OSEM");
+		transmitOSEM(tcpPort);
+		break;
+	}
 }
