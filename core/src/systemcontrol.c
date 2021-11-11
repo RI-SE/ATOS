44af8ae521a8c06027463cbec9a9d6c8a0c7d2e8
hyle <alfred.aronsson@conmore.se>
2021-11-11 16:01:50 +0100

    Added todo and fixed whitespace

--- core/src/systemcontrol.c
+++ core/src/systemcontrol.c
@@ -623,23 +623,23 @@ void systemcontrol_task(TimeType * GPSTime, GSDType * GSD, LOG_LEVEL logLevel) {
 		case COMM_BACKTOSTART_RESPONSE:
 
 			if(*pcRecvBuffer == BTS_FAIL){
-                LogMessage(LOG_LEVEL_DEBUG, "Back-to-start result: %s", pcRecvBuffer);
+				LogMessage(LOG_LEVEL_DEBUG, "Back-to-start result: %s", pcRecvBuffer);
 				memset(ControlResponseBuffer, 0, sizeof (ControlResponseBuffer));
-                SystemControlSendControlResponse(SYSTEM_CONTROL_RESPONSE_CODE_OK, "BTS:",
-                                                 "0", 1,
-                                                 &ClientSocket, 0);
-            }
+				SystemControlSendControlResponse(SYSTEM_CONTROL_RESPONSE_CODE_OK, "BTS:",
+												 "0", 1,
+												 &ClientSocket, 0);		//TODO: Send SMCP response the right way(?)
+			}
 			else if(*pcRecvBuffer == BTS_PASS){
-                LogMessage(LOG_LEVEL_DEBUG, "Back-to-start result: %s", pcRecvBuffer);
+				LogMessage(LOG_LEVEL_DEBUG, "Back-to-start result: %s", pcRecvBuffer);
 				memset(ControlResponseBuffer, 0, sizeof (ControlResponseBuffer));
-                SystemControlSendControlResponse(SYSTEM_CONTROL_RESPONSE_CODE_OK, "BTS:",
-                                                 "1", 1,
-                                                 &ClientSocket, 0);
-            }
-            else{
+				SystemControlSendControlResponse(SYSTEM_CONTROL_RESPONSE_CODE_OK, "BTS:",
+												 "1", 1,
+												 &ClientSocket, 0);
+			}
+			else{
 				LogMessage(LOG_LEVEL_ERROR, "Cannot parse COMM_BACKTOSTART_RESPONSE message.");
-            }
-            break;
+			}
+			break;
 
 		default:
 			LogMessage(LOG_LEVEL_WARNING, "Unhandled message bus command: %u", iCommand);
