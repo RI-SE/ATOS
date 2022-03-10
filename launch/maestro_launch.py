8766a1504435a6388e3bdc7566beea8531f8553f
Lukas Wikander <lukas.wikander@astazero.com>
2022-03-10 17:46:44 +0100

    Added maestro launch file

---
+++ launch/maestro_launch.py
@@ -0,0 +1,40 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='maestro',
+            namespace='maestro',
+            executable='maestro_base',
+            name='maestro_base'
+        ),
+        Node(
+            package='maestro',
+            namespace='maestro',
+            executable='system_control',
+            name='system_control'
+        ),
+        Node(
+            package='maestro',
+            namespace='maestro',
+            executable='object_control',
+            name='object_control'
+        ),
+        #Node(
+        #    package='maestro',
+        #    namespace='maestro',
+        #    executable='time_control',
+        #    name='time_control'
+        #),
+        Node(
+            package='maestro',
+            namespace='maestro',
+            executable='journal_control',
+            name='journal_control'
+        )
+    ])
