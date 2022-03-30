b1d40db406fa2a85248460dfbf6cdecb3a16ed35
Victor Jarlow <victor.jarlow@astazero.com>
2022-03-30 10:57:50 +0200

    wrapped maestro in namespace

--- modules/ScenarioControl/inc/isotrigger.h
+++ modules/ScenarioControl/inc/isotrigger.h
@@ -4,53 +4,54 @@
 #include "util.h"
 #include "trigger.h"
 
-class ISOTrigger : public Trigger
-{
-public:
-    using Trigger::Trigger;
-
-    TriggerReturnCode_t parseParameters() override { return OK; }
-
-    using Trigger::update;
-    TriggerReturnCode_t update(void) override { return wasTriggeredByLastUpdate = NO_TRIGGER_OCCURRED; }
-    TriggerReturnCode_t update(TriggerEvent::SharedPtr) override;
-private:
-    TriggerReturnCode_t checkIfTriggered() const override;
-    const std::set<TriggerParameter_t> getAcceptedParameters() const override
+namespace maestro {
+    class ISOTrigger : public Trigger
     {
-        std::set<Trigger::TriggerParameter_t> retval;
+    public:
+        using Trigger::Trigger;
 
-        retval.insert(TRIGGER_PARAMETER_FALSE);
-        retval.insert(TRIGGER_PARAMETER_TRUE);
-        retval.insert(TRIGGER_PARAMETER_RELEASED);
-        retval.insert(TRIGGER_PARAMETER_PRESSED);
-        retval.insert(TRIGGER_PARAMETER_LOW);
-        retval.insert(TRIGGER_PARAMETER_HIGH);
-        retval.insert(TRIGGER_PARAMETER_RISING_EDGE);
-        retval.insert(TRIGGER_PARAMETER_FALLING_EDGE);
-        retval.insert(TRIGGER_PARAMETER_ANY_EDGE);
-        retval.insert(TRIGGER_PARAMETER_RELATIVE);
-        retval.insert(TRIGGER_PARAMETER_ABSOLUTE);
-        retval.insert(TRIGGER_PARAMETER_VALUE);
-        retval.insert(TRIGGER_PARAMETER_MIN);
-        retval.insert(TRIGGER_PARAMETER_MAX);
-        retval.insert(TRIGGER_PARAMETER_MEAN);
-        retval.insert(TRIGGER_PARAMETER_EQUAL_TO);
-        retval.insert(TRIGGER_PARAMETER_GREATER_THAN);
-        retval.insert(TRIGGER_PARAMETER_GREATER_THAN_OR_EQUAL_TO);
-        retval.insert(TRIGGER_PARAMETER_LESS_THAN);
-        retval.insert(TRIGGER_PARAMETER_LESS_THAN_OR_EQUAL_TO);
-        retval.insert(TRIGGER_PARAMETER_NOT_EQUAL_TO);
-        retval.insert(TRIGGER_PARAMETER_X);
-        retval.insert(TRIGGER_PARAMETER_Y);
-        retval.insert(TRIGGER_PARAMETER_Z);
-        retval.insert(TRIGGER_PARAMETER_TIME);
-        retval.insert(TRIGGER_PARAMETER_DATE);
-        retval.insert(TRIGGER_PARAMETER_RULE);
-        retval.insert(TRIGGER_PARAMETER_UNAVAILABLE);
+        TriggerReturnCode_t parseParameters() override { return OK; }
 
-        return retval;
-    }
-};
+        using Trigger::update;
+        TriggerReturnCode_t update(void) override { return wasTriggeredByLastUpdate = NO_TRIGGER_OCCURRED; }
+        TriggerReturnCode_t update(TriggerEvent::SharedPtr) override;
+    private:
+        TriggerReturnCode_t checkIfTriggered() const override;
+        const std::set<TriggerParameter_t> getAcceptedParameters() const override
+        {
+            std::set<Trigger::TriggerParameter_t> retval;
 
+            retval.insert(TRIGGER_PARAMETER_FALSE);
+            retval.insert(TRIGGER_PARAMETER_TRUE);
+            retval.insert(TRIGGER_PARAMETER_RELEASED);
+            retval.insert(TRIGGER_PARAMETER_PRESSED);
+            retval.insert(TRIGGER_PARAMETER_LOW);
+            retval.insert(TRIGGER_PARAMETER_HIGH);
+            retval.insert(TRIGGER_PARAMETER_RISING_EDGE);
+            retval.insert(TRIGGER_PARAMETER_FALLING_EDGE);
+            retval.insert(TRIGGER_PARAMETER_ANY_EDGE);
+            retval.insert(TRIGGER_PARAMETER_RELATIVE);
+            retval.insert(TRIGGER_PARAMETER_ABSOLUTE);
+            retval.insert(TRIGGER_PARAMETER_VALUE);
+            retval.insert(TRIGGER_PARAMETER_MIN);
+            retval.insert(TRIGGER_PARAMETER_MAX);
+            retval.insert(TRIGGER_PARAMETER_MEAN);
+            retval.insert(TRIGGER_PARAMETER_EQUAL_TO);
+            retval.insert(TRIGGER_PARAMETER_GREATER_THAN);
+            retval.insert(TRIGGER_PARAMETER_GREATER_THAN_OR_EQUAL_TO);
+            retval.insert(TRIGGER_PARAMETER_LESS_THAN);
+            retval.insert(TRIGGER_PARAMETER_LESS_THAN_OR_EQUAL_TO);
+            retval.insert(TRIGGER_PARAMETER_NOT_EQUAL_TO);
+            retval.insert(TRIGGER_PARAMETER_X);
+            retval.insert(TRIGGER_PARAMETER_Y);
+            retval.insert(TRIGGER_PARAMETER_Z);
+            retval.insert(TRIGGER_PARAMETER_TIME);
+            retval.insert(TRIGGER_PARAMETER_DATE);
+            retval.insert(TRIGGER_PARAMETER_RULE);
+            retval.insert(TRIGGER_PARAMETER_UNAVAILABLE);
+
+            return retval;
+        }
+    };
+}
 #endif // ISOTRIGGER_H
--- modules/ScenarioControl/src/booleantrigger.cpp
+++ modules/ScenarioControl/src/booleantrigger.cpp
@@ -1,40 +1,42 @@
 #include "booleantrigger.h"
 
-/*!
- * \brief BooleanTrigger::update Updates the tracked signal (i.e. which causes the trigger) to the value specified
- * \param isBrakeCurrentlyPressed Boolean describing if the boolean
- * \param Unused time of measurement
- * \return Value according to ::TriggerReturnCode_t
- */
-Trigger::TriggerReturnCode_t BooleanTrigger::update(bool currentStateValue, struct timeval)
-{
-	wasStateTrue = isStateTrue;
-	isStateTrue = currentStateValue;
-	wasTriggeredByLastUpdate = checkIfTriggered();
-	return wasTriggeredByLastUpdate;
-}
-
-/*!
- * \brief BooleanTrigger::checkIfTriggered Check if the trigger has occurred based on the mode and state
- * \return Value according to ::TriggerReturnCode_t
- */
-Trigger::TriggerReturnCode_t BooleanTrigger::checkIfTriggered() const
-{
-	switch (mode) {
-	case HIGH:
-		return isStateTrue ? TRIGGER_OCCURRED : NO_TRIGGER_OCCURRED;
-	case LOW:
-		return !isStateTrue ? TRIGGER_OCCURRED : NO_TRIGGER_OCCURRED;
-	case EDGE_ANY:
-		return (isStateTrue != wasStateTrue) ? TRIGGER_OCCURRED : NO_TRIGGER_OCCURRED;
-	case EDGE_RISING:
-		return (isStateTrue && !wasStateTrue) ? TRIGGER_OCCURRED : NO_TRIGGER_OCCURRED;
-	case EDGE_FALLING:
-		return (!isStateTrue && wasStateTrue) ? TRIGGER_OCCURRED : NO_TRIGGER_OCCURRED;
-	case INVALID_MODE:
-		throw std::logic_error("Boolean trigger cannot be triggered if mode invalid");
+namespace maestro {
+	/*!
+	* \brief BooleanTrigger::update Updates the tracked signal (i.e. which causes the trigger) to the value specified
+	* \param isBrakeCurrentlyPressed Boolean describing if the boolean
+	* \param Unused time of measurement
+	* \return Value according to ::TriggerReturnCode_t
+	*/
+	Trigger::TriggerReturnCode_t BooleanTrigger::update(bool currentStateValue, struct timeval)
+	{
+		wasStateTrue = isStateTrue;
+		isStateTrue = currentStateValue;
+		wasTriggeredByLastUpdate = checkIfTriggered();
+		return wasTriggeredByLastUpdate;
 	}
-	return NO_TRIGGER_OCCURRED;
-}
 
+	/*!
+	* \brief BooleanTrigger::checkIfTriggered Check if the trigger has occurred based on the mode and state
+	* \return Value according to ::TriggerReturnCode_t
+	*/
+	Trigger::TriggerReturnCode_t BooleanTrigger::checkIfTriggered() const
+	{
+		switch (mode) {
+		case HIGH:
+			return isStateTrue ? TRIGGER_OCCURRED : NO_TRIGGER_OCCURRED;
+		case LOW:
+			return !isStateTrue ? TRIGGER_OCCURRED : NO_TRIGGER_OCCURRED;
+		case EDGE_ANY:
+			return (isStateTrue != wasStateTrue) ? TRIGGER_OCCURRED : NO_TRIGGER_OCCURRED;
+		case EDGE_RISING:
+			return (isStateTrue && !wasStateTrue) ? TRIGGER_OCCURRED : NO_TRIGGER_OCCURRED;
+		case EDGE_FALLING:
+			return (!isStateTrue && wasStateTrue) ? TRIGGER_OCCURRED : NO_TRIGGER_OCCURRED;
+		case INVALID_MODE:
+			throw std::logic_error("Boolean trigger cannot be triggered if mode invalid");
+		}
+		return NO_TRIGGER_OCCURRED;
+	}
+
+}
 
