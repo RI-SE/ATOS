a34cc43af7fad39f8eee399f39ad8244790b3d46
Samuel Thorén <samuel.thoren@astazero.com>
2022-09-23 11:06:40 +0200

    Updated CMakeLists

--- modules/OSIAdapter/inc/osiadapter.hpp
+++ modules/OSIAdapter/inc/osiadapter.hpp
@@ -3,20 +3,19 @@
 #include <future>
 
 #include "module.hpp"
+#include "osi_handler.hpp"
+#include "server.hpp"
 
 class OSIAdapter : public Module
 {
-
-  rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher;
-  rclcpp::TimerBase::SharedPtr timer;
-
   public:
     int initialize();
     OSIAdapter();
 
   private:
-    void sendPosition();
+    void sendPositionOSI();
     static inline std::string const moduleName = "osi_adapter";
     void onAbortMessage(const ROSChannels::Abort::message_type::SharedPtr) override;
-
+    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher;
+    rclcpp::TimerBase::SharedPtr timer;
 };
--- modules/OSIAdapter/src/osiadapter.cpp
+++ modules/OSIAdapter/src/osiadapter.cpp
@@ -2,6 +2,7 @@
 #include <chrono>
 
 #include "util.h"
+#include "osi_handler.hpp"
 #include "osiadapter.hpp"
 
 
@@ -13,19 +14,25 @@ OSIAdapter::OSIAdapter() :
   {
     initialize();
     publisher = this->create_publisher<std_msgs::msg::String>("position", 10);
-    timer = this->create_wall_timer(500ms, std::bind(&OSIAdapter::sendPosition, this));  
+    timer = this->create_wall_timer(500ms, std::bind(&OSIAdapter::sendPositionOSI, this));  
   };
 
 
 int OSIAdapter::initialize() {
   RCLCPP_INFO(get_logger(), "%s task running with PID %d", get_name(), getpid());
+
+  // make socket to driver model
+  struct sockaddr_in addr;
+  
+
+
+
   return 0;
 }
 
-void OSIAdapter::sendPosition() {
+
+void OSIAdapter::sendPositionOSI() {
   RCLCPP_INFO(get_logger(), "Sending position - testing!");
-
-
 }
 
 void OSIAdapter::onAbortMessage(const Abort::message_type::SharedPtr) {
