ebd8593415584623fe8878b88b73664b07ca71b3
Samuel Thorén <samuel.thoren@astazero.com>
2023-02-02 10:59:47 +0100

    Even smaller fixes

--- modules/OSIAdapter/src/server.cpp
+++ modules/OSIAdapter/src/server.cpp
@@ -2,7 +2,6 @@
 
 
 Server::Server(const std::string address, const uint16_t port, rclcpp::Logger logger) : Loggable(logger) {
-
   this->address = address;
   this->port = port;
   io_service = std::make_shared<boost::asio::io_service>();
