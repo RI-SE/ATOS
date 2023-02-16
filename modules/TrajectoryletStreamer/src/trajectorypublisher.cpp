370675322d9237a842ff44e042ffea7ffb5f20b6
Victor Jarlow <victor.jarlow@astazero.com>
2023-02-16 15:06:30 +0100

    added linceses to OBC states and trajectorylet Streamer

--- modules/TrajectoryletStreamer/src/trajectorypublisher.cpp
+++ modules/TrajectoryletStreamer/src/trajectorypublisher.cpp
@@ -1,3 +1,8 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
 #include "trajectorypublisher.hpp"
 #include <tf2/LinearMath/Quaternion.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
