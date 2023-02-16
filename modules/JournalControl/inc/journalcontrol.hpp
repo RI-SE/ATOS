69644bde27cde634aadfac6fa27fc4d885b4cd14
Lukas <lukas.wikander@astazero.com>
2023-02-16 15:00:34 +0100

    Update modules/JournalControl/inc/journalcontrol.hpp

--- modules/JournalControl/inc/journalcontrol.hpp
+++ modules/JournalControl/inc/journalcontrol.hpp
@@ -3,9 +3,6 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
-/*------------------------------------------------------------------------------
-  -- Copyright   : (C) 2020 AstaZero
-  ------------------------------------------------------------------------------
   -- File        : journalcontrol.h
   -- Author      : Lukas Wikander
   -- Description :
