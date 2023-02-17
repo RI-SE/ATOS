5f3023df4324d2734bbf33eb7249fcfbe9806f13
Lukas Wikander <lukas.wikander@astazero.com>
2023-02-17 09:53:52 +0100

    Indentation

--- modules/TrajectoryletStreamer/src/trajectoryletstreamer.cpp
+++ modules/TrajectoryletStreamer/src/trajectoryletstreamer.cpp
@@ -4,92 +4,93 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
 #include "trajectoryletstreamer.hpp"
-#include "atos_interfaces/srv/get_object_ids.hpp"
 #include <algorithm>
+#include "atos_interfaces/srv/get_object_ids.hpp"
 
 using namespace ATOS;
 using namespace ROSChannels;
 using std::placeholders::_1;
 
 TrajectoryletStreamer::TrajectoryletStreamer()
-    : Module(TrajectoryletStreamer::moduleName),
-    initSub(*this, std::bind(&TrajectoryletStreamer::onInitMessage, this, _1)),
-    connectedSub(*this, std::bind(&TrajectoryletStreamer::onObjectsConnectedMessage, this, _1)),
-    startSub(*this, std::bind(&TrajectoryletStreamer::onStartMessage, this, _1)),
-    stopSub(*this, std::bind(&TrajectoryletStreamer::onStopMessage, this, _1)),
-    abortSub(*this, std::bind(&TrajectoryletStreamer::onAbortMessage, this, _1))
-{
-    declare_parameter("chunk_duration", 0.0);
-    idClient = create_client<atos_interfaces::srv::GetObjectIds>(ServiceNames::getObjectIds);
-	trajectoryClient = create_client<atos_interfaces::srv::GetObjectTrajectory>(ServiceNames::getObjectTrajectory);
+	: Module(TrajectoryletStreamer::moduleName),
+	  initSub(*this, std::bind(&TrajectoryletStreamer::onInitMessage, this, _1)),
+	  connectedSub(*this, std::bind(&TrajectoryletStreamer::onObjectsConnectedMessage, this, _1)),
+	  startSub(*this, std::bind(&TrajectoryletStreamer::onStartMessage, this, _1)),
+	  stopSub(*this, std::bind(&TrajectoryletStreamer::onStopMessage, this, _1)),
+	  abortSub(*this, std::bind(&TrajectoryletStreamer::onAbortMessage, this, _1)) {
+	declare_parameter("chunk_duration", 0.0);
+	idClient = create_client<atos_interfaces::srv::GetObjectIds>(ServiceNames::getObjectIds);
+	trajectoryClient
+		= create_client<atos_interfaces::srv::GetObjectTrajectory>(ServiceNames::getObjectTrajectory);
 }
 
 void TrajectoryletStreamer::onInitMessage(const std_msgs::msg::Empty::SharedPtr) {
-    // Parse trajectory files into trajectories
-    loadObjectFiles();
+	// Parse trajectory files into trajectories
+	loadObjectFiles();
 }
 
 void TrajectoryletStreamer::onObjectsConnectedMessage(const ObjectsConnected::message_type::SharedPtr msg) {
-    // TODO setup and first chunk transmission
-    RCLCPP_INFO(get_logger(), "Starting trajectory publishers");
-    for (const auto& [id, traj] : trajectories) {
-        publishers.emplace_back(*this, *traj, id, chunkLength);
-    }
+	// TODO setup and first chunk transmission
+	RCLCPP_INFO(get_logger(), "Starting trajectory publishers");
+	for (const auto& [id, traj] : trajectories) {
+		publishers.emplace_back(*this, *traj, id, chunkLength);
+	}
 }
 
 void TrajectoryletStreamer::onStartMessage(const std_msgs::msg::Empty::SharedPtr) {
-    for (auto& pub : publishers) {
-        pub.handleStart();
-    }
+	for (auto& pub : publishers) {
+		pub.handleStart();
+	}
 }
 
 void TrajectoryletStreamer::loadObjectFiles() {
 	clearScenario();
-    double res = 0.0;
-    auto success = get_parameter("chunk_duration", res);
-    if (!success) {
-        RCLCPP_ERROR(get_logger(), "Could not get parameter chunk_duration");
-        return;
-    }
-    else {
-        chunkLength = std::chrono::milliseconds(static_cast<int>(res * 1000.0));
-        RCLCPP_INFO(get_logger(), "Chunk duration: %d ms", chunkLength.count());
-    }
-    RCLCPP_INFO(get_logger(), "Loading trajectories");
-    auto idsCallback = [&](const rclcpp::Client<atos_interfaces::srv::GetObjectIds>::SharedFuture future) {
-        auto idResponse = future.get();
-        for (const auto id : idResponse->ids) {
-            auto trajectoryCallback = [this](const rclcpp::Client<atos_interfaces::srv::GetObjectTrajectory>::SharedFuture future) {
-                RCLCPP_INFO(get_logger(), "Got trajectory");
-				auto trajResponse = future.get();
-				if (!trajResponse->success) {
-					RCLCPP_ERROR(get_logger(), "Get trajectory service call failed for object %u", trajResponse->id);
-					return;
-				}
-				ATOS::Trajectory traj(get_logger());
-				traj.initializeFromCartesianTrajectory(trajResponse->trajectory);
-				trajectories[trajResponse->id] = std::make_unique<ATOS::Trajectory>(traj);
-				RCLCPP_INFO(get_logger(), "Loaded trajectory for object %u with %d points", trajResponse->id, trajectories[trajResponse->id]->size());
-			};
+	double res = 0.0;
+	auto success = get_parameter("chunk_duration", res);
+	if (!success) {
+		RCLCPP_ERROR(get_logger(), "Could not get parameter chunk_duration");
+		return;
+	} else {
+		chunkLength = std::chrono::milliseconds(static_cast<int>(res * 1000.0));
+		RCLCPP_INFO(get_logger(), "Chunk duration: %d ms", chunkLength.count());
+	}
+	RCLCPP_INFO(get_logger(), "Loading trajectories");
+	auto idsCallback = [&](const rclcpp::Client<atos_interfaces::srv::GetObjectIds>::SharedFuture future) {
+		auto idResponse = future.get();
+		for (const auto id : idResponse->ids) {
+			auto trajectoryCallback =
+				[this](const rclcpp::Client<atos_interfaces::srv::GetObjectTrajectory>::SharedFuture future) {
+					RCLCPP_INFO(get_logger(), "Got trajectory");
+					auto trajResponse = future.get();
+					if (!trajResponse->success) {
+						RCLCPP_ERROR(get_logger(), "Get trajectory service call failed for object %u",
+									 trajResponse->id);
+						return;
+					}
+					ATOS::Trajectory traj(get_logger());
+					traj.initializeFromCartesianTrajectory(trajResponse->trajectory);
+					trajectories[trajResponse->id] = std::make_unique<ATOS::Trajectory>(traj);
+					RCLCPP_INFO(get_logger(), "Loaded trajectory for object %u with %d points",
+								trajResponse->id, trajectories[trajResponse->id]->size());
+				};
 			auto trajRequest = std::make_shared<atos_interfaces::srv::GetObjectTrajectory::Request>();
 			trajRequest->id = id;
 			trajectoryClient->async_send_request(trajRequest, trajectoryCallback);
-        }
-    };
+		}
+	};
 	auto request = std::make_shared<atos_interfaces::srv::GetObjectIds::Request>();
 	idClient->async_send_request(request, idsCallback);
 }
 
-
 void TrajectoryletStreamer::clearScenario() {
-    publishers.clear();
+	publishers.clear();
 	trajectories.clear();
 }
 
 void TrajectoryletStreamer::onAbortMessage(const ROSChannels::Abort::message_type::SharedPtr) {
-    // TODO
+	// TODO
 }
 
 void TrajectoryletStreamer::onStopMessage(const ROSChannels::Stop::message_type::SharedPtr) {
-    // TODO
+	// TODO
 }
--- modules/TrajectoryletStreamer/src/trajectorypublisher.cpp
+++ modules/TrajectoryletStreamer/src/trajectorypublisher.cpp
@@ -12,95 +12,95 @@ using namespace ATOS;
 using namespace std::chrono_literals;
 
 TrajectoryPublisher::TrajectoryPublisher(
-    rclcpp::Node& node,
-    const Trajectory& _traj,
-    const uint32_t objectId,
-    const std::chrono::milliseconds chunkLength)
-    : traj(std::make_unique<const Trajectory>(_traj)),
-    pub(node, objectId),
-    lastPublishedChunk(traj->points.end(), traj->points.end()),
-    chunkLength(chunkLength)
+	rclcpp::Node& node,
+	const Trajectory& _traj,
+	const uint32_t objectId,
+	const std::chrono::milliseconds chunkLength)
+	: traj(std::make_unique<const Trajectory>(_traj)),
+	pub(node, objectId),
+	lastPublishedChunk(traj->points.end(), traj->points.end()),
+	chunkLength(chunkLength)
 {
-    timer = node.create_wall_timer(publishPeriod, std::bind(&TrajectoryPublisher::publishChunk, this));
+	timer = node.create_wall_timer(publishPeriod, std::bind(&TrajectoryPublisher::publishChunk, this));
 }
 
 void TrajectoryPublisher::publishChunk()
 {
-    using std::chrono::steady_clock;
-    Chunk chunk(traj->points.cbegin(), traj->points.cend());
-    auto now = steady_clock::now();
-    auto trajTimeOffset = now;
-    auto timeIntoTraj = steady_clock::duration(0);
-    if (startTime) {
-        timeIntoTraj = now-*startTime;
-        trajTimeOffset = *startTime;
-    }
-    if (chunkLength.count() > 0) {
-        chunk = extractChunk(timeIntoTraj, timeIntoTraj + chunkLength);
-    }
-    if (chunk != lastPublishedChunk ||
-        (chunkLength > 0ms && now - lastPublishTime > 1s)) {
-        auto trajMsg = chunkToPath(chunk, trajTimeOffset);
-        pub.publish(trajMsg);
-        lastPublishedChunk = chunk;
-        lastPublishTime = now;
-    }
+	using std::chrono::steady_clock;
+	Chunk chunk(traj->points.cbegin(), traj->points.cend());
+	auto now = steady_clock::now();
+	auto trajTimeOffset = now;
+	auto timeIntoTraj = steady_clock::duration(0);
+	if (startTime) {
+		timeIntoTraj = now-*startTime;
+		trajTimeOffset = *startTime;
+	}
+	if (chunkLength.count() > 0) {
+		chunk = extractChunk(timeIntoTraj, timeIntoTraj + chunkLength);
+	}
+	if (chunk != lastPublishedChunk ||
+		(chunkLength > 0ms && now - lastPublishTime > 1s)) {
+		auto trajMsg = chunkToPath(chunk, trajTimeOffset);
+		pub.publish(trajMsg);
+		lastPublishedChunk = chunk;
+		lastPublishTime = now;
+	}
 }
 
 
 void TrajectoryPublisher::handleStart() {
-    using std::chrono::steady_clock;
-    startTime.reset(new steady_clock::time_point(steady_clock::now()));
+	using std::chrono::steady_clock;
+	startTime.reset(new steady_clock::time_point(steady_clock::now()));
 }
 
 
 TrajectoryPublisher::Chunk TrajectoryPublisher::extractChunk(
-    std::chrono::steady_clock::duration beginTime,
-    std::chrono::steady_clock::duration endTime)
+	std::chrono::steady_clock::duration beginTime,
+	std::chrono::steady_clock::duration endTime)
 {
-    using std::chrono::steady_clock;
-    auto ret = Chunk(traj->points.cbegin(), traj->points.cend());
+	using std::chrono::steady_clock;
+	auto ret = Chunk(traj->points.cbegin(), traj->points.cend());
 
-    ret.first = std::upper_bound(traj->points.cbegin(), traj->points.cend(), beginTime,
-        [](const steady_clock::duration& dur, const Trajectory::TrajectoryPoint& pt) {
-            return pt.getTime() > dur;
-        });
-    if (ret.first == traj->points.cend()) {
-        return ret;
-    }
-    ret.second = std::upper_bound(ret.first, traj->points.cend(), endTime,
-        [](const steady_clock::duration& dur, const Trajectory::TrajectoryPoint& pt) {
-            return pt.getTime() > dur;
-        }
-    );
-    return ret;
+	ret.first = std::upper_bound(traj->points.cbegin(), traj->points.cend(), beginTime,
+		[](const steady_clock::duration& dur, const Trajectory::TrajectoryPoint& pt) {
+			return pt.getTime() > dur;
+		});
+	if (ret.first == traj->points.cend()) {
+		return ret;
+	}
+	ret.second = std::upper_bound(ret.first, traj->points.cend(), endTime,
+		[](const steady_clock::duration& dur, const Trajectory::TrajectoryPoint& pt) {
+			return pt.getTime() > dur;
+		}
+	);
+	return ret;
 }
 
 nav_msgs::msg::Path TrajectoryPublisher::chunkToPath(
-    Chunk chunk,
-    std::chrono::steady_clock::time_point relTimeOffset)
+	Chunk chunk,
+	std::chrono::steady_clock::time_point relTimeOffset)
 {
-    nav_msgs::msg::Path ret;
-    ret.header.frame_id = "map";
-    auto rosTimeOffset = rclcpp::Time(relTimeOffset.time_since_epoch().count());
-    ret.header.stamp = rosTimeOffset;
+	nav_msgs::msg::Path ret;
+	ret.header.frame_id = "map";
+	auto rosTimeOffset = rclcpp::Time(relTimeOffset.time_since_epoch().count());
+	ret.header.stamp = rosTimeOffset;
 
-    std::transform(chunk.first, chunk.second, std::back_inserter(ret.poses),
-        [&](const Trajectory::TrajectoryPoint& pt){
-            geometry_msgs::msg::PoseStamped pose;
-            pose.header.stamp = rosTimeOffset + rclcpp::Duration(pt.getTime());
-            pose.pose.position.x = pt.getXCoord();
-            pose.pose.position.y = pt.getYCoord();
-            pose.pose.position.z = pt.getZCoord();
-            tf2::Quaternion q;
-            q.setRPY(0, 0, pt.getHeading());
-            tf2::convert(q, pose.pose.orientation);
-            return pose;
-        }
-    );
-    // Force same coordinate frame as header
-    for (auto& pose : ret.poses) {
-        pose.header.frame_id = ret.header.frame_id;
-    }
-    return ret;
+	std::transform(chunk.first, chunk.second, std::back_inserter(ret.poses),
+		[&](const Trajectory::TrajectoryPoint& pt){
+			geometry_msgs::msg::PoseStamped pose;
+			pose.header.stamp = rosTimeOffset + rclcpp::Duration(pt.getTime());
+			pose.pose.position.x = pt.getXCoord();
+			pose.pose.position.y = pt.getYCoord();
+			pose.pose.position.z = pt.getZCoord();
+			tf2::Quaternion q;
+			q.setRPY(0, 0, pt.getHeading());
+			tf2::convert(q, pose.pose.orientation);
+			return pose;
+		}
+	);
+	// Force same coordinate frame as header
+	for (auto& pose : ret.poses) {
+		pose.header.frame_id = ret.header.frame_id;
+	}
+	return ret;
 }
