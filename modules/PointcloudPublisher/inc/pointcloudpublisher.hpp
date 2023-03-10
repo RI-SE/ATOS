2002c65392b06ac88830d56ecba1656e6f7a8e4f
Samuel Thorén <samuel.thoren@astazero.com>
2023-03-10 13:11:26 +0100

    Publishing an empty pointcloud message

--- modules/PointcloudPublisher/inc/pointcloudpublisher.hpp
+++ modules/PointcloudPublisher/inc/pointcloudpublisher.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "module.hpp"
+#include <sensor_msgs/msg/point_cloud2.hpp>
 
 
 
@@ -12,6 +13,16 @@ class PointcloudPublisher : public Module {
 
   private:
     static inline std::string const moduleName = "pointcloud_publisher";
+    std::string pointcloudFile;
+    sensor_msgs::msg::PointCloud2 msg;
 
-  
+    std::shared_ptr<rclcpp::Publisher<sensor_msgs::msg::PointCloud2>> publisher;
+    std::shared_ptr<rclcpp::TimerBase> timer;
+
+
+    void initialize();
+    void getPointcloudFile();
+    void loadPointCloud();
+    void createPointcloudMessage();
+    void publishPointcloud();
 };
--- modules/PointcloudPublisher/src/pointcloudpublisher.cpp
+++ modules/PointcloudPublisher/src/pointcloudpublisher.cpp
@@ -5,10 +5,45 @@
  */
 
 #include "pointcloudpublisher.hpp"
-
+#include <chrono>
+using namespace std::chrono_literals;
 
 PointcloudPublisher::PointcloudPublisher() : Module(PointcloudPublisher::moduleName) {
+  initialize();
+  RCLCPP_INFO(get_logger(), "POINTCLOUD FILE: %s", pointcloudFile.c_str());
+
+  publisher = this->create_publisher<sensor_msgs::msg::PointCloud2>("/atos/pointcloud", 1);
+  timer = this->create_wall_timer(500ms, std::bind(&PointcloudPublisher::publishPointcloud, this));
 
 }
 
 PointcloudPublisher::~PointcloudPublisher() {}
+
+
+void PointcloudPublisher::initialize() {
+  getPointcloudFile();
+  createPointcloudMessage();
+  loadPointCloud();
+}
+
+void PointcloudPublisher::getPointcloudFile() {
+  declare_parameter("pointcloud_file");
+  get_parameter("pointcloud_file", pointcloudFile);
+  const std::string homeDir = getenv("HOME");
+  pointcloudFile = homeDir + "/.astazero/ATOS/pointclouds/" + pointcloudFile;
+}
+
+void PointcloudPublisher::loadPointCloud() {
+  
+}
+
+
+void PointcloudPublisher::createPointcloudMessage() {
+  msg.header.frame_id = "map";
+  msg.header.stamp = this->get_clock()->now();
+}
+
+
+void PointcloudPublisher::publishPointcloud() {
+  publisher->publish(msg);
+}
