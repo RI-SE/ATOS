b1aed8953ad9d4994928240b47db5cae107e897b
Robert Brenick <robert.brenick@astazero.com>
2023-03-22 13:47:10 +0100

    WIP: Read the docs walking skeleton

---
+++ doc/source/conf.py
@@ -0,0 +1,45 @@
+
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information
+
+project = 'ATOS Docs'
+copyright = '2023, Astazero'
+author = 'Astazero'
+
+release = '0.1'
+version = '0.1.0'
+
+# -- General configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+]
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3/', None),
+    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+}
+intersphinx_disabled_domains = ['std']
+
+templates_path = ['_templates']
+
+# -- For markdown support
+extensions = ["myst_parser"]
+
+# -- Options for HTML output
+
+html_logo = "ATOS_icon.png"
+html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+    'logo_only': True,
+    'display_version': False,
+}
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
+
