409b6389888ab0aab4e53d860d1994cb95f89a2c
Robert Brenick <robert.brenick@astazero.com>
2023-03-27 15:49:56 +0200

    Print version on start up
    Also correct copy-paste error

--- launch/launch_utils/launch_base.py
+++ launch/launch_utils/launch_base.py
@@ -5,11 +5,16 @@ from ament_index_python.packages import get_package_prefix
 import subprocess
 from pathlib import Path
 from .validate_files import validate_files
+import rclpy.logging as logging
 
 def get_base_nodes():
     files = validate_files()
     atos_conf_dir = os.path.join(os.path.expanduser('~'), '.astazero', 'ATOS')
     atos_install_dir = get_package_prefix('atos')
+
+    # Print ATOS version
+    version_file = open(atos_install_dir / Path(".VERSION"), 'r')
+    logging.get_logger('launch').info("ATOS version: " + version_file.read())
 
     # control-gui logging
     control_gui_log = open(atos_conf_dir / Path("webgui.log"), 'w')
