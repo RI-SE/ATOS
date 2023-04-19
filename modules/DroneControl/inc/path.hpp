2bc4aeeb85d60f4b0044481c6e954a61d10a60f5
dandeandean <dpd5518@psu.edu>
2023-04-19 11:46:17 -0400

    added in the class definitions

--- modules/DroneControl/inc/path.hpp
+++ modules/DroneControl/inc/path.hpp
@@ -2,22 +2,36 @@
 
 #include <vector>
 #include <string>
+#include <iterator>
+#include "tinyxml2.h"
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <string>
 
-namespace ABD 
+namespace ABD
 {
-    class Segment{
-        public:
-            Segment();
-            ~Segment() = default;
+    class Segment
+    {
+    public:
+        Segment();
+        ~Segment() = default;
     };
 
-    class Path{
-        public:
-            Path(const std::string& path);
-            ~Path() = default;
-
-        private:
-            std::vector<Segment> segments;
+    class Path
+    {
+    public:
+        Path(const std::string &path);
+        ~Path() = default;
+        float OriginLatitiude;
+        float OriginLongitude;
+        float OriginAltitude;
+        float bearing;
+        std::string path_to_file;
+        void get_path(void);
+        void get_traj(float x_offset, float y_offset, float z_offset);
+        std::list<Segment> segmentsList;
+        std::list<Segment> dronePath;
     };
 
 }
