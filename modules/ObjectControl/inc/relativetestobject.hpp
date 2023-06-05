8eeec82af24325a5caf8ead0bbbc4ecff94a7cfd
Victor Jarlow <victor.jarlow@astazero.com>
2023-06-05 14:32:43 +0200

    add a RelativeTestObject class

---
+++ modules/ObjectControl/inc/relativetestobject.hpp
@@ -0,0 +1,19 @@
+#include "testobject.hpp"
+
+class RelativeTestObject : public TestObject {
+public:
+	RelativeTestObject(uint32_t id, uint32_t anchorID);
+	RelativeTestObject(const RelativeTestObject&) = delete;
+	//RelativeTestObject(RelativeTestObject&&);
+
+	RelativeTestObject& operator=(const RelativeTestObject&) = delete;
+	RelativeTestObject& operator=(RelativeTestObject&&) = default;
+
+private:
+    uint32_t anchor;
+    bool isAnchor;
+    ObjectMonitorType transformCoordinate(const ObjectMonitorType& point,
+        const ObjectMonitorType& anchor,
+        const bool debug);
+    MonitorMessage readMonitorMessage() override;
+};
