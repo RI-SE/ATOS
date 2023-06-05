d44844b582c7d0947e5baa3f0fe6303f24039fa3
Victor Jarlow <victor.jarlow@astazero.com>
2023-06-05 14:31:59 +0200

    move ros2 message specific parsing to roschannels

--- common/roschannels/monitorchannel.hpp
+++ common/roschannels/monitorchannel.hpp
@@ -7,6 +7,9 @@
 
 #include "roschannel.hpp"
 #include "atos_interfaces/msg/monitor.hpp"
+#include "positioning.h"
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
 
 namespace ROSChannels {
     namespace Monitor {
@@ -29,5 +32,49 @@ namespace ROSChannels {
                 objectId(id),
                 BaseSub<message_type>(node, "object_" + std::to_string(id) + "/" + topicName, callback, qos) {}
         };
+        static message_type fromMonr(const uint32_t id, const ObjectMonitorType& monrMessage) {
+            atos_interfaces::msg::Monitor msg;
+            auto txid = id;
+            auto indata = monrMessage;
+            auto stamp = rclcpp::Time(indata.timestamp.tv_sec, indata.timestamp.tv_usec*1000);
+            
+            // Set stamp for all subtypes
+            msg.atos_header.header.stamp = stamp;
+            msg.pose.header.stamp = stamp;
+            msg.velocity.header.stamp = stamp;
+            msg.acceleration.header.stamp = stamp;
+
+            // Set frame ids
+            msg.atos_header.header.frame_id = "map"; // TODO
+            msg.pose.header.frame_id = "map"; // TODO
+            msg.velocity.header.frame_id = "map"; // TODO vehicle local
+            msg.acceleration.header.frame_id = "map"; // TODO vehicle local
+
+            msg.atos_header.object_id = txid;
+            msg.object_state.state = indata.state;
+            if (indata.position.isPositionValid) {
+                msg.pose.pose.position.x = indata.position.xCoord_m;
+                msg.pose.pose.position.y = indata.position.yCoord_m;
+                msg.pose.pose.position.z = indata.position.isZcoordValid ? indata.position.zCoord_m : 0.0;
+            }
+            if (indata.position.isHeadingValid) {
+                tf2::Quaternion orientation;
+                orientation.setRPY(0, 0, indata.position.heading_rad);
+                msg.pose.pose.orientation = tf2::toMsg(orientation);
+            }
+            msg.velocity.twist.linear.x = indata.speed.isLongitudinalValid ? indata.speed.longitudinal_m_s : 0;
+            msg.velocity.twist.linear.y = indata.speed.isLateralValid ? indata.speed.lateral_m_s : 0;
+            msg.velocity.twist.linear.z = 0;
+            msg.velocity.twist.angular.x = 0;
+            msg.velocity.twist.angular.y = 0;
+            msg.velocity.twist.angular.z = 0;
+            msg.acceleration.accel.linear.x = indata.acceleration.isLongitudinalValid ? indata.acceleration.longitudinal_m_s2 : 0;
+            msg.acceleration.accel.linear.y = indata.acceleration.isLateralValid ? indata.acceleration.lateral_m_s2 : 0;
+            msg.acceleration.accel.linear.z = 0;
+            msg.acceleration.accel.angular.x = 0;
+            msg.acceleration.accel.angular.y = 0;
+            msg.acceleration.accel.angular.z = 0;
+            return msg;
+        }
     }
 }
