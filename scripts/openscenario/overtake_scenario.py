6410f01e2c4b86cca8dc23ea80995fc227067516
Sebastian Lindholm <sebastian.lindholm@astazero.com>
2023-08-17 14:14:54 +0200

    Adding gvt in the other lane.

--- scripts/openscenario/overtake_scenario.py
+++ scripts/openscenario/overtake_scenario.py
@@ -26,6 +26,14 @@ vut_brake_position = xosc.LanePosition(14, 0, -1, 1057000)
 vut_limit_position = xosc.LanePosition(14, 0, -1, 1057000)
 
 # GVT
+gvt_speed = 25 / 3.6  # m/s
+gvt_acceleration = 1.6  # m/s^2
+gvt_retardation = 1.75  # m/s^2
+gvt_starting_point = xosc.LanePosition(675, 0, 2, 1055000)
+gvt_brake_position = xosc.LanePosition(14, 0, 1, 1057000)
+gvt_limit_position = xosc.LanePosition(14, 0, 1, 1057000)
+
+
 
 # Preamble
 init = xosc.Init()
@@ -109,14 +117,90 @@ vut_move_event.add_action(VUT_ID + ",start_follow_trajectory", vut_follow_trajec
 vut_move_event.add_action(VUT_ID + ",set_speed", vut_set_speed)
 vut_maneuver.add_event(vut_move_event)
 
-# ------------------------ GVT ------------------------
+# ----------------------VUT end------------------------------
 
+# ------------------------ GVT ------------------------
 # Add GVT start position, trajectory etc here. See above for VUT example
+
+gvt_maneuver_group = xosc.ManeuverGroup(GVT_ID + ",maneuver_group")
+gvt_maneuver = xosc.Maneuver(GVT_ID + ",maneuver")
+gvt_maneuver_group.add_actor(GVT_ID)
+
+osc_entities.add_scenario_object(GVT_ID, xosc.CatalogReference(CATALOG_NAME, "car_red"))
+
+gvt_start_teleport = xosc.TeleportAction(gvt_starting_point)
+
+init.add_init_action(GVT_ID, gvt_start_teleport)
+
+# Actions and Events
+
+# GVT movement profile:
+# accelerate to top speed,
+# follow a trajectory,
+# then decelerate to a stop
+times = []
+positions = [
+    gvt_starting_point,
+    gvt_limit_position
+
+]
+polyline = xosc.Polyline(times, positions)
+trajectory = xosc.Trajectory(GVT_ID + ",start_follow_trajectory", closed=False)
+trajectory.add_shape(polyline)
+gvt_follow_trajectory = xosc.FollowTrajectoryAction(
+    trajectory,
+    xosc.FollowingMode.position,
+    xosc.ReferenceContext.relative,
+    1,
+    0
+)
+gvt_set_speed = xosc.AbsoluteSpeedAction(
+    gvt_speed,
+    xosc.TransitionDynamics(
+        xosc.DynamicsShapes.linear,
+        xosc.DynamicsDimension.rate,
+        gvt_acceleration
+    ),
+)
+gvt_brake = xosc.AbsoluteSpeedAction(
+    0,
+    xosc.TransitionDynamics(
+        xosc.DynamicsShapes.linear,
+        xosc.DynamicsDimension.rate,
+        -gvt_retardation
+    ),
+)
+gvt_reach_stop_position = xosc.EntityTrigger(
+    name=GVT_ID + ",reach_stop_position",
+    delay=0,
+    conditionedge=xosc.ConditionEdge.none,
+    entitycondition=xosc.ReachPositionCondition(gvt_brake_position, 1),
+    triggerentity=GVT_ID
+)
+gvt_brake_event = xosc.Event(
+    GVT_ID + ",brake_event",
+    xosc.Priority.parallel
+)
+gvt_brake_event.add_trigger(gvt_reach_stop_position)
+gvt_brake_event.add_action(GVT_ID + ",brake_to_stop", gvt_brake)
+gvt_maneuver.add_event(gvt_brake_event)
+
+gvt_move_event = xosc.Event(
+    GVT_ID + ",start_move_event",
+    xosc.Priority.parallel
+)
+gvt_move_event.add_action(GVT_ID + ",start_follow_trajectory", gvt_follow_trajectory)
+gvt_move_event.add_action(GVT_ID + ",set_speed", gvt_set_speed)
+gvt_maneuver.add_event(gvt_move_event)
+
+# ----------------------GVT end------------------------------
 
 # Collect into a scenario and write to file
 vut_maneuver_group.add_maneuver(vut_maneuver)
+gvt_maneuver_group.add_maneuver(gvt_maneuver)
 
 osc_act.add_maneuver_group(vut_maneuver_group)
+osc_act.add_maneuver_group(gvt_maneuver_group)
 
 osc_storyboard.add_act(osc_act)
 osc_file = xosc.Scenario(
