be914bb9854de53e1c2356e2663fb8010229678e
Robert Brenick <robert.brenick@astazero.com>
2023-10-09 22:17:40 +0200

    Divide install scripts into smaller files

---
+++ scripts/install_atos.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+
+# Check if called without arguments
+if [ $# -eq 0 ]; then
+    echo "Don't call this file directly, use setup_atos.sh instead."
+    echo "Usage: ./install_deps.sh <path to ATOS git repo>"           
+    exit 0
+fi
+
+# Take the first argument as the PATH to the ATOS git repo
+ATOS_REPO_PATH="$1"
+source "${ATOS_REPO_PATH}/scripts/install_functions.sh"
+
+################################################
+###### Install Control Panel dependencies ######
+################################################
+
+echo "Installing nvm and Node.js..."
+if [ "$ROS_DISTRO" == "humble" ]; then 
+    NODE_VERSION=16.20.0    # Also specified in the Dockerfile
+elif [ "$ROS_DISTRO" == "foxy" ]; then
+    NODE_VERSION=12
+fi
+NVM_DIR="$HOME/.nvm"
+
+if ! command -v nvm &> /dev/null; then
+    echo "nvm not found, installing..."
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+    . "$NVM_DIR/nvm.sh"
+    check_command_failed $? "Failed to install nvm."
+else 
+    echo "nvm already installed, skipping installation..."
+fi
+
+echo "Installing Node.js v${NODE_VERSION}..."
+nvm install ${NODE_VERSION}
+nvm use v${NODE_VERSION}
+nvm alias default v${NODE_VERSION}
+echo 'export PATH="$HOME/.nvm/versions/node/v${NODE_VERSION}/bin/:$PATH"' >> $HOME/.bashrc
+source $HOME/.bashrc
+npm --version
+
+# Install pyOpenSSL
+pip install pyOpenSSL
+
+################################################
+############## Install ATOS ####################
+################################################
+
+# Create a workspace dir if it doesn't exist
+if [ ! -d "$HOME/atos_ws/src" ]; then
+    mkdir -p $HOME/atos_ws/src
+fi
+cd $HOME/atos_ws
+
+# Set ATOS_INTERFACES_PATH using ATOS_PATH
+ATOS_INTERFACES_PATH="$ATOS_REPO_PATH/atos_interfaces"
+
+# Update symlinks to atos and atos_interfaces
+update_symlink "$ATOS_REPO_PATH" $HOME/atos_ws/src/atos
+update_symlink "$ATOS_INTERFACES_PATH" $HOME/atos_ws/src/atos_interfaces
+
+
+# Change directory into the workspace and build, check with the user before continuing
+echo "Dependecy installation done and ATOS workspace created."
+
+# First make sure the submodules are up to date
+echo "Updating submodules to make sure they are up to date..."
+cd $HOME/atos_ws/src/atos
+git submodule update --init --recursive
+cd -
+
+# temporarily cd into the workspace and build with colcon
+echo "Building ATOS..."
+cd $HOME/atos_ws
+source /opt/ros/$ROS_DISTRO/setup.bash
+MAKEFLAGS=-j4 && colcon build --symlink-install
+check_command_failed $? "Failed to build ATOS."
+cd -
+
+#####################################
+###### Configure setup scripts ######
+#####################################
+
+atos_setup_script="source $HOME/atos_ws/install/setup."
+ros2_setup_script="source /opt/ros/$ROS_DISTRO/setup."
+
+case "$SHELL" in
+    */bash)
+        add_source_line_if_needed $HOME/.bashrc "bash" "${atos_setup_script}"
+        add_source_line_if_needed $HOME/.bashrc "bash" "${ros2_setup_script}"
+        source $HOME/.bashrc 
+    ;;
+    */zsh)
+        add_source_line_if_needed $HOME/.zshrc "zsh" "${atos_setup_script}"
+        add_source_line_if_needed $HOME/.zshrc "zsh" "${ros2_setup_script}"
+        source $HOME/.zshrc
+    ;;
+    *)
+        echo "Unsupported shell detected! Please use either bash or zsh shells to run ATOS"
+        exit 1
+    ;;
+esac
---
+++ setup_atos.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+#####################################
+###### Pre-installation checks ######
+#####################################
+
+source "scripts/install_functions.sh"
+
+# Get this file location
+REPO_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+
+# Check if running on Ubuntu
+if ! grep -q "Ubuntu" /etc/os-release; then
+    echo "This script is designed for Ubuntu systems only."
+    exit 1
+fi
+
+# Set ROS_DISTRO based on Ubuntu distribution
+case "$(get_ubuntu_codename)" in
+    "focal")
+        ROS_DISTRO=foxy
+    ;;
+    "jammy")
+        ROS_DISTRO=humble
+    ;;
+    *)
+        echo "Unsupported Ubuntu distribution. Only 20.04 (focal) and 22.04 (jammy) are supported."
+        exit 1
+    ;;
+esac
+
+# Add -h/--help option
+if [ "$1" == "-h" ] || [ "$1" == "--help" ]; then
+    echo "Usage: ./setup_atos.sh [single option]"
+    echo "This script will install all necessary dependencies, setup the ROS workspace at ~/atos_ws and install ATOS. Please open and inspect this script for further details."
+    echo "Your ROS distribution: $ROS_DISTRO"
+    #Show options
+    echo "Options:"
+    echo "  -h, --help      Show this help message and exit"
+    echo "  -d              Only install dependencies"              
+    exit 0
+# Add yes to all option
+elif [ "$1" == "-d" ]; then
+    INSTALL_ATOS=true
+else
+    INSTALL_ATOS=false
+fi
+
+#######################################
+###### Install ATOS dependencies ######
+#######################################
+echo "Installing ATOS dependencies..."
+${REPO_DIR}/scripts/install_deps.sh ${REPO_DIR}
+########################################
+###### Start installation of ATOS ######
+########################################
+if [ "$INSTALL_ATOS" = false ]; then
+    echo "ATOS installation skipped."
+    exit 0
+fi
+echo "Installing ATOS..."
+${REPO_DIR}/scripts/install_atos.sh ${REPO_DIR}
+
+echo "ATOS installation and setup is complete. You can now test run ATOS by running the following command:"
+echo "ros2 launch atos launch_basic.py"
+echo "Please see the documentation for further details: https://atos.readthedocs.io/en/latest/"
