67b3870c7342fd01ffa3c53340d7bc6bc14f2098
Robert Brenick <robert.brenick@astazero.com>
2023-11-02 16:19:32 +0100

    Run launch_integration_testing.py in test

--- launch/test_start_test.py
+++ launch/test_start_test.py
@@ -4,36 +4,48 @@ from ament_index_python.packages import get_package_prefix
 sys.path.insert(0, os.path.join(  # Need to modify the sys.path since we launch from the ros2 installed path
     get_package_prefix('atos'),
     'share', 'atos', 'launch'))
-
+import rclpy
+import signal
+import pytest
 import launch
+from launch import LaunchDescription
 import launch_pytest
 from launch_pytest.tools import process as process_tools
+import re
 
-import pytest
-import launch_integration_testing
-
+@pytest.fixture
+def integration_test_proc():
+    # Launch a process to test
+    return launch.actions.ExecuteProcess(
+        cmd=['ros2', 'launch', 'atos', 'launch_integration_testing.py'],
+        shell=True,
+        cached_output=True,
+    )
 
 # This function specifies the processes to be run for our test.
 @launch_pytest.fixture
-def launch_description(hello_world_proc):
+def launch_description(integration_test_proc):
     """Launch a simple process to print 'hello_world'."""
-    return launch_integration_testing.generate_launch_description() 
-    
+    return launch.LaunchDescription([
+        integration_test_proc,
+        launch_pytest.actions.ReadyToTest()
+    ])
 
 @pytest.mark.launch(fixture=launch_description)
-def test_read_stdout(hello_world_proc, launch_context):
-    """Check if 'hello_world' was found in the stdout."""
+def test_read_stdout(integration_test_proc, launch_context):
     def validate_output(output):
         # this function can use assertions to validate the output or return a boolean.
         # pytest generates easier to understand failures when assertions are used.
-        assert output.splitlines() == ['hello_world'], 'process never printed hello_world'
+        assert any(re.search('Scenario Execution result: NOK', line) for line in output.splitlines()), 'Scenario test failed'
     process_tools.assert_output_sync(
-        launch_context, hello_world_proc, validate_output, timeout=5)
-
-    def validate_output(output):
-        return output == 'this will never happen'
-    assert not process_tools.wait_for_output_sync(
-        launch_context, hello_world_proc, validate_output, timeout=0.1)
+        launch_context, integration_test_proc, validate_output, timeout=30)
     yield
     # this is executed after launch service shutdown
-    assert hello_world_proc.return_code == 0
+    assert False
+
+@pytest.fixture(scope='session', autouse=True)
+def term_handler():
+    orig = signal.signal(signal.SIGTERM, signal.getsignal(signal.SIGINT))
+    yield
+    signal.signal(signal.SIGTERM, orig)
+    
