cc451ac4804a1f0872fa0a6f4446aded2a3ceb58
SebastianSvanland <sebastian.svanland@astazero.com>
2023-12-12 09:24:55 +0100

    Added configpanel to index route.

--- gui/controlpanel/routes/index.js
+++ gui/controlpanel/routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Simple Control GUI for ATOS' });
+router.get('/controlpanel', function(req, res, next) {
+  res.render('controlpanel', { title: 'Simple Control GUI for ATOS' });
+});
+router.get('/configpanel', function(req, res, next) {
+  res.render('configpanel', { title: 'Simple Control GUI for ATOS' });
 });
 
 
