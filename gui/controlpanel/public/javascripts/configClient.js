3c5cdd894cb8bee866cbd6961eb78f968ed746d0
SebastianSvanland <sebastian.svanland@astazero.com>
2023-12-20 11:41:28 +0100

    Updated config client to use jsonForm lib.

--- gui/controlpanel/public/javascripts/configClient.js
+++ gui/controlpanel/public/javascripts/configClient.js
@@ -1,19 +1,88 @@
-const { set } = require("../../server");
+
+/**
+ * Loads the example identified by the given name
+ */
+function loadJsonFile(example) {
+  var jsonPlaceholder;
+  $.ajax({
+    url: 'json/' + example + '.json',
+    dataType: 'text',
+    async: false
+  }).done(function (code) {
+    console.log("Loading example: " + example);
+    jsonPlaceholder = code;
+  }).fail(function () {
+    $('#form').html('Sorry, I could not retrieve the example!');
+  });
+  return jsonPlaceholder
+};
+
+
+
+/**
+ * Displays the form entered by the user
+ * (this function runs whenever once per second whenever the user
+ * changes the contents of the ACE input field)
+ */
+function generateForm(jsonString) {
+  var createdForm = JSON.parse(jsonString)
+
+  // Reset form pane
+  $('#form').html('');
+
+  // Render the resulting form, binding to onSubmitValid
+  try {
+    createdForm.onSubmitValid = function (values) {
+      if (console && console.log) {
+        console.log('Values extracted from submitted form', values);
+      }
+      window.alert('Form submitted. Values object:\n' +
+        JSON.stringify(values, null, 2));
+    };
+    createdForm.onSubmit = function (errors, values) {
+      if (errors) {
+        console.log('Validation errors', errors);
+        return false;
+      }
+      return true;
+    };
+    $('#form').jsonForm(createdForm);
+  }
+  catch (e) {
+    $('#form').html('<pre>Entered content is not yet a valid' +
+      ' JSON Form object.\n\nThe JSON Form library returned:\n' +
+      e + '</pre>');
+    return;
+  }
+};
+
+// Wait until ACE is loaded
+var itv = window.setInterval(function() {
+  if (window.ace) {
+    var example = 'atos-param-schema';
+    window.clearInterval(itv);
+    const jsonPlaceholder = loadJsonFile(example);
+    generateForm(jsonPlaceholder);
+  }
+}, 1000);
 
 // Websocket connection
 var ws;
 
-function sendParam(param, value, ws){
-    var clientParam = new Object();
-    clientParam.msg_type = param;
-    clientParam.value = value;
+// function sendParam(param, value, ws){
+//     var clientParam = new Object();
+//     clientParam.msg_type = param;
+//     clientParam.value = value;
 
-    ws.send(JSON.stringify(clientParam));
-}
+//     ws.send(JSON.stringify(clientParam));
+// }
 
 // HTML-Button callbacks
-function requestParameters(request){
-    scenarioPath = request.getElementById('scenario_file_path').value;
+function requestParameters(parameters){
+    tmpName = parameters.Name;
+    console.log("tmpName: " + tmpName);
+    scenarioPath = parameters.ScenarioPath;
+    console.log("Sending scenario file: " + scenarioPath);
 }
 
 function sendScenarioParam(scenarioPath){
