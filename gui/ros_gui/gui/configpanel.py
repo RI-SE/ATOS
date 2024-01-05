99107ea21c9520deef5acf587d4f98417593289d
SebastianSvanland <sebastian.svanland@astazero.com>
2024-01-05 15:36:37 +0100

    General clean up

--- gui/ros_gui/gui/configpanel.py
+++ gui/ros_gui/gui/configpanel.py
@@ -14,7 +14,6 @@ QOS = rclpy.qos.QoSProfile(depth=10)
 
 MAX_TIMEOUT = 1
 
-# @router.page('/')
 class ConfigPanelNode(Node):
 
     def __init__(self) -> None:
@@ -189,8 +188,9 @@ def main() -> None:
     # NOTE: This function is defined as the ROS entry point in setup.py, but it's empty to enable NiceGUI auto-reloading
     pass
 
-
-
+#Starting the ros node in a thread managed by nicegui. It will restarted with "on_startup" after a reload.
+#It has to be in a thread, since NiceGUI wants the main thread for itself.
 app.on_startup(lambda: threading.Thread(target=ros_main).start())
+
 ui_run.APP_IMPORT_STRING = f'{__name__}:app'  # ROS2 uses a non-standard module name, so we need to specify it here
 ui.run(uvicorn_reload_dirs=str(Path(__file__).parent.resolve()), favicon='🤖', port=3001, title='Config Panel')
