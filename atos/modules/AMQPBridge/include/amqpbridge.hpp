a60fc39e11b49787d54eda96b836b47728198033
SebastianSvanland <sebastian.svanland@astazero.com>
2024-02-15 10:36:19 +0100

    Added shell for AMQPBridge.

---
+++ atos/modules/AMQPBridge/include/amqpbridge.hpp
@@ -0,0 +1,17 @@
+
+class AmqpBridge
+{
+private:
+    /* data */
+public:
+    AmqpBridge(/* args */);
+    ~AmqpBridge();
+};
+
+AmqpBridge::AmqpBridge(/* args */)
+{
+}
+
+AmqpBridge::~AmqpBridge()
+{
+}
