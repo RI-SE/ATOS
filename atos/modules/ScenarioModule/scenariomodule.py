4e0c4dbf24b1bf765b58a298ec358a2c712a465e
Robert Brenick <robert.brenick@astazero.com>
2024-07-10 13:11:39 +0200

    set/get IP for object

--- atos/modules/ScenarioModule/scenariomodule.py
+++ atos/modules/ScenarioModule/scenariomodule.py
@@ -21,8 +21,7 @@ class ScenarioObject:
     def __init__(self, name: str, catalog_ref: xosc.CatalogReference):
         self.name = name
         self.catalog_ref: xosc.CatalogReference = catalog_ref
-        self.id: int
-        self.ip: str
+        self.ip: str = None
 
 
 class ScenarioModule(Node):
@@ -40,82 +39,108 @@ class ScenarioModule(Node):
         self.get_object_ip_ = self.create_service(
             atos_interfaces.srv.GetObjectIp, "get_object_ip", self.srv_get_object_ip
         )
+        self.get_object_ip_ = self.create_service(
+            atos_interfaces.srv.SetObjectIp, "set_object_ip", self.srv_set_object_ip
+        )
 
         self.declare_parameter(SCENARIO_FILE_PARAMETER, "")
         self.declare_parameter(
             ACTIVE_OBJECT_NAME_PARAMETER, rclpy.Parameter.Type.STRING_ARRAY
         )
         self.add_on_set_parameters_callback(self.parameter_callback)
-        self.scenario_objects = []
+        self.active_objects = {}
         self.vehicle_catalog = None
 
     def init_callback(self, msg):
         self.get_logger().info("Init callback called")
+        self.update_scenario(self.get_parameter(SCENARIO_FILE_PARAMETER).value)
+        self.update_active_scenario_objects(
+            self.get_parameter(ACTIVE_OBJECT_NAME_PARAMETER).value
+        )
 
     def parameter_callback(self, params):
         for param in params:
             if param.name == SCENARIO_FILE_PARAMETER and param.value:
                 self.update_scenario(file_name=param.value)
             elif param.name == ACTIVE_OBJECT_NAME_PARAMETER:
-                self.update_active_scenario_objects(active_objects=param.value)
+                self.update_active_scenario_objects(active_objects_name=param.value)
         return SetParametersResult(successful=True)
 
     def update_scenario(self, file_name: str):
         self.scenario_file = path.join(ATOS_XOSC_DIR, file_name)
         self.get_logger().info("Loading scenario file: {}".format(self.scenario_file))
 
-    def update_active_scenario_objects(self, active_objects):
-        if len(active_objects) != len(set(active_objects)):
+    def update_active_scenario_objects(self, active_objects_name: List[str]):
+        if len(active_objects_name) != len(set(active_objects_name)):
             raise ValueError("Names must be unique")
-        self.scenario_objects = self.get_all_objects_in_scenario(self.scenario_file)
-        for name in active_objects:
-            if name not in [obj.name for obj in self.scenario_objects]:
-                raise ValueError("Name {} not found in scenario file".format(name))
-        # Set an unique id and get IP for each object
-        for i, obj in enumerate(self.scenario_objects):
-            obj.id = i + 1
-            self.get_logger().info(
-                "Assigning ID {} to object with name {}".format(obj.id, obj.name)
-            )
-            obj.ip = self.get_ip_property_for_object(obj)
+        scenario_objects = self.get_all_objects_in_scenario(self.scenario_file)
+
+        # Remove any object that have (now) inactive
+        for id, obj in self.active_objects.items():
+            if obj.name not in active_objects_name:
+                self.active_objects.pop(id)
+
+        # Add any object that have become active
+        for id, obj in scenario_objects.items():
+            if obj.name in active_objects_name and id not in self.active_objects:
+                self.active_objects[id] = obj
+                # Update the ip property of the object unless it is already set
+                if obj.ip is None:
+                    obj.ip = self.get_ip_property_for_object(obj)
 
     def get_all_objects_in_scenario(self, scenario_file) -> List[ScenarioObject]:
         scenario = xosc.ParseOpenScenario(scenario_file)
-        scenario_objects = [
-            ScenarioObject(
+        scenario_objects = {
+            id
+            + 1: ScenarioObject(
                 name=scenario_object.name,
                 catalog_ref=xosc.CatalogReference(
                     scenario_object.entityobject.catalogname,
                     scenario_object.entityobject.entryname,
                 ),
             )
-            for scenario_object in scenario.entities.scenario_objects
-        ]
+            for id, scenario_object in enumerate(scenario.entities.scenario_objects)
+        }
         return scenario_objects
 
     def get_ip_property_for_object(self, obj: ScenarioObject) -> str:
+        properties = self.get_properties_for_object(obj)
+        # Convert the list of tuples to a dict to get the ip property easily
+        return dict(properties)["ip"]
+
+    def get_properties_for_object(self, obj: ScenarioObject):
         catalog_path = xosc.ParseOpenScenario(self.scenario_file).catalog.catalogs.get(
             obj.catalog_ref.catalogname
         )
         catalog_object = xosc.xosc_reader.CatalogReader(
             obj.catalog_ref, path.join(ATOS_XOSC_DIR, catalog_path)
         )
-        # Convert the list of tuples to a dict to get the ip property easily
-        return dict(catalog_object.properties.properties)["ip"]
+        return catalog_object.properties.properties
 
     def srv_get_object_id_array(self, request, response):
         self.get_logger().info("Service get_object_id_array callback called")
-        active_object_names = self.get_parameter(ACTIVE_OBJECT_NAME_PARAMETER).value
-        ids = [
-            obj.id for obj in self.scenario_objects if obj.name in active_object_names
-        ]
+        ids = [id for id, _ in self.active_objects.items()]
         response.ids = ids
         response.success = True
         return response
 
     def srv_get_object_ip(self, request, response):
-        id = request.id
-        response.ip = [obj.ip for obj in self.scenario_objects if obj.id == id][0]
+        scenario_object = self.active_objects.get(request.id)
+        if scenario_object is None:
+            response.success = False
+            return response
+        response.ip = scenario_object.ip
+        response.success = True
+        return response
+
+    def srv_set_object_ip(self, request, response):
+        scenario_object = self.active_objects.get(request.id)
+        if scenario_object is None:
+            response.success = False
+            return response
+        scenario_object.ip = request.ip
+        response.success = True
+        return response
 
 
 def main(args=None):
--- atos/modules/ScenarioModule/tests/test_scenariomodule.py
+++ atos/modules/ScenarioModule/tests/test_scenariomodule.py
@@ -1,20 +1,13 @@
+import pytest
+import rclpy
+
 import modules.ScenarioModule.scenariomodule as sm
 import atos_interfaces.srv as srv
-import rclpy
 import rclpy.parameter
 
 
-def test_get_six_objects_in_scenario(get_file_path):
-    rclpy.init()
-    scenario_module = sm.ScenarioModule()
-    scenario_path = get_file_path("../../conf/osc/GaragePlanScenario.xosc")
-    objects = scenario_module.get_all_objects_in_scenario(scenario_path)
-    assert len(objects) == 6
-
-    rclpy.shutdown()
-
-
-def test_get_object_ids():
+@pytest.fixture(scope="module")
+def scenario_module():
     rclpy.init()
     active_objects = ["1", "2"]
     scenario_module = sm.ScenarioModule()
@@ -34,37 +27,55 @@ def test_get_object_ids():
             )
         ]
     )
+    yield scenario_module
+    rclpy.shutdown()
+
+
+def test_get_six_objects_in_scenario(get_file_path, scenario_module):
+    scenario_path = get_file_path("../../conf/osc/GaragePlanScenario.xosc")
+    objects = scenario_module.get_all_objects_in_scenario(scenario_path)
+    assert len(objects) == 6
+
+
+def test_get_object_ids(scenario_module):
     request = srv.GetObjectIds.Request()
     response = srv.GetObjectIds.Response()
     scenario_module.srv_get_object_id_array(request, response)
     assert list(response.ids) == [1, 2]
     assert response.success == True
-    rclpy.shutdown()
 
 
-def test_get_object_ip():
-    rclpy.init()
-    active_objects = ["1"]
-    scenario_module = sm.ScenarioModule()
-    scenario_module.set_parameters(
-        [
-            rclpy.parameter.Parameter(
-                "open_scenario_file",
-                rclpy.Parameter.Type.STRING,
-                "GaragePlanScenario.xosc",
-            )
-        ]
-    )
-    scenario_module.set_parameters(
-        [
-            rclpy.parameter.Parameter(
-                "active_object_names", rclpy.Parameter.Type.STRING_ARRAY, active_objects
-            )
-        ]
-    )
+def test_get_object_ip(scenario_module):
     request = srv.GetObjectIp.Request(id=1)
     response = srv.GetObjectIp.Response()
     scenario_module.srv_get_object_ip(request, response)
     assert response.ip == "127.0.0.1"
     assert response.success == True
-    rclpy.shutdown()
+
+
+def test_set_object_ip(scenario_module):
+    request = srv.SetObjectIp.Request(id=1, ip="6.6.6.6")
+    response = srv.SetObjectIp.Response()
+    scenario_module.srv_set_object_ip(request, response)
+    assert response.success == True
+
+    request = srv.GetObjectIp.Request(id=1)
+    response = srv.GetObjectIp.Response()
+    scenario_module.srv_get_object_ip(request, response)
+    assert response.ip == "6.6.6.6"
+    assert response.success == True
+
+
+def test_ip_is_still_user_defined_after_init(scenario_module):
+    request = srv.SetObjectIp.Request(id=1, ip="6.6.6.6")
+    response = srv.SetObjectIp.Response()
+    scenario_module.srv_set_object_ip(request, response)
+    assert response.success == True
+
+    scenario_module.init_callback(None)
+
+    request = srv.GetObjectIp.Request(id=1)
+    response = srv.GetObjectIp.Response()
+    scenario_module.srv_get_object_ip(request, response)
+    assert response.ip == "6.6.6.6"
+    assert response.success == True
