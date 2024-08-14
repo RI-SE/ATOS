06d69be02ce51d247e05626accd2fd3a5cd15b7b
Robert Brenick <robert.brenick@astazero.com>
2024-08-14 09:30:18 +0200

    Update atos/common/CRSTransformation.cpp
    Co-authored-by: victorjarlow <97448034+victorjarlow@users.noreply.github.com>

--- atos/common/CRSTransformation.cpp
+++ atos/common/CRSTransformation.cpp
@@ -55,7 +55,8 @@ void CRSTransformation::apply(
   // Put transformed PJ_POINTS back into TrajPoints
   for (int i = 0; i < arraySize; i++) {
     if (isnan(in[i].xyz.x) || isnan(in[i].xyz.y) || isnan(in[i].xyz.z) ) {
-      // Apply transformation to the point again. quick fix. Need to understand
+      // Apply transformation to the point again. quick fix. 
+      // TODO: Need to understand why some points are not transformed
       // why some points are not transformed
       auto point = geometry_msgs::msg::Point();
       point.x = trajPoints[i].getXCoord();
