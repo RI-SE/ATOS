95590717a5450fba9b2e47e862658d0c27d012fc
Robert Brenick <robert.brenick@astazero.com>
2024-08-22 11:47:46 +0200

    Update logs and formatting

--- atos/modules/ObjectControl/src/states/initialized.cpp
+++ atos/modules/ObjectControl/src/states/initialized.cpp
@@ -3,71 +3,61 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/.
  */
-#include "state.hpp"
 #include "journal.hpp"
+#include "state.hpp"
 
+AbstractKinematics::Initialized::Initialized() {}
 
-AbstractKinematics::Initialized::Initialized() {
-
-}
-
-void AbstractKinematics::Initialized::connectRequest(
-		ObjectControl& handler) {
-	RCLCPP_INFO(handler.get_logger(), "Handling connect request");
-	JournalRecordData(JOURNAL_RECORD_EVENT, "CONNECT received");
+void AbstractKinematics::Initialized::connectRequest(ObjectControl &handler) {
+  RCLCPP_INFO(handler.get_logger(), "Handling connect request");
+  JournalRecordData(JOURNAL_RECORD_EVENT, "CONNECT received");
 }
 
 void AbstractKinematics::Initialized::disconnectRequest(
-		ObjectControl& handler) {
-	handler.clearScenario();
+    ObjectControl &handler) {
+  handler.clearScenario();
 }
-
 
 /*! ******************************************************
  * \section RelativeKinematics
  *  ******************************************************
  */
-void RelativeKinematics::Initialized::connectRequest(
-		ObjectControl& handler) {
+void RelativeKinematics::Initialized::connectRequest(ObjectControl &handler) {
 
-	if (handler.getVehicleIDs().empty()) {
-		RCLCPP_INFO(handler.get_logger(), "Object directory empty. Disconnecting...");
-		RelativeKinematics::Initialized::disconnectRequest(handler);
-	}
-	else {
-		AbstractKinematics::Initialized::connectRequest(handler);
-		setState(handler, new RelativeKinematics::Connecting);
-	}
+  if (handler.getVehicleIDs().empty()) {
+    RCLCPP_WARN(handler.get_logger(),
+                "No objects are configured! Canceling connect request...");
+    RelativeKinematics::Initialized::disconnectRequest(handler);
+  } else {
+    AbstractKinematics::Initialized::connectRequest(handler);
+    setState(handler, new RelativeKinematics::Connecting);
+  }
 }
 
 void RelativeKinematics::Initialized::disconnectRequest(
-		ObjectControl& handler) {
-	AbstractKinematics::Initialized::disconnectRequest(handler);
-	setState(handler, new RelativeKinematics::Idle);
+    ObjectControl &handler) {
+  AbstractKinematics::Initialized::disconnectRequest(handler);
+  setState(handler, new RelativeKinematics::Idle);
 }
-
 
 /*! ******************************************************
  * \section AbsoluteKinematics
  *  ******************************************************
  */
-void AbsoluteKinematics::Initialized::connectRequest(
-		ObjectControl& handler) {
+void AbsoluteKinematics::Initialized::connectRequest(ObjectControl &handler) {
 
-	if (handler.getVehicleIDs().empty()) {
-		RCLCPP_INFO(handler.get_logger(), "Object directory empty. Disconnecting...");
-		AbsoluteKinematics::Initialized::disconnectRequest(handler);
-	}
-	else {
-		AbstractKinematics::Initialized::connectRequest(handler);
-		setState(handler, new AbsoluteKinematics::Connecting);
-	}
+  if (handler.getVehicleIDs().empty()) {
+    RCLCPP_WARN(handler.get_logger(),
+                "No objects are configured! Canceling connect request...");
+    AbsoluteKinematics::Initialized::disconnectRequest(handler);
+  } else {
+    AbstractKinematics::Initialized::connectRequest(handler);
+    setState(handler, new AbsoluteKinematics::Connecting);
+  }
 }
 
 void AbsoluteKinematics::Initialized::disconnectRequest(
-		ObjectControl& handler) {
-	AbstractKinematics::Initialized::disconnectRequest(handler);
-	setState(handler, new AbsoluteKinematics::Idle);
+    ObjectControl &handler) {
+  AbstractKinematics::Initialized::disconnectRequest(handler);
+  setState(handler, new AbsoluteKinematics::Idle);
 }
-
-
