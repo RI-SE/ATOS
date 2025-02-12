2497429a7ded2c4223991389aefb1bcdd8c5e676
Robert Brenick <robert.brenick@astazero.com>
2025-02-12 08:58:05 +0100

    Add half second to object start times

--- atos/modules/ObjectControl/src/testobject.cpp
+++ atos/modules/ObjectControl/src/testobject.cpp
@@ -339,6 +339,8 @@ void TestObject::sendStart(std::chrono::system_clock::time_point startTime) {
 	StartMessageType strt;
 	strt.startTime.tv_sec = std::chrono::duration_cast<std::chrono::seconds>(startTime.time_since_epoch()).count();
 	strt.startTime.tv_usec = std::chrono::duration_cast<std::chrono::microseconds>(startTime.time_since_epoch()).count() % 1000000;
+	auto delay_us = 500000; // 0.5 s
+	strt.startTime.tv_usec += delay_us;
 	strt.isTimestampValid = true;
 	this->comms.cmd << strt;
 }
