1ea85eb0fb959a5f852383f8150ecd7114054aea
Sebastian Loh Lindholm <sll@sebart.net>
2026-06-08 15:33:28 +0200

    Tied to used esmini version instead of ROS version.

--- atos/modules/EsminiAdapter/src/esminiadapter.cpp
+++ atos/modules/EsminiAdapter/src/esminiadapter.cpp
@@ -221,7 +221,7 @@ void EsminiAdapter::reportObjectPosition(const Monitor::message_type::SharedPtr 
 	auto speed = monr->velocity.twist.linear;
 
 	// Reporting to Esmini
-#if ROS_HUMBLE
+#ifdef ESMINI_REPORT_OBJECT_POS_HAS_TIMESTAMP
 	float timestamp = 0.0f; // Not really used according to esmini documentation
 	SE_ReportObjectPos(esminiObjectId, timestamp, pos.x, pos.y, pos.z, yaw, pitch, roll);
 #else
