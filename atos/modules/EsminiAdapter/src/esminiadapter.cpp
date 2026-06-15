0c7792ea9689ad85352648332e210e8c710dddb4
Sebastian Loh Lindholm <sll@sebart.net>
2026-06-15 15:28:39 +0200

    Updated according to comment

--- atos/modules/EsminiAdapter/src/esminiadapter.cpp
+++ atos/modules/EsminiAdapter/src/esminiadapter.cpp
@@ -221,12 +221,8 @@ void EsminiAdapter::reportObjectPosition(const Monitor::message_type::SharedPtr 
 	auto speed = monr->velocity.twist.linear;
 
 	// Reporting to Esmini
-#ifdef ESMINI_REPORT_OBJECT_POS_HAS_TIMESTAMP
 	float timestamp = 0.0f; // Not really used according to esmini documentation
 	SE_ReportObjectPos(esminiObjectId, timestamp, pos.x, pos.y, pos.z, yaw, pitch, roll);
-#else
-	SE_ReportObjectPos(esminiObjectId, pos.x, pos.y, pos.z, yaw, pitch, roll);
-#endif
 	SE_ReportObjectSpeed(esminiObjectId, speed.x);
 }
 
