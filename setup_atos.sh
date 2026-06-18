089fd9e542b6ef55f1c67fe8663421b8959d450a
Sebastian Loh Lindholm <sll@sebart.net>
2026-06-18 22:44:34 +0200

    Fix setup_atos helper sourcing

--- setup_atos.sh
+++ setup_atos.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Get this file location
-REPO_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
-
 #####################################
 ###### Pre-installation checks ######
 #####################################
 
-source "${REPO_DIR}/scripts/installation/install_functions.sh"
+source "scripts/installation/install_functions.sh"
+
+# Get this file location
+REPO_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 
 # Check if running on a supported Ubuntu variant
 if is_ubuntu_core; then
