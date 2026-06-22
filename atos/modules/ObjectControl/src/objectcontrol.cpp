932a54da95ae5a9c3c899383574bb283337242f5
Sebastian Loh Lindholm <sll@sebart.net>
2026-06-22 11:07:09 +0200

    Relax scenario loading timeouts in ObjectControl

--- atos/modules/ObjectControl/src/objectcontrol.cpp
+++ atos/modules/ObjectControl/src/objectcontrol.cpp
@@ -289,17 +289,17 @@ bool ObjectControl::loadScenario() {
 			std::future<bool> trajLoadedFuture	 = trajLoaded.get_future();
 			std::future<bool> ipLoadedFuture	 = ipLoaded.get_future();
 			std::future<bool> originLoadedFuture = originLoaded.get_future();
-			if (auto status = trajLoadedFuture.wait_for(5s);
+			if (auto status = trajLoadedFuture.wait_for(15s);
 				status != std::future_status::ready || !trajLoadedFuture.get()) {
 				RCLCPP_ERROR(get_logger(), "Trajectory loading failed for object ID %u", id);
 				successful = false;
 			}
-			if (auto status = ipLoadedFuture.wait_for(250ms);
+			if (auto status = ipLoadedFuture.wait_for(2s);
 				status != std::future_status::ready || !ipLoadedFuture.get()) {
 				RCLCPP_ERROR(get_logger(), "IP loading failed for object ID %u", id);
 				successful = false;
 			}
-			if (auto status = originLoadedFuture.wait_for(250ms);
+			if (auto status = originLoadedFuture.wait_for(2s);
 				status != std::future_status::ready || !originLoadedFuture.get()) {
 				RCLCPP_ERROR(get_logger(), "Origin loading failed for object ID %u", id);
 				successful = false;
@@ -309,6 +309,7 @@ bool ObjectControl::loadScenario() {
 				trajectoryClient->prune_pending_requests();
 				ipClient->prune_pending_requests();
 				originClient->prune_pending_requests();
+				scenarioLoaded.set_value(false);
 				return;
 			}
 		}
@@ -320,7 +321,7 @@ bool ObjectControl::loadScenario() {
 	auto future	 = idClient->async_send_request(request, idsCallback);
 	// Wait for all objects to load
 	std::future scenarioLoadedFuture = scenarioLoaded.get_future();
-	if (auto status = scenarioLoadedFuture.wait_for(10s); status == std::future_status::ready) {
+	if (auto status = scenarioLoadedFuture.wait_for(30s); status == std::future_status::ready) {
 		return scenarioLoadedFuture.get(); // Get the status value
 	} else if (status == std::future_status::timeout) {
 		RCLCPP_ERROR(get_logger(), "Scenario loading timed out");
